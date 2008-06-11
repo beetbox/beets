f6feb5fbb0be84c003e01082afe1762ac2ef2f90
adrian.sampson <none@none>
2008-06-11 07:10:56 +0000

    added alltest convenience script
    --HG--
    rename : test/tagtest.py => test/tag.py
    extra : convert_revision : svn%3A41726ec3-264d-0410-9c23-a9f1637257cc/trunk%4013

---
+++ test/alltests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import unittest
+
+test_modules = ['tag', 'library']
+
+def suite():
+    s = unittest.TestSuite()
+    for mod in map(__import__, test_modules):
+        s.addTest(mod.suite())
+    return s
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
