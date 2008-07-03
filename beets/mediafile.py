fda59f5c8560478ad6150a88070f9483f195c99c
adrian.sampson <none@none>
2008-07-03 23:54:19 +0000

    mediafile docstring corrections
    --HG--
    extra : convert_revision : svn%3A41726ec3-264d-0410-9c23-a9f1637257cc/trunk%4051

--- beets/mediafile.py
+++ beets/mediafile.py
@@ -3,10 +3,11 @@ automatically detect file types and provide a unified interface for a useful
 subset of music files' tags.
 
 Usage:
->>> f = MediaFile('something.mp3')
+>>> f = MediaFile('Lucy.mp3')
 >>> f.title
 u'Lucy in the Sky with Diamonds'
 >>> f.artist = 'The Beatles'
+>>> f.save_tags()
 
 A field will always return a reasonable value of the correct type, even if no
 tag is present. If no value is available, the value will be false (e.g., zero
