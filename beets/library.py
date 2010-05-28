67c4ec9507955601c3e9b3f7aed9ac4b9ef50bca
Adrian Sampson <adrian@radbox.org>
2010-05-28 00:07:11 -0700

    change to MIT license
    --HG--
    rename : COPYING.txt => LICENSE.txt

--- beets/library.py
+++ beets/library.py
@@ -1,18 +1,16 @@
 # This file is part of beets.
-# Copyright 2009, Adrian Sampson.
+# Copyright 2010, Adrian Sampson.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
 # 
-# Beets is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# Beets is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with beets.  If not, see <http://www.gnu.org/licenses/>.
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
 
 import sqlite3
 import os
