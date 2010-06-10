08552e2046d1baa94673310e8be6285df373cca2
Adrian Sampson <adrian@radbox.org>
2010-06-10 10:25:25 -0700

    remove download link for munkres (author updated pypi entry)

--- setup.py
+++ setup.py
@@ -29,8 +29,9 @@ setup(name='beets',
 
       The purpose of beets is to get your music collection right once
       and for all. It catalogs your collection, automatically
-      improving its metadata as it goes. It then provides a set of
-      tools for manipulating and accessing your music.
+      improving its metadata as it goes using the MusicBrainz database.
+      It then provides a set of tools for manipulating and accessing
+      your music.
       
       Beets also includes a music player that implements the MPD
       protocol, so you can play music in your beets library using any
@@ -51,8 +52,5 @@ setup(name='beets',
           'cmdln',
           'eventlet >= 0.9.3',
       ],
-
-      # The munkres module doesn't have any working download links.
-      dependency_links=['http://beets.radbox.org/packages/'],
 )
 
