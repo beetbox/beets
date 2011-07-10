3b2ea5d289c5a243bbe9e19a14213f157331ca91
Adrian Sampson <adrian@radbox.org>
2011-07-10 12:07:52 -0700

    add a missing __future__ import in embedart

--- beetsplug/embedart.py
+++ beetsplug/embedart.py
@@ -1,3 +1,20 @@
+# This file is part of beets.
+# Copyright 2011, Adrian Sampson.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+"""Allows beets to embed album art into file metadata."""
+from __future__ import with_statement
+
 import logging
 import imghdr
 
