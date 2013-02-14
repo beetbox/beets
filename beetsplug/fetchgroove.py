297078f17340dcfd5b1972eba8f90a254ae65c04
Pedro Silva <psilva+git@pedrosilva.pt>
2013-02-14 18:50:30 +0000

    First fully working commit

--- beetsplug/fetchgroove.py
+++ beetsplug/fetchgroove.py
@@ -12,54 +12,65 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-"""Determine which tracks are missing from the library, and either list
-them or download and import them using the fetch and miss commands.
-"""
+'''list or download missing items
+'''
+import os
 import logging
+import tempfile
 
 import grooveshark
 
+from beets import config
 from beets.autotag import hooks
 from beets.library import Item
+from beets.mediafile import MediaFile
 from beets.plugins import BeetsPlugin
-from beets.ui import Subcommand
+from beets.ui import decargs, print_obj, Subcommand
+from beets.util import mkdirall
 
-log = logging.getLogger("beets")
+plugin = 'fetchmissing'
+log = logging.getLogger('beets')
 client = grooveshark.Client()
 client.init()
 
 
 def _missing(album, lib=None):
-    """Fetch from Musicbrainz and return any (track, album) info missing
-    from album.
-    """
-    item_paths = map(lambda i: i.path, album.items())
-    item_mbids = map(lambda i: i.mb_trackid, album.items())
-    item_total = len(filter(None, item_paths))
+    '''Query MusicBrainz to determine items missing from `album`,
+    optionally caching them in `lib` to avoid further queries.
+    '''
+    item_paths = filter(None, map(lambda i: i.path, album.items()))
+    item_mbids = map(lambda x: x.mb_trackid,
+                     filter(lambda i: i.path is not None, album.items()))
 
-    if item_total < album.tracktotal:
+    if len(item_paths) < album.tracktotal:
+        # check for cached items (in library but with no associated file)
         missing = [i for i in album.items() if not i.path]
         if missing:
             for item in missing:
-                log.info("{0} is missing but cached".format(item.id))
+                log.debug('{0}: {1} is missing (cache hit)'
+                          .format(plugin, item.title.encode('utf-8')))
                 yield item
             return
 
+        # fetch and cache missing items
+        # items are stored in library without path field
+        # this might break something, like the web plugin
         album_info = hooks._album_for_id(album.mb_albumid)
         for track_info in album_info.tracks:
             if track_info.track_id not in item_mbids:
                 item = _item(track_info, album_info, album.id)
-                log.warn("{0} is missing and uncached".format(item.id))
+                log.debug('{0}: {1} is missing (caching)'
+                          .format(plugin, item.title.encode('utf-8')))
                 if lib:
                     lib.add(item)
                 yield item
 
 
 def _matches(songs, albums, artists):
-    """Given iterators of pygrooveshark songs, albums, and artists,
-    return an iterator of any songs spanning a transitive closure over
-    all three collections.
-    """
+    '''Given iterators of pygrooveshark `songs`, `albums`, and
+    `artists`, return an iterator of any `song`s spanning a transitive
+    closure over all three collections.
+    '''
     ### FIXME: the commented stuff uses an internal dictionary, and
     ### provides more matches, for example for tracks prefixed by
     ### numbers, etc.  The currently uncommented method appears to only
@@ -77,6 +88,10 @@ def _matches(songs, albums, artists):
     #             if song_from_album and song_from_artist and album_from_artist:
     #                yield grooveshark.Song.from_export(song, client.connection)
 
+    # naive algorithm with O(n*m*p). must profile to check whether problematic
+    # TODO: index everything in some data structure for O(n+m+p)
+    # currently checks for songs with associated album and artist.
+    # TODO: account for case of singletons (ie. songs with no albums)
     for artist in artists:
         for album in albums:
             for song in songs:
@@ -84,12 +99,19 @@ def _matches(songs, albums, artists):
                 song_from_artist = song.artist.id == artist.id
                 album_from_artist = album.artist.id == artist.id
                 if song_from_album and song_from_artist and album_from_artist:
+                    log.debug('{0}: found {1} in grooveshark'.format(plugin,
+                                                                     song))
                     yield song
 
 
 def _item(track_info, album_info, album_id):
-    """Build a library Item from track and album info objects.
-    """
+    '''Build and return `item` from `track_info` and `album info`
+    objects. `item` is missing what fields cannot be obtained from
+    MusicBrainz alone (encoder, rg_track_gain, rg_track_peak,
+    rg_album_gain, rg_album_peak, original_year, original_month,
+    original_day, length, bitrate, format, samplerate, bitdepth,
+    channels, mtime.)
+    '''
     t = track_info
     a = album_info
 
@@ -129,9 +151,12 @@ def _item(track_info, album_info, album_id):
 
 
 def _candidates(item):
-    """Given a track item, query grooveshark for matching songs, albums,
-    and artists, and yield any matching songs.
-    """
+    '''Given a track `item`, query grooveshark for matching songs,
+    albums, and artists, and yield any matching `song`s.
+    '''
+    # the 3 calls are necessary because grooveshark's API does not
+    # offer, to my knowledge, an advanced query syntax, so we query for
+    # all 3 fields, and then try to filter songs with `_matches`
     songs = client.search(item.title, type=client.SONGS)
     albums = client.search(item.album, type=client.ALBUMS)
     artists = client.search(item.albumartist, type=client.ARTISTS)
@@ -140,36 +165,112 @@ def _candidates(item):
         yield song
 
 
-def _download(song):
-    """Download and return song.
-    """
-    return song.stream.data.read()
+def _download(song, directory=None):
+    '''Download `song` to `directory`. Return `path`.
+    '''
+    if not directory:
+        directory = tempfile.mkdtemp()
+    path = os.path.join(directory, song.id)
+    mkdirall(path)
+
+    with open(path, 'wb') as f:
+        data = song.stream.data.read()
+        f.write(data)
+        nbytes = len(data)
+        log.debug('{0}: wrote {1} bytes to {2}'.format(plugin, nbytes, path))
+
+    return path
+
+
+def _update(item, path):
+    '''Update metadata to `item` based on MediaFile(`path`). Return
+    modified `item`.
+    '''
+    mf = MediaFile(path)
+
+    newpath = "{0}.{1}".format(path, mf.type)
+    os.rename(path, newpath)
+    log.debug('{0}: renamed {1} to {2}'.format(plugin,
+                                               path,
+                                               newpath))
+
+    item.path = newpath
+    item.bitdepth = mf.bitdepth
+    item.bitrate = mf.bitrate
+    item.channels = mf.channels
+    item.encoder = mf.encoder
+    item.format = mf.format
+    item.length = mf.length
+    item.original_day = mf.original_day
+    item.original_month = mf.original_month
+    item.original_year = mf.original_year
+    item.rg_album_gain = mf.rg_album_gain
+    item.rg_album_peak = mf.rg_album_peak
+    item.rg_track_gain = mf.rg_track_gain
+    item.rg_track_peak = mf.rg_track_peak
+    item.samplerate = mf.samplerate
+
+    return item
 
 
 class GroovesharkPlugin(BeetsPlugin):
-    """Fetch tracks missing from albums from grooveshark.
-    """
+    '''Fetch tracks missing from albums from grooveshark.
+    '''
     def __init__(self):
         super(GroovesharkPlugin, self).__init__()
-        self._commands = [Subcommand('fetchgroove',
-                                     help=__doc__,
-                                     aliases=['fetch']),
-                          Subcommand('missing',
-                                     help=__doc__,
-                                     aliases=['miss'])]
+
+        self.config.add({'download': False,
+                         'move': False,
+                         'format': None,
+                         'tmpdir': None})
+
+        self._command = Subcommand('missing',
+                                   help=__doc__,
+                                   aliases=['miss'])
+
+        self._command.parser.add_option('-d', '--download', dest='download',
+                                        action='store_true',
+                                        help='download missing songs')
+
+        self._command.parser.add_option('-m', '--move', dest='move',
+                                        action='store_true',
+                                        help='move new items to library')
+
+        self._command.parser.add_option('-f', '--format', dest='format',
+                                        action='store', type='string',
+                                        help='print with custom FORMAT',
+                                        metavar='FORMAT')
+
+        self._command.parser.add_option('-t', '--tmpdir', dest='tmpdir',
+                                        action='store', type='string',
+                                        help='temp DIR to save songs',
+                                        metavar='DIR')
 
     def commands(self):
-        def _fetch(lib, opts, args):
-            for album in lib.albums():
-                for item in _missing(album, lib):
-                    print [unicode(i.name) for i in _candidates(item)]
-
         def _miss(lib, opts, args):
-            for album in lib.albums():
-                for item in _missing(album, lib):
-                    print "%s - %s - %s" % (item.albumartist,
-                                            item.album, item.title)
+            self.config.set_args(opts)
+            download = self.config['download'].get(bool)
+            move = self.config['move'].get(bool)
+            fmt = self.config['format'].get()
+            tmpdir = self.config['tmpdir'].get()
+            write = config['import']['write'].get(bool)
 
-        self._commands[0].func = _fetch
-        self._commands[1].func = _miss
-        return self._commands
+            for album in lib.albums(decargs(args)):
+                for item in _missing(album, lib):
+                    print_obj(item, lib, fmt=fmt)
+
+                    if download:
+                        for s in _candidates(item):
+                            tmp = _download(s, tmpdir)
+                            item = _update(item, tmp)
+                            if move:
+                                lib.move(item)
+                                log.debug('{0}: moved item to {1}'
+                                          .format(plugin, item.path))
+                            if write:
+                                item.write()
+                                log.debug('{0}: wrote tags to {1}'
+                                          .format(plugin, item.path))
+
+        self._command.func = _miss
+        return [self._command]
