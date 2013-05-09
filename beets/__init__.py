12be4f23b09cb11c332b942a1ae3033dbe691212
Adrian Sampson <adrian@radbox.org>
2013-05-08 22:28:35 -0700

    version bump: 1.1.1

--- beets/__init__.py
+++ beets/__init__.py
@@ -12,7 +12,7 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __author__ = 'Adrian Sampson <adrian@radbox.org>'
 
 import beets.library
--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ project = u'beets'
 copyright = u'2012, Adrian Sampson'
 
 version = '1.1'
-release = '1.1.0'
+release = '1.1.1'
 
 pygments_style = 'sphinx'
 
