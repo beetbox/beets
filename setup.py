daec2e68067e5508fbc5efde1a9c89fe7aa89d26
Adrian Sampson <adrian@radbox.org>
2013-06-06 10:17:46 -0700

    version bump: 1.2.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if 'sdist' in sys.argv:
     shutil.copytree(os.path.join(docdir, '_build', 'man'), mandir)
 
 setup(name='beets',
-      version='1.2.0',
+      version='1.2.1',
       description='music tagger and library organizer',
       author='Adrian Sampson',
       author_email='adrian@radbox.org',
