bd598696f0cc1b764fc42ad222c11fde904f8351
Adrian Sampson <adrian@radbox.org>
2013-09-17 12:38:16 -0700

    ftintitle: syntactic/comment style cleanup
    - expunge remaining CamelCase identifiers
    - start adding PEP8 space around operators
    - start wrapping to 79 columns
    - access track fields with attribute syntax
    
    --HG--
    extra : amend_source : 9c916013230dd3cde187f2a55e89587b009bce6a

--- beetsplug/ftintitle.py
+++ beetsplug/ftintitle.py
@@ -1,5 +1,5 @@
-# This file is a plugin on beets.
-#Copyright (c) <2013> Verrus, <github.com/Verrus/beets-plugin-featInTitle>
+# This file is part of beets.
+# Copyright 2013, Verrus, <github.com/Verrus/beets-plugin-featInTitle>
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -12,86 +12,112 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-"""puts featuring artists in the title instead of the artist field"""
-
+"""Moves "featured" artists to the title from the artist field.
+"""
 from beets.plugins import BeetsPlugin
-from beets import library
 from beets import ui
-import locale
 import re
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8") # fixes encoding issues using a pipe
 
 
-
-
-class ftInTitle(BeetsPlugin):
+class FtInTitlePlugin(BeetsPlugin):
     def commands(self):
-        cmd = ui.Subcommand('ftintitle', help='puts featuring artists in the title instead of the artist field')
+        cmd = ui.Subcommand('ftintitle',
+                            help='move featured artists to the title field')
         def func(lib, opts, args):
-        
+
             def find_supplementary_artists(artistfield):
-                return re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|\b[wW]ith\b|&|vs\.|and', artistfield,1) #only split on the first.
-            def detect_If_featuring_artist_already_In_title(titleField):
-                return re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|\b[wW]ith\b|&', titleField)
-            # feat is already in title only replace artistfield
-            def write_artist_field_only_and_print_edited_file_loc(track,albumArtist,sortArtist):
+                return re.split(
+                    r'[fF]t\.|[fF]eaturing|[fF]eat\.|\b[wW]ith\b|&|vs\.|and',
+                    artistfield,
+                    1  # Only split on the first.
+                )
+
+            def detect_if_featuring_artist_already_in_title(title):
+                return re.split(
+                    r'[fF]t\.|[fF]eaturing|[fF]eat\.|\b[wW]ith\b|&',
+                    title
+                )
+
+            # Feat is already in title: only replace the artist field.
+            def write_artist_field_only_and_print_edited_file_loc(track, albumartist, sort_artist):
                 print track.__getattr__("path")
-                print "new artist field",albumArtist.strip()
-                track["artist"] = albumArtist
-                track["artist_sort"] = rewrite_sort_artist(sortArtist)
+                print "new artist field",albumartist.strip()
+                track.artist = albumartist
+                track.artist_sort = rewrite_sort_artist(sort_artist)
                 track.write()
-            # write a new title and a new artistfield.
-            def write_artist_and_title_field_and_print_edited_file_loc(track,albumArtist,titleField,featuringPartofArtistField,sortArtist):
+
+            # Write a new title and a new artist field.
+            def write_artist_and_title_field_and_print_edited_file_loc(track, albumartist, title, feat_part, sort_artist):
                 print track.__getattr__("path")
-                print "albumartist:",albumArtist," title:",titleField," featuartist:",featuringPartofArtistField
-                track["artist"] = albumArtist
-                track["artist_sort"] = rewrite_sort_artist(sortArtist)
-                track["title"] = titleField.strip() + " feat." + featuringPartofArtistField
+                print "albumartist:",albumartist," title:",title," featuartist:",feat_part
+                track.artist = albumartist
+                track.artist_sort = rewrite_sort_artist(sort_artist)
+                track.title = title.strip() + " feat." + feat_part
                 track.write()
-            # split the extended artistfield in the extended part and albumartist
-            def split_on_album_artist(albumArtist,artistfield):
-                return re.split(albumArtist, artistfield)
-            #checks if title has a feat artist and calls the writing methods accordingly
-            def choose_writing_of_title_and_write(track,albumArtist,titleField,featuringPartofArtistField,sortArtist):
-                if len(detect_If_featuring_artist_already_In_title(titleField))>1: #if already in title only replace the artist field.
-                    #no replace title
-                    write_artist_field_only_and_print_edited_file_loc(track,albumArtist,sortArtist)
+
+            # Split the extended artistfield in the extended part and
+            # albumartist.
+            def split_on_album_artist(albumartist, artistfield):
+                return re.split(albumartist, artistfield)
+
+            # Checks if title has a feat artist and calls the writing methods
+            # accordingly.
+            def choose_writing_of_title_and_write(track, albumartist, title,
+                                                  feat_part, sort_artist):
+                # If already in title, only replace the artist field.
+                if len(detect_if_featuring_artist_already_in_title(title)) > 1:
+                    # no replace title
+                    write_artist_field_only_and_print_edited_file_loc(
+                        track, albumartist, sort_artist
+                    )
                 else:
-                    #do replace title.
-                    write_artist_and_title_field_and_print_edited_file_loc(track,albumArtist,titleField,featuringPartofArtistField,sortArtist)
-            # filter the sort artist with a regex split. and strip.
-            def rewrite_sort_artist(sortArtist):
-                return find_supplementary_artists(sortArtist)[0].strip()
+                    # do replace title.
+                    write_artist_and_title_field_and_print_edited_file_loc(
+                        track, albumartist, title, feat_part, sort_artist
+                    )
+
+            # Filter the sort artist with a regex split and strip.
+            def rewrite_sort_artist(sort_artist):
+                return find_supplementary_artists(sort_artist)[0].strip()
+
             for track in lib.items():
-                artistfield  = track.__getattr__("artist").strip()
-                titleField = track.__getattr__("title").strip()
-                albumArtist = track.__getattr__("albumartist").strip()
-                sortArtist = track.__getattr__("artist_sort").strip()
-                suppArtistsSplit = find_supplementary_artists(artistfield)
+                artistfield  = track.artist.strip()
+                title = track.title.strip()
+                albumartist = track.albumartist.strip()
+                sort_artist = track.artist_sort.strip()
+                supp_artists = find_supplementary_artists(artistfield)
                 
-                if len(suppArtistsSplit)>1 and albumArtist!=artistfield: # found supplementary artist. and the albumArtist is not a perfect match.
+                if len(supp_artists) > 1 and albumartist != artistfield:
+                    # Found supplementary artist, and the albumArtist is
+                    # not a perfect match.
         
-                    albumArtistSplit = split_on_album_artist(albumArtist,artistfield) 
-                    if len(albumArtistSplit)>1 and albumArtistSplit[-1]!='': # check if the artist field is composed of the albumartist.  AND check if the last element of the split is not empty.
-                        featuringPartofArtistField = find_supplementary_artists(albumArtistSplit[-1])[-1] #last elements
-                        choose_writing_of_title_and_write(track,albumArtist,titleField,featuringPartofArtistField,sortArtist)
+                    albumartist_split = split_on_album_artist(albumartist,
+                                                              artistfield)
+                    if len(albumartist_split) > 1 and \
+                       albumartist_split[-1] != '':
+                        # Check if the artist field is composed of the
+                        # albumartist, AND check if the last element of
+                        # the split is not empty.
+                        feat_part = find_supplementary_artists(albumartist_split[-1])[-1] #last elements
+                        choose_writing_of_title_and_write(track, albumartist,
+                                                          title, feat_part,
+                                                          sort_artist)
                             
-                    elif len(albumArtistSplit)>1 and len(find_supplementary_artists(albumArtistSplit[0]))>1: #check for inversion of artist and featuring ; if feat is listed on the first split.
-                        featuringPartofArtistField = find_supplementary_artists(albumArtistSplit[0])[0] #first elements because of inversion
-                        choose_writing_of_title_and_write(track,albumArtist,titleField,featuringPartofArtistField,sortArtist)
+                    elif len(albumartist_split) > 1 and len(find_supplementary_artists(albumartist_split[0])) > 1:
+                        # Check for inversion of artist and featuring,
+                        # if feat is listed on the first split.
+                        feat_part = find_supplementary_artists(albumartist_split[0])[0] #first elements because of inversion
+                        choose_writing_of_title_and_write(track,albumartist,title,feat_part,sort_artist)
                                     
                     else:
                         print "#############################"
                         print "ftInTitle has not touched this track, unsure what to do with this one.:"
                         print "artistfield: ",artistfield
-                        print "albumArtist",albumArtist
-                        print "titleField: ",titleField
+                        print "albumArtist",albumartist
+                        print "titleField: ",title
                         print track.__getattr__("path")
                         print "#############################"
 
-            print "A Manual 'beet update' run is recommended. "
+            print "Manual 'beet update' run is recommended. "
         cmd.func = func
         return [cmd]
-    
