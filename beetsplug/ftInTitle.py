7565692de0d805735803bde5038f27471bd03766
Verrus <vrijks@gmail.com>
2013-09-17 14:33:27 +0200

    Updated plugin to beets coding standard

--- beetsplug/ftInTitle.py
+++ beetsplug/ftInTitle.py
@@ -1,29 +1,16 @@
-#Copyright (c) <2013> Verrus, <github.com/Verrus/beets-plugin-featInTitle>
-#All rights reserved.
-## New BSD License:
-#Redistribution and use in source and binary forms, with or without
-#modification, are permitted provided that the following conditions are met:
-    #* Redistributions of source code must retain the above copyright
-      #notice, this list of conditions and the following disclaimer.
-    #* Redistributions in binary form must reproduce the above copyright
-      #notice, this list of conditions and the following disclaimer in the
-      #documentation and/or other materials provided with the distribution.
-    #* Neither the name of the <organization> nor the
-      #names of its contributors may be used to endorse or promote products
-      #derived from this software without specific prior written permission.
-
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-#DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 # This file is a plugin on beets.
+#Copyright (c) <2013> Verrus, <github.com/Verrus/beets-plugin-featInTitle>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
 
 """puts featuring artists in the title instead of the artist field"""
 
@@ -40,65 +27,71 @@ sys.setdefaultencoding("utf-8") # fixes encoding issues using a pipe
 
 
 class ftInTitle(BeetsPlugin):
-	def commands(self):
-		cmd = ui.Subcommand('ftintitle', help='puts featuring artists in the title instead of the artist field')
-		def func(lib, opts, args):
-		
-			def findSupplementaryArtists(artistfield):
-				return re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|[wW]ith|&|vs\.|and', artistfield,1) #only split on the first.
-			def DetectIfFeaturingArtistAlreadyInTitle(titleField):
-				return re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|[wW]ith|&', titleField)
-			# feat is already in title only replace artistfield
-			def writeArtistFieldOnlyAndPrintEditedFileLoc(track,albumArtist):
-				print track.__getattr__("path")
-				print "new artist field",albumArtist.strip()
-				track.__setattr__("artist", albumArtist)
-				track.write()
-			# write a new title and a new artistfield.
-			def writeArtistAndTitleFieldAndPrintEditedFileLoc(track,albumArtist,titleField,featuringPartofArtistField):
-				print track.__getattr__("path")
-				print "albumartist:",albumArtist," title:",titleField," featuartist:",featuringPartofArtistField
-				track.__setattr__("artist", albumArtist)
-				track.__setattr__("title", titleField.strip() + " feat." + featuringPartofArtistField)
-				track.write()
-			# split the extended artistfield in the extended part and albumartist
-			def splitOnAlbumArtist(albumArtist,artistfield):
-				return re.split(albumArtist, artistfield)
-			#checks if title has a feat artist and calls the writing methods accordingly
-			def chooseWritingOfTitleAndWrite(track,albumArtist,titleField,featuringPartofArtistField):
-				if len(DetectIfFeaturingArtistAlreadyInTitle(titleField))>1: #if already in title only replace the artist field.
-					#no replace title
-					writeArtistFieldOnlyAndPrintEditedFileLoc(track,albumArtist)
-				else:
-					#do replace title.
-					writeArtistAndTitleFieldAndPrintEditedFileLoc(track,albumArtist,titleField,featuringPartofArtistField)
-			
-			for track in lib.items():
-				artistfield  = track.__getattr__("artist").strip()
-				titleField = track.__getattr__("title").strip()
-				albumArtist = track.__getattr__("albumartist").strip()
-				suppArtistsSplit = findSupplementaryArtists(artistfield)
-				if len(suppArtistsSplit)>1 and albumArtist!=artistfield: # found supplementary artist. and the albumArtist is not a perfect match.
-					albumArtistSplit = splitOnAlbumArtist(albumArtist,artistfield) 
-					
-					if len(albumArtistSplit)>1 and albumArtistSplit[-1]!='': # check if the artist field is composed of the albumartist.  AND check if the last element of the split is not empty.
-						featuringPartofArtistField = findSupplementaryArtists(albumArtistSplit[-1])[-1] #last elements
-						chooseWritingOfTitleAndWrite(track,albumArtist,titleField,featuringPartofArtistField)
-							
-					elif len(albumArtistSplit)>1 and len(findSupplementaryArtists(albumArtistSplit[0]))>1: #check for inversion of artist and featuring ; if feat is listed on the first split.
-						featuringPartofArtistField = findSupplementaryArtists(albumArtistSplit[0])[0] #first elements because of inversion
-						chooseWritingOfTitleAndWrite(track,albumArtist,titleField,featuringPartofArtistField)
-									
-					else:
-						print "#############################"
-						print "ftInTitle has not touched this track, unsure what to do with this one.:"
-						print "artistfield: ",artistfield
-						print "albumArtist",albumArtist
-						print "titleField: ",titleField
-						print track.__getattr__("path")
-						print "#############################"
-
-			print "A Manual 'beet update' run is recommended. "
-		cmd.func = func
-		return [cmd]
+    def commands(self):
+        cmd = ui.Subcommand('ftintitle', help='puts featuring artists in the title instead of the artist field')
+        def func(lib, opts, args):
         
+            def find_supplementary_artists(artistfield):
+                return re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|\b[wW]ith\b|&|vs\.|and', artistfield,1) #only split on the first.
+            def detect_If_featuring_artist_already_In_title(titleField):
+                return re.split('[fF]t\.|[fF]eaturing|[fF]eat\.|\b[wW]ith\b|&', titleField)
+            # feat is already in title only replace artistfield
+            def write_artist_field_only_and_print_edited_file_loc(track,albumArtist,sortArtist):
+                print track.__getattr__("path")
+                print "new artist field",albumArtist.strip()
+                track["artist"] = albumArtist
+                track["artist_sort"] = rewrite_sort_artist(sortArtist)
+                track.write()
+            # write a new title and a new artistfield.
+            def write_artist_and_title_field_and_print_edited_file_loc(track,albumArtist,titleField,featuringPartofArtistField,sortArtist):
+                print track.__getattr__("path")
+                print "albumartist:",albumArtist," title:",titleField," featuartist:",featuringPartofArtistField
+                track["artist"] = albumArtist
+                track["artist_sort"] = rewrite_sort_artist(sortArtist)
+                track["title"] = titleField.strip() + " feat." + featuringPartofArtistField
+                track.write()
+            # split the extended artistfield in the extended part and albumartist
+            def split_on_album_artist(albumArtist,artistfield):
+                return re.split(albumArtist, artistfield)
+            #checks if title has a feat artist and calls the writing methods accordingly
+            def choose_writing_of_title_and_write(track,albumArtist,titleField,featuringPartofArtistField,sortArtist):
+                if len(detect_If_featuring_artist_already_In_title(titleField))>1: #if already in title only replace the artist field.
+                    #no replace title
+                    write_artist_field_only_and_print_edited_file_loc(track,albumArtist,sortArtist)
+                else:
+                    #do replace title.
+                    write_artist_and_title_field_and_print_edited_file_loc(track,albumArtist,titleField,featuringPartofArtistField,sortArtist)
+            # filter the sort artist with a regex split. and strip.
+            def rewrite_sort_artist(sortArtist):
+                return find_supplementary_artists(sortArtist)[0].strip()
+            for track in lib.items():
+                artistfield  = track.__getattr__("artist").strip()
+                titleField = track.__getattr__("title").strip()
+                albumArtist = track.__getattr__("albumartist").strip()
+                sortArtist = track.__getattr__("artist_sort").strip()
+                suppArtistsSplit = find_supplementary_artists(artistfield)
+                
+                if len(suppArtistsSplit)>1 and albumArtist!=artistfield: # found supplementary artist. and the albumArtist is not a perfect match.
+        
+                    albumArtistSplit = split_on_album_artist(albumArtist,artistfield) 
+                    if len(albumArtistSplit)>1 and albumArtistSplit[-1]!='': # check if the artist field is composed of the albumartist.  AND check if the last element of the split is not empty.
+                        featuringPartofArtistField = find_supplementary_artists(albumArtistSplit[-1])[-1] #last elements
+                        choose_writing_of_title_and_write(track,albumArtist,titleField,featuringPartofArtistField,sortArtist)
+                            
+                    elif len(albumArtistSplit)>1 and len(find_supplementary_artists(albumArtistSplit[0]))>1: #check for inversion of artist and featuring ; if feat is listed on the first split.
+                        featuringPartofArtistField = find_supplementary_artists(albumArtistSplit[0])[0] #first elements because of inversion
+                        choose_writing_of_title_and_write(track,albumArtist,titleField,featuringPartofArtistField,sortArtist)
+                                    
+                    else:
+                        print "#############################"
+                        print "ftInTitle has not touched this track, unsure what to do with this one.:"
+                        print "artistfield: ",artistfield
+                        print "albumArtist",albumArtist
+                        print "titleField: ",titleField
+                        print track.__getattr__("path")
+                        print "#############################"
+
+            print "A Manual 'beet update' run is recommended. "
+        cmd.func = func
+        return [cmd]
+    
