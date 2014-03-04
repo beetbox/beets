e916609eeec4a410702324d339988abbe60e55ba
Thomas Scholtes <thomas-scholtes@gmx.de>
2014-03-04 17:12:42 +0100

    Save import history only when path associated
    Fixes #570

--- beets/importer.py
+++ beets/importer.py
@@ -454,7 +454,7 @@ class ImportTask(object):
     def save_history(self):
         """Save the directory in the history for incremental imports.
         """
-        if self.is_album and not self.sentinel:
+        if self.is_album and self.paths and not self.sentinel:
             history_add(self.paths)
 
 
