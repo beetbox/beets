7f3092ce5a2fb1a3a7cf0cbdcfed44afa90fa4d5
Kyle Konrad <kyle.t.konrad@gmail.com>
2014-04-03 12:04:51 -0700

    four space indentation and assertGreater for __gt__ coverage

--- test/test_util.py
+++ test/test_util.py
@@ -19,21 +19,24 @@ from _common import unittest
 from beets.util.enumeration import OrderedEnum, IndexableEnum
 
 class EnumTest(_common.TestCase):
-  """
-  Test Enum Subclasses defined in beets.util.enumeration
-  """
-  def test_ordered_enum(self):
-    OrderedEnumTest = OrderedEnum('OrderedEnumTest', ['a', 'b', 'c'])
-    self.assertLess(OrderedEnumTest.a, OrderedEnumTest.b)
-    self.assertLess(OrderedEnumTest.a, OrderedEnumTest.c)
-    self.assertLess(OrderedEnumTest.b, OrderedEnumTest.c)
+    """
+    Test Enum Subclasses defined in beets.util.enumeration
+    """
+    def test_ordered_enum(self):
+      OrderedEnumTest = OrderedEnum('OrderedEnumTest', ['a', 'b', 'c'])
+      self.assertLess(OrderedEnumTest.a, OrderedEnumTest.b)
+      self.assertLess(OrderedEnumTest.a, OrderedEnumTest.c)
+      self.assertLess(OrderedEnumTest.b, OrderedEnumTest.c)
+      self.assertGreater(OrderedEnumTest.b, OrderedEnumTest.a)
+      self.assertGreater(OrderedEnumTest.c, OrderedEnumTest.a)
+      self.assertGreater(OrderedEnumTest.c, OrderedEnumTest.b)
     
-  def test_indexable_enum(self):
-    values = ['a', 'b', 'c']
-    IndexableEnumTest = IndexableEnum('IndexableEnumTest', values)
-    for v in values:
-      self.assertEqual(IndexableEnumTest[IndexableEnumTest[v].value],
-                       IndexableEnumTest[v])
+    def test_indexable_enum(self):
+      values = ['a', 'b', 'c']
+      IndexableEnumTest = IndexableEnum('IndexableEnumTest', values)
+      for v in values:
+        self.assertEqual(IndexableEnumTest[IndexableEnumTest[v].value],
+                         IndexableEnumTest[v])
                                       
 
 def suite():
