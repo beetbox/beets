a9981c1dc7b581d6f581fdb5709efba07f0680e8
Adrian Sampson <adrian@radbox.org>
2014-11-07 19:51:38 -0800

    Comment about workaround in #1073

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -149,6 +149,9 @@ class DiscogsPlugin(BeetsPlugin):
         # cause a query to return no results, even if they match the artist or
         # album title. Use `re.UNICODE` flag to avoid stripping non-english
         # word characters.
+        # TEMPORARY: Encode as ASCII to work around a bug:
+        # https://github.com/sampsyo/beets/issues/1051
+        # When the library is fixed, we should encode as UTF-8.
         query = re.sub(r'(?u)\W+', ' ', query).encode('ascii', "replace")
         # Strip medium information from query, Things like "CD1" and "disk 1"
         # can also negate an otherwise positive result.
