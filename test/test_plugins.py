fa41de9e3f899bc271b96b9275fca34fcfcb11b4
Malte Ried <mried@users.noreply.github.com>
2014-11-28 17:50:04 +0100

    Added an interface for plugins to filter imported files

--- test/test_plugins.py
+++ test/test_plugins.py
@@ -154,6 +154,75 @@ class ItemTypeConflictTest(unittest.TestCase, TestHelper):
         self.register_plugin(AdventListenerPlugin)
         self.assertNotEqual(None, plugins.types(Item))
 
+class FileFilterPluginTest(_common.TestCase, ImportHelper):
+    """ Test the file filter plugin interface
+    """
+    def setUp(self):
+        super(FileFilterPluginTest, self).setUp()
+        self.setup_beets()
+        self._create_import_dir(1)
+        self._setup_import_session()
+        config['import']['enumerate_only'] = True
+        self.matcher = AutotagStub().install()
+#        self.io.install()
+
+        plugins.file_filters = self.file_filters
+
+        self.filters = []
+
+    def file_filters(self):
+        return self.filters
+
+    def filter_nothing(self, path, base):
+        return False
+
+    def filter_all(self, path, base):
+        return True
+
+    def filter_two(self, path, base):
+        return '2' in base
+
+    def tearDown(self):
+        self.teardown_beets()
+        self.matcher.restore()
+
+    def test_import(self):
+        resource_path = os.path.join(_common.RSRC, u'empty.mp3')
+        single_path = os.path.join(self.import_dir, u'track_2.mp3')
+
+        shutil.copy(resource_path, single_path)
+        import_files = [
+            os.path.join(self.import_dir, u'the_album'),
+            single_path
+        ]
+        self._setup_import_session(singletons=True)
+        self.importer.paths = import_files
+
+        # Filter will return False every time
+        self.filters = [self.filter_nothing]
+        lines = self.run_import()
+        self.assertEqual(len(lines), 0)
+
+        # Filter will return True every time
+        self.filters = [self.filter_all]
+        lines = self.run_import()
+        self.assertEqual(len(lines), 2)
+        self.assertEqual(lines[0], os.path.join(import_files[0], u'track_1.mp3'))
+        self.assertEqual(lines[1], import_files[1])
+
+        # Filter will return True if the file contains '2'
+        self.filters = [self.filter_two]
+        lines = self.run_import()
+        self.assertEqual(len(lines), 1)
+        self.assertEqual(lines[0], import_files[1])
+
+    def run_import(self):
+        self.io.restore()
+        self.io.install()
+        self.importer.run()
+        out = self.io.getoutput()
+
+        return out.splitlines()
 
 class FileFilterPluginTest(_common.TestCase, ImportHelper):
     """ Test the file filter plugin interface
