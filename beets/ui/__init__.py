267aec977fc1fd8b8f784336a3fff6e32e05adf3
Thomas Scholtes <thomas-scholtes@gmx.de>
2014-12-02 09:35:36 +0100

    Edit invalid config with beet config --edit

--- beets/ui/__init__.py
+++ beets/ui/__init__.py
@@ -923,7 +923,7 @@ def _raw_main(args, lib=None):
 
     # Bypass _setup so that an invalid configuration does not prevent
     # the editor from starting.
-    if subargs[0] == 'config' and '-e' in subargs:
+    if subargs[0] == 'config' and ('-e' in subargs or '--edit' in subargs):
         from beets.ui.commands import config_edit
         return config_edit()
 
