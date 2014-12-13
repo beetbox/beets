6a8a574f219bd3b7379671b18b3d70c4b065ee44
Fabrice Laporte <kraymer@gmail.com>
2014-12-13 23:34:50 +0100

    fix #1060

--- beets/util/__init__.py
+++ beets/util/__init__.py
@@ -696,3 +696,17 @@ def max_filename_length(path, limit=MAX_FILENAME_LENGTH):
         return min(res[9], limit)
     else:
         return limit
+
+
+def feat_tokens(extended=False):
+    """Returns the tokens to use to detect featuring artists in strings."""
+
+    FEAT_SPECIAL_CHARS = ['&', 'feat.', 'ft.']
+    FEAT_WORDS = ['ft', 'featuring', 'feat']
+    if extended:
+        FEAT_WORDS += ['with', 'vs', 'and', 'con']
+    regex = r'(%s)' % '|'.join(['\\b%s\\b' % re.escape(x) for x in FEAT_WORDS])
+    if extended:
+        regex = r'(%s|%s)' % \
+            ('|'.join([re.escape(x) for x in FEAT_SPECIAL_CHARS]), regex)
+    return regex
