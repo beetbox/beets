06f0e1dee1d7d1076dd37e2e56d70c2e322eaf25
Bruno Cauet <brunocauet@gmail.com>
2015-01-05 09:15:18 +0100

    Port beets.logging to python 2.6
    Multiple hacks :-)

--- test/test_logging.py
+++ test/test_logging.py
@@ -11,15 +11,15 @@ class LoggingTest(TestCase):
         l1 = log.getLogger("foo123")
         l2 = blog.getLogger("foo123")
         self.assertEqual(l1, l2)
-        self.assertEqual(type(l1), log.Logger)
+        self.assertEqual(l1.__class__, log.Logger)
 
         l3 = blog.getLogger("bar123")
         l4 = log.getLogger("bar123")
         self.assertEqual(l3, l4)
-        self.assertEqual(type(l3), blog.StrFormatLogger)
+        self.assertEqual(l3.__class__, blog.StrFormatLogger)
 
         l5 = l3.getChild("shalala")
-        self.assertEqual(type(l5), blog.StrFormatLogger)
+        self.assertEqual(l5.__class__, blog.StrFormatLogger)
 
     def test_str_format_logging(self):
         l = blog.getLogger("baz123")
@@ -29,7 +29,7 @@ class LoggingTest(TestCase):
         l.addHandler(handler)
         l.propagate = False
 
-        l.warning("foo {} {bar}", "oof", bar="baz")
+        l.warning("foo {0} {bar}", "oof", bar="baz")
         handler.flush()
         self.assertTrue(stream.getvalue(), "foo oof baz")
 
