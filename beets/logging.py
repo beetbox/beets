4ef52e62657b9c0ba44ddc00e3ae5cc30095a41a
Adrian Sampson <adrian@radbox.org>
2015-01-05 17:39:13 -0800

    Minor formatting fixes for logging overhaul #1198

--- beets/logging.py
+++ beets/logging.py
@@ -1,10 +1,23 @@
-"""Allow {}-style logging on python 2 and 3
+# This file is part of beets.
+# Copyright 2015, Adrian Sampson.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
 
-Provide everything the "logging" module does, the only difference is that when
-getLogger(name) instantiates a logger that logger uses {}-style formatting.
+"""A drop-in replacement for the standard-library `logging` module that
+allows {}-style log formatting on Python 2 and 3.
 
-It requires special hacks for python 2.6 due to logging.Logger being an old-
-style class and having no loggerClass attribute.
+Provides everything the "logging" module does. The only difference is
+that when getLogger(name) instantiates a logger that logger uses
+{}-style formatting.
 """
 
 from __future__ import absolute_import
@@ -13,7 +26,12 @@ from logging import *  # noqa
 import sys
 
 
-# create a str.format-based logger
+# We need special hacks for Python 2.6 due to logging.Logger being an
+# old- style class and having no loggerClass attribute.
+PY26 = sys.version_info[:2] == (2, 6)
+
+
+# Create a `str.format`-based logger.
 class StrFormatLogger(Logger):
     class _LogMessage(object):
         def __init__(self, msg, args, kwargs):
@@ -28,15 +46,15 @@ class StrFormatLogger(Logger):
         """Log msg.format(*args, **kwargs)"""
         m = self._LogMessage(msg, args, kwargs)
         return Logger._log(self, level, m, (), exc_info, extra)
-        # we cannot call super(StrFormatLogger, self) because it is not
-        # allowed on old-style classes (py2) which Logger is in python 2.6
-        # moreover we cannot make StrFormatLogger a new-style class (by
+        # We cannot call super(StrFormatLogger, self) because it is not
+        # allowed on old-style classes (py2), which Logger is in python 2.6.
+        # Moreover, we cannot make StrFormatLogger a new-style class (by
         # declaring 'class StrFormatLogger(Logger, object)' because the class-
         # patching stmt 'logger.__class__ = StrFormatLogger' would not work:
-        # both prev & new __class__ values must be either old- or new- style,
+        # both prev & new __class__ values must be either old- or new- style;
         # no mixing allowed.
 
-    if sys.version_info[:2] == (2, 6):
+    if PY26:
         def getChild(self, suffix):
             """Shameless copy from cpython's Lib/logging/__init__.py"""
             if self.root is not self:
@@ -54,10 +72,11 @@ def getLogger(name=None):
         return Logger.root
 
 
-if sys.version_info[:2] == (2, 6):
-    # no Manager.loggerClass so we dynamically change the logger class
-    # we must be careful to do that on new loggers only to avoid side-effects.
-    # Wrap Manager.getLogger
+# On Python 2.6, there is no Manager.loggerClass so we dynamically
+# change the logger class. We must be careful to do that on new loggers
+# only to avoid side-effects.
+if PY26:
+    # Wrap Manager.getLogger.
     old_getLogger = my_manager.getLogger
 
     def new_getLogger(name):
