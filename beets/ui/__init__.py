054488e1ab0806822e3956b0ef8153e3604bc8b9
Bruno Cauet <brunocauet@gmail.com>
2015-01-06 16:32:43 +0100

    Don't crash when no argument is given

--- beets/ui/__init__.py
+++ beets/ui/__init__.py
@@ -924,7 +924,8 @@ def _raw_main(args, lib=None):
     # Special case for the `config --edit` command: bypass _setup so
     # that an invalid configuration does not prevent the editor from
     # starting.
-    if subargs[0] == 'config' and ('-e' in subargs or '--edit' in subargs):
+    if subargs and subargs[0] == 'config' \
+       and ('-e' in subargs or '--edit' in subargs):
         from beets.ui.commands import config_edit
         return config_edit()
 
