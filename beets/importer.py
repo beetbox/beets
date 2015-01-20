beetbox/beets

commit b7125e434315b00584cb6d6abf8baf5d26fb3bf0

Centralize some counting in ImportTaskFactory
Part of a larger effort to simplify read_tasks for plugin interposition
(#1186).

beets/importer.py
@@ -837,11 +837,11 @@ class SingletonImportTask(ImportTask):
 # are so many methods which pass. We should introduce a new
 # BaseImportTask class.
 class SentinelImportTask(ImportTask):
-    """This class marks the progress of an import and does not import
-    any items itself.
+    """A sentinel task marks the progress of an import and does not
+    import any items itself.
 
-    If only `toppath` is set the task indicats the end of a top-level
-    directory import. If the `paths` argument is givent, too, the task
+    If only `toppath` is set the task indicates the end of a top-level
+    directory import. If the `paths` argument is also given, the task
     indicates the progress in the `toppath` import.
     """
 
@@ -879,9 +879,16 @@ class SentinelImportTask(ImportTask):
 
 
 class ArchiveImportTask(SentinelImportTask):
-    """Additional methods for handling archives.
+    """An import task that represents the processing of an archive.
 
-    Use when `toppath` points to a `zip`, `tar`, or `rar` archive.
+    `toppath` must be a `zip`, `tar`, or `rar` archive. Archive tasks
+    serve two purposes:
+    - First, it will unarchive the files to a temporary directory and
+      return it. The client should read tasks from the resulting
+      directory and send them through the pipeline.
+    - Second, it will clean up the temporary directory when it proceeds
+      through the pipeline. The client should send the archive task
+      after sending the rest of the music tasks to make this work.
     """
 
     def __init__(self, toppath):
@@ -960,7 +967,8 @@ class ImportTaskFactory(object):
     def __init__(self, toppath, session):
         self.toppath = toppath
         self.session = session
-        self.skipped = 0
+        self.skipped = 0  # Skipped due to incremental mode.
+        self.imported = 0  # "Real" tasks created.
 
     def tasks(self):
         """Yield all import tasks for music found in the user-specified
@@ -971,12 +979,14 @@ class ImportTaskFactory(object):
                 for path in paths:
                     task = self.singleton(path)
                     if task:
+                        self.imported += 1
                         yield task
                 yield self.sentinel(dirs)
 
             else:
                 task = self.album(paths, dirs)
                 if task:
+                    self.imported += 1
                     yield task
 
     def paths(self):
@@ -1099,9 +1109,7 @@ def read_tasks(session):
             toppath = archive_task.toppath
 
         task_factory = ImportTaskFactory(toppath, session)
-        imported = False
         for t in task_factory.tasks():
-            imported |= not t.skip
             yield t
         skipped += task_factory.skipped
 
@@ -1112,7 +1120,7 @@ def read_tasks(session):
         else:
             yield archive_task
 
-        if not imported:
+        if not task_factory.imported:
             log.warn(u'No files imported from {0}',
                      displayable_path(user_toppath))
 

commit 293e2512edccdf5788e3b0c89e6e3745410ab7b3

Imported count: sensitive to plugin skips
Regression from b7125e434315b00584cb6d6abf8baf5d26fb3bf0 pointed out by
@mried.

beets/importer.py
@@ -1004,16 +1004,14 @@ class ImportTaskFactory(object):
         for dirs, paths in self.paths():
             if self.session.config['singletons']:
                 for path in paths:
-                    task = self.singleton(path)
+                    task = self._create(self.singleton(path))
                     if task:
-                        self.imported += 1
                         yield task
                 yield self.sentinel(dirs)
 
             else:
-                task = self.album(paths, dirs)
+                task = self._create(self.album(paths, dirs))
                 if task:
-                    self.imported += 1
                     yield task
 
         # Produce the final sentinel for this toppath to indicate that
@@ -1024,6 +1022,19 @@ class ImportTaskFactory(object):
             yield archive_task
         else:
             yield self.sentinel()
+
+    def _create(self, task):
+        """Handle a new task to be emitted by the factory.
+
+        Emit the `import_task_created` event and increment the
+        `imported` count if the task is not skipped. Return the same
+        task. If `task` is None, do nothing.
+        """
+        if task:
+            task.emit_created(self.session)
+            if not task.skip:
+                self.imported += 1
+            return task
 
     def paths(self):
         """Walk `self.toppath` and yield `(dirs, files)` pairs where
@@ -1155,7 +1166,6 @@ def read_tasks(session):
         # Generate tasks.
         task_factory = ImportTaskFactory(toppath, session)
         for t in task_factory.tasks():
-            t.emit_created(session)
             yield t
         skipped += task_factory.skipped
 