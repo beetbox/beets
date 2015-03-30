65307b410c99e84f2e4f17ab42a6596f54703815
Heinz Wiesinger <pprkut@liwjatan.at>
2015-03-30 18:15:28 +0200

    Fix issues with unicode and unplayed tracks when syncing data with amarok.

--- beetsplug/psync/amarok.py
+++ beetsplug/psync/amarok.py
@@ -18,6 +18,8 @@
 from os.path import basename
 from datetime import datetime
 from time import mktime
+from beets.util import displayable_path
+from xml.sax.saxutils import escape
 import dbus
 
 
@@ -34,13 +36,15 @@ class Amarok(object):
             dbus.SessionBus().get_object('org.kde.amarok', '/Collection')
 
     def get_data(self, item):
+        path = displayable_path(item.path)
+
         # amarok unfortunately doesn't allow searching for the full path, only
         # for the patch relative to the mount point. But the full path is part
         # of the result set. So query for the filename and then try to match
         # the correct item from the results we get back
-        results = self.collection.Query(self.queryXML % basename(item.path))
+        results = self.collection.Query(self.queryXML % escape(basename(path)))
         for result in results:
-            if result['xesam:url'] != item.path:
+            if result['xesam:url'] != path:
                 continue
 
             item.amarok_rating = result['xesam:userRating']
@@ -49,25 +53,29 @@ class Amarok(object):
             item.amarok_uid = \
                 result['xesam:id'].replace('amarok-sqltrackuid://', '')
 
-            # These dates are stored as timestamps in amarok's db, but
-            # exposed over dbus as fixed integers in the current timezone.
-            first_played = datetime(
-                result['xesam:firstUsed'][0][0],
-                result['xesam:firstUsed'][0][1],
-                result['xesam:firstUsed'][0][2],
-                result['xesam:firstUsed'][1][0],
-                result['xesam:firstUsed'][1][1],
-                result['xesam:firstUsed'][1][2]
-            )
+            if result['xesam:firstUsed'][0][0] != 0:
+                # These dates are stored as timestamps in amarok's db, but
+                # exposed over dbus as fixed integers in the current timezone.
+                first_played = datetime(
+                    result['xesam:firstUsed'][0][0],
+                    result['xesam:firstUsed'][0][1],
+                    result['xesam:firstUsed'][0][2],
+                    result['xesam:firstUsed'][1][0],
+                    result['xesam:firstUsed'][1][1],
+                    result['xesam:firstUsed'][1][2]
+                )
 
-            last_played = datetime(
-                result['xesam:lastUsed'][0][0],
-                result['xesam:lastUsed'][0][1],
-                result['xesam:lastUsed'][0][2],
-                result['xesam:lastUsed'][1][0],
-                result['xesam:lastUsed'][1][1],
-                result['xesam:lastUsed'][1][2]
-            )
+                if result['xesam:lastUsed'][0][0] != 0:
+                    last_played = datetime(
+                        result['xesam:lastUsed'][0][0],
+                        result['xesam:lastUsed'][0][1],
+                        result['xesam:lastUsed'][0][2],
+                        result['xesam:lastUsed'][1][0],
+                        result['xesam:lastUsed'][1][1],
+                        result['xesam:lastUsed'][1][2]
+                    )
+                else:
+                    last_played = first_played
 
-            item.amarok_firstplayed = mktime(first_played.timetuple())
-            item.amarok_lastplayed = mktime(last_played.timetuple())
+                item.amarok_firstplayed = mktime(first_played.timetuple())
+                item.amarok_lastplayed = mktime(last_played.timetuple())
