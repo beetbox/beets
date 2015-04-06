e203dd2ae378acfff2f5d0b61a93ac294c7dfebe
Adrian Sampson <adrian@radbox.org>
2015-04-05 23:26:37 -0500

    Version bump for dev: 1.3.12

--- beets/__init__.py
+++ beets/__init__.py
@@ -17,7 +17,7 @@ from __future__ import absolute_import, unicode_literals
 import beets.library
 from beets.util import confit
 
-__version__ = '1.3.11'
+__version__ = '1.3.12'
 __author__ = 'Adrian Sampson <adrian@radbox.org>'
 
 Library = beets.library.Library
