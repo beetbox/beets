df5dc27d6cc1cf8ca584a97acce8a018aaa9bdac
multikatt <github@kattfest.se>
2015-04-06 17:21:51 -0400

    Adding docstring

--- beetsplug/ipfs.py
+++ beetsplug/ipfs.py
@@ -11,6 +11,8 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
+"""Adds support for ipfs. Requires go-ipfs and a running ipfs daemon
+"""
 from beets import ui
 from beets.plugins import BeetsPlugin
 
