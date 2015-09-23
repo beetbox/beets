168a1645e06e3d646b7a32f7a55a5a539982e813
David Logie <djl@mksh.org>
2015-09-23 14:16:00 +0100

    Move the included file loading out of confit.py.

--- beets/__init__.py
+++ beets/__init__.py
@@ -14,6 +14,8 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import os
+
 import beets.library
 from beets.util import confit
 
@@ -23,3 +25,13 @@ __author__ = 'Adrian Sampson <adrian@radbox.org>'
 Library = beets.library.Library
 
 config = confit.LazyConfig('beets', __name__)
+
+try:
+    included_filenames = config['include'].get(list)
+except confit.NotFoundError:
+    included_filenames = []
+
+for filename in included_filenames:
+    filename = os.path.join(config.config_dir(), filename)
+    if os.path.isfile(filename):
+        config.set_file(filename)
