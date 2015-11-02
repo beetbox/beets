fe61f68f59b286bb412b72ccccc842ebdebdbf5d
jmwatte <jean.marie.winters@gmail.com>
2015-11-02 23:11:42 +0100

    Update yamleditor.py
    polishing

--- beetsplug/yamleditor.py
+++ beetsplug/yamleditor.py
@@ -1,5 +1,5 @@
 # This file is part of beets.
-# Copyright 2015
+# Copyright 2015, Jean-Marie Winters
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
