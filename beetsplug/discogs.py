c6abfb4a0a6e713f426748aa10eceb8ff59edc11
kooimens <leonkooiman@gmail.com>
2015-11-03 20:03:24 +0100

    Discogs: option to change 'various' album artist to 'Various Artists' (Musicbrainz naming)
    Just a simple config option to change 'various' album artist to the one that MusicBrainz uses: Various Artists.

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -57,6 +57,7 @@ class DiscogsPlugin(BeetsPlugin):
             'apisecret': 'plxtUTqoCzwxZpqdPysCwGuBSmZNdZVy',
             'tokenfile': 'discogs_token.json',
             'source_weight': 0.5,
+            'change_va': False,
         })
         self.config['apikey'].redact = True
         self.config['apisecret'].redact = True
@@ -224,6 +225,8 @@ class DiscogsPlugin(BeetsPlugin):
         albumtype = ', '.join(
             result.data['formats'][0].get('descriptions', [])) or None
         va = result.data['artists'][0]['name'].lower() == 'various'
+        if va and self.config['change_va']:
+           artist = 'Various Artists'
         year = result.data['year']
         label = result.data['labels'][0]['name']
         mediums = len(set(t.medium for t in tracks))
