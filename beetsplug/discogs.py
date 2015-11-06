4d656c9abeedede019f80d9face59dd9394b97dc
kooimens <leonkooiman@gmail.com>
2015-11-06 19:27:36 +0100

    Fix style error

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -226,7 +226,7 @@ class DiscogsPlugin(BeetsPlugin):
             result.data['formats'][0].get('descriptions', [])) or None
         va = result.data['artists'][0]['name'].lower() == 'various'
         if va:
-           artist = self.config['va_name'].get()
+            artist = self.config['va_name'].get()
         year = result.data['year']
         label = result.data['labels'][0]['name']
         mediums = len(set(t.medium for t in tracks))
