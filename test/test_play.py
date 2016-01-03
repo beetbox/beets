3807d4fc57be97bb72b24178d508f3f0f172c21b
Jesse Weinstein <jesse@wefu.org>
2016-01-02 21:50:13 -0800

    fix flake8 warnings

--- test/test_play.py
+++ test/test_play.py
@@ -10,8 +10,6 @@ from mock import patch, Mock
 from test._common import unittest
 from test.helper import TestHelper
 
-from beetsplug.play import PlayPlugin
-
 
 class PlayPluginTest(unittest.TestCase, TestHelper):
     def setUp(self):
@@ -26,6 +24,7 @@ class PlayPluginTest(unittest.TestCase, TestHelper):
     @patch('beetsplug.play.util.interactive_open', Mock())
     def test_basic(self):
         self.run_command('play', 'title:aNiceTitle')
+
 
 def suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
