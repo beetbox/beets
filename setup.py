df07d83a6d069740336b5a3f3094655990462382
Adrian Sampson <adrian@radbox.org>
2016-02-07 22:39:32 -0800

    Version bump: 1.3.18

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if 'sdist' in sys.argv:
 
 setup(
     name='beets',
-    version='1.3.17',
+    version='1.3.18',
     description='music tagger and library organizer',
     author='Adrian Sampson',
     author_email='adrian@radbox.org',
