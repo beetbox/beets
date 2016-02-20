fc3d10d2483427e0ab1cb2affeb7edb532982ac3
Peter Kessen <p.kessen@kessen-peter.de>
2016-02-20 13:53:10 +0100

    Removed unicode_literals from plugins
    * fuzzy
    * ihate
    * importadded
    * importfeeds
    * info
    * inline

--- beetsplug/ihate.py
+++ beetsplug/ihate.py
@@ -13,8 +13,7 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-from __future__ import (division, absolute_import, print_function,
-                        unicode_literals)
+from __future__ import (division, absolute_import, print_function)
 
 """Warns you about things you hate (or even blocks import)."""
 
