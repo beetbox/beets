7135fac17bc4fea1f92ae9320527005a26d02abc
Peter Kessen <p.kessen@kessen-peter.de>
2016-02-23 18:10:08 +0100

    Removed unicode_literals from test_importfeeds

--- test/test_importfeeds.py
+++ test/test_importfeeds.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import (division, absolute_import, print_function,
-                        unicode_literals)
+from __future__ import (division, absolute_import, print_function)
 
 import os
 import os.path
