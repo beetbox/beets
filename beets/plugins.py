aa4b6976339b8902b446595a719928aff65c9521
wordofglass <wordofglass@users.noreply.github.com>
2016-04-26 00:14:35 +0200

    Remove untyped except statements

--- beets/plugins.py
+++ beets/plugins.py
@@ -264,7 +264,7 @@ def load_plugins(names=()):
                             and obj != BeetsPlugin and obj not in _classes:
                         _classes.add(obj)
 
-        except:
+        except Exception:
             log.warn(
                 u'** error loading plugin {}:\n{}',
                 name,
