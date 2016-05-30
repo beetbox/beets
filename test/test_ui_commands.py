ee14adb4bdf6342a98303aa8a918932f142a9b5f
Johnny Robeson <johnny@localmomentum.net>
2016-05-30 01:11:37 -0400

    fix the rest of b'__main__':

--- test/test_ui_commands.py
+++ test/test_ui_commands.py
@@ -119,5 +119,5 @@ class FieldsTest(_common.LibTestCase):
 def suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
 
-if __name__ == b'__main__':
+if __name__ == '__main__':
     unittest.main(defaultTest='suite')
