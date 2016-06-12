b147c32b6910f1f455d1554239ec705fba75298b
Dixon Xavier <dixoncx@gmail.com>
2016-06-12 21:26:21 +0530

    Initial code changes for review, issue #185

--- beets/ui/commands.py
+++ beets/ui/commands.py
@@ -824,11 +824,19 @@ class TerminalImportSession(importer.ImportSession):
         log.warn(u"This {0} is already in the library!",
                  (u"album" if task.is_album else u"item"))
 
+        # If set, get the action to follow from config.
+        sel = config['import']['duplicate_action'].as_choice({
+            u'skip': u's',
+            u'keep': u'k',
+            u'remove': u'r',
+            u'ask': None,
+        })
+
         if config['import']['quiet']:
             # In quiet mode, don't prompt -- just skip.
             log.info(u'Skipping.')
             sel = u's'
-        else:
+        elif sel is None:
             # Print some detail about the existing and new items so the
             # user can make an informed decision.
             for duplicate in found_duplicates:
