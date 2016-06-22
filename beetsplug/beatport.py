9789c32a0de8b3c85ae769552d87ca995ac5faee
Johannes Baiter <johannes.baiter@gmail.com>
2016-06-22 22:15:44 +0200

    Another fix for developer clumsiness in beatport plugin

--- beetsplug/beatport.py
+++ beetsplug/beatport.py
@@ -451,6 +451,6 @@ class BeatportPlugin(BeetsPlugin):
     def _get_tracks(self, query):
         """Returns a list of TrackInfo objects for a Beatport query.
         """
-        bp_tracks = self.client.search(query, release_type='track').results
+        bp_tracks = self.client.search(query, release_type='track')
         tracks = [self._get_track_info(x) for x in bp_tracks]
         return tracks
