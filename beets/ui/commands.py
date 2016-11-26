55d9233e39aa903558ebe044174ad9d573c3479b
Adrian Sampson <adrian@radbox.org>
2016-11-25 21:06:01 -0500

    Capitalize Python

--- beets/ui/commands.py
+++ beets/ui/commands.py
@@ -1337,7 +1337,7 @@ default_commands.append(stats_cmd)
 
 def show_version(lib, opts, args):
     print_(u'beets version %s' % beets.__version__)
-    print_(u'python version {}'.format(python_version()))
+    print_(u'Python version {}'.format(python_version()))
     # Show plugins.
     names = sorted(p.name for p in plugins.find_plugins())
     if names:
