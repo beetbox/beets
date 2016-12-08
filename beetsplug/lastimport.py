6d9492207520eb4081525bea40e1ed3b1f42f266
tigranl <t.kostandyan1@gmail.com>
2016-12-08 19:20:18 +0300

    Add SNI_SUPPORTED

--- beetsplug/lastimport.py
+++ beetsplug/lastimport.py
@@ -17,13 +17,13 @@ from __future__ import division, absolute_import, print_function
 
 import pylast
 from pylast import TopItem, _extract, _number
-from beets import ui
+from beets import util
 from beets import dbcore
 from beets import config
 from beets import plugins
 from beets.dbcore import types
 
-if ui.SNI_SUPPORTED >= (2, 7, 9):
+if util.SNI_SUPPORTED:
     API_URL = 'https://ws.audioscrobbler.com/2.0/'
 else:
     API_URL = 'https://ws.audioscrobbler.com/2.0/'
