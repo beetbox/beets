0f8de26dbc8bbafd94e01a0e8d9eeaba4359773d
Johnny Robeson <johnny@localmomentum.net>
2016-12-17 18:55:59 -0500

    Add Python 3 trove classifiers

--- setup.py
+++ setup.py
@@ -135,5 +135,8 @@ setup(
         'Environment :: Web Environment',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
