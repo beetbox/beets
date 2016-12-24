7de97df209cc2c7d2b029d7aa5817562349c2e2a
nathdwek@laptop <nathdwek@ulb.ac.be>
2016-12-24 19:13:34 +0100

    Zero: rewrite tests.
    Rely on plugin interface only.
    Unregister listeners by hand for the moment.

--- test/test_zero.py
+++ test/test_zero.py
@@ -8,7 +8,6 @@ import unittest
 from test.helper import TestHelper
 
 from beets.library import Item
-from beets import config
 from beetsplug.zero import ZeroPlugin
 from beets.mediafile import MediaFile
 from beets.util import syspath
@@ -17,108 +16,96 @@ from beets.util import syspath
 class ZeroPluginTest(unittest.TestCase, TestHelper):
     def setUp(self):
         self.setup_beets()
+        self.config['zero'] = {
+            'fields': [],
+            'keep_fields': [],
+            'update_database': False,
+        }
 
     def tearDown(self):
+        ZeroPlugin.listeners = None
         self.teardown_beets()
         self.unload_plugins()
 
     def test_no_patterns(self):
-        tags = {
-            'comments': u'test comment',
-            'day': 13,
-            'month': 3,
-            'year': 2012,
-        }
-        z = ZeroPlugin()
-        z.debug = False
-        z.fields = ['comments', 'month', 'day']
-        z.patterns = {'comments': [u'.'],
-                      'month': [u'.'],
-                      'day': [u'.']}
-        z.write_event(None, None, tags)
-        self.assertEqual(tags['comments'], None)
-        self.assertEqual(tags['day'], None)
-        self.assertEqual(tags['month'], None)
-        self.assertEqual(tags['year'], 2012)
+        self.config['zero']['fields'] = ['comments', 'month']
 
-    def test_patterns(self):
-        z = ZeroPlugin()
-        z.debug = False
-        z.fields = ['comments', 'year']
-        z.patterns = {'comments': u'eac lame'.split(),
-                      'year': u'2098 2099'.split()}
-
-        tags = {
-            'comments': u'from lame collection, ripped by eac',
-            'year': 2012,
-        }
-        z.write_event(None, None, tags)
-        self.assertEqual(tags['comments'], None)
-        self.assertEqual(tags['year'], 2012)
-
-    def test_delete_replaygain_tag(self):
-        path = self.create_mediafile_fixture()
-        item = Item.from_path(path)
-        item.rg_track_peak = 0.0
+        item = self.add_item_fixture(
+            comments=u'test comment',
+            title=u'Title',
+            month=1,
+            year=2000,
+        )
         item.write()
 
-        mediafile = MediaFile(syspath(item.path))
-        self.assertIsNotNone(mediafile.rg_track_peak)
-        self.assertIsNotNone(mediafile.rg_track_gain)
-
-        config['zero'] = {
-            'fields': ['rg_track_peak', 'rg_track_gain'],
-        }
         self.load_plugins('zero')
-
         item.write()
-        mediafile = MediaFile(syspath(item.path))
-        self.assertIsNone(mediafile.rg_track_peak)
-        self.assertIsNone(mediafile.rg_track_gain)
+
+        mf = MediaFile(syspath(item.path))
+        self.assertIsNone(mf.comments)
+        self.assertIsNone(mf.month)
+        self.assertEqual(mf.title, u'Title')
+        self.assertEqual(mf.year, 2000)
+
+    def test_pattern_match(self):
+        self.config['zero']['fields'] = ['comments']
+        self.config['zero']['comments'] = [u'encoded by']
+
+        item = self.add_item_fixture(comments=u'encoded by encoder')
+        item.write()
+
+        self.load_plugins('zero')
+        item.write()
+
+        mf = MediaFile(syspath(item.path))
+        self.assertIsNone(mf.comments)
+
+    def test_pattern_nomatch(self):
+        self.config['zero']['fields'] = ['comments']
+        self.config['zero']['comments'] = [u'encoded by']
+
+        item = self.add_item_fixture(comments=u'recorded at place')
+        item.write()
+
+        self.load_plugins('zero')
+        item.write()
+
+        mf = MediaFile(syspath(item.path))
+        self.assertEqual(mf.comments, u'recorded at place')
 
     def test_do_not_change_database(self):
+        self.config['zero']['fields'] = ['year']
+
         item = self.add_item_fixture(year=2000)
         item.write()
-        mediafile = MediaFile(syspath(item.path))
-        self.assertEqual(2000, mediafile.year)
 
-        config['zero'] = {'fields': ['year']}
         self.load_plugins('zero')
-
         item.write()
-        mediafile = MediaFile(syspath(item.path))
+
         self.assertEqual(item['year'], 2000)
-        self.assertIsNone(mediafile.year)
 
     def test_change_database(self):
+        self.config['zero']['fields'] = ['year']
+        self.config['zero']['update_database'] = True
+
         item = self.add_item_fixture(year=2000)
         item.write()
-        mediafile = MediaFile(syspath(item.path))
-        self.assertEqual(2000, mediafile.year)
 
-        config['zero'] = {
-            'fields': [u'year'],
-            'update_database': True,
-        }
         self.load_plugins('zero')
-
         item.write()
-        mediafile = MediaFile(syspath(item.path))
+
         self.assertEqual(item['year'], 0)
-        self.assertIsNone(mediafile.year)
 
     def test_album_art(self):
+        self.config['zero']['fields'] = ['images']
+
         path = self.create_mediafile_fixture(images=['jpg'])
         item = Item.from_path(path)
 
-        mediafile = MediaFile(syspath(item.path))
-        self.assertNotEqual(0, len(mediafile.images))
-
-        config['zero'] = {'fields': [u'images']}
         self.load_plugins('zero')
-
         item.write()
-        mediafile = MediaFile(syspath(item.path))
+
+        mediafile = MediaFile(syspath(path))
         self.assertEqual(0, len(mediafile.images))
 
 
