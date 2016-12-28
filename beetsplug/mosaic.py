e2e1ddfed41265b711cfc8ff60e116e1601fd686
Susanna Maria Hepp <susanna.hepp@de.bosch.com>
2016-12-28 22:56:01 +0100

    First working sample

--- beetsplug/mosaic.py
+++ beetsplug/mosaic.py
@@ -24,6 +24,9 @@ from beets.util import syspath, normpath, displayable_path, bytestring_path
 from beets.util.artresizer import ArtResizer
 from beets import config
 from beets import art
+from PIL import Image
+
+import math
 
 class MosaicCoverArtPlugin(BeetsPlugin):
 	col_size = 4
@@ -31,43 +34,68 @@ class MosaicCoverArtPlugin(BeetsPlugin):
 
 	def __init__(self):
 		super(MosaicCoverArtPlugin, self).__init__()
-		self.config.add({'widthcover': 300})
+		self.config.add({'maxwidth': 300})
+		self.maxwidth = self.config['maxwidth'].get(int)
 
  	def commands(self):
 		cmd = ui.Subcommand('mosaic', help=u"create mosaic from coverart")
 
 		def func(lib, opts, args):
-			self._generate_montage(lib, lib.albums(ui.decargs(args)), u'c:\\temp\mos.png')
+			self._generate_montage(lib, lib.albums(ui.decargs(args)), u'mos.png')
 		cmd.func = func
 		return [cmd]
 
 	def _generate_montage(self, lib, albums, output_fn):
+		fullwidth=0;
+		fullheight=0;
+
+		covers = list();
+
 		for album in albums:
+			self._log.info(u'#{}#', album.artpath)
+			
+			if not album.artpath :
+				continue
 
 			if not os.path.exists(album.artpath):
 				continue
-			self._log.info(u'Album: {}-{}', album,album.artpath)
-			image = art.mediafile_image(album.artpath)
 
-	
+			
+			covers.append(album.artpath) 
 
-		#for image in images
-		#	width += image.size[0]+ margin
-		#width = max(image.size[0] + margin for image in images)*row_size
-		#height = sum(image.size[1] + margin for image in images)
-		#montage = Image.new(mode='RGBA', size=(width, height), color=(0,0,0,0))
-		#max_x = 0
-		#max_y = 0
-		#offset_x = 0
-		#offset_y = 0
-		#for i,image in enumerate(images):
-	#		montage.paste(image, (offset_x, offset_y))
-	#		max_x = max(max_x, offset_x + image.size[0])
-	#		max_y = max(max_y, offset_y + image.size[1])
-	#		if i % row_size == row_size-1:
-	#			offset_y = max_y + margin
-	#			offset_x = 0
-	#		else:
-	#			offset_x += margin + image.size[0]
-	#	montage = montage.crop((0, 0, max_x, max_y))
-	#	montage.save(output_fn)
+		sqrtnum = int(math.sqrt(len(covers)))
+
+		tail = len(covers)-(sqrtnum*sqrtnum)
+
+		rows = cols = sqrtnum
+		
+		if tail>0:
+			rows +=1
+
+		self._log.info(u'{}x{}', rows,cols)
+
+		montage = Image.new(mode='RGBA', size=(cols*(100+self.margin), rows*(100+self.margin)), color=(0,0,0,0))
+
+		size = 100, 100
+		offset_x = 0
+		offset_y = 0
+		colcounter=0;
+		for cover in covers:	
+			
+			self._log.info(u'Album: {}', cover)
+			try:	
+				im = Image.open(cover)
+				im.thumbnail(size, Image.ANTIALIAS)
+				montage.paste(im, (offset_x, offset_y))
+				colcounter +=1
+				offset_x += 100+self.margin
+				if colcounter > cols:
+					offset_y += 100+self.margin
+					colcounter =0
+					offset_x =0;
+
+				im.close()
+			except IOError:	
+				self._log.error(u'Problem with {}', cover)
+		self._log.info(u'Save montage to: {}', output_fn)
+		montage.save(output_fn)
