941351792386c9ce4e2301194bc06b9ad6ee1d8c
Susanna Maria Hepp <susanna.hepp@de.bosch.com>
2016-12-29 21:54:44 +0100

    Intermediate commit

--- beetsplug/mosaic.py
+++ beetsplug/mosaic.py
@@ -19,6 +19,7 @@ import os.path
 
 from beets.plugins import BeetsPlugin
 from beets import ui
+from beets import util
 from PIL import Image
 
 import math
@@ -30,30 +31,93 @@ class MosaicCoverArtPlugin(BeetsPlugin):
 
     def __init__(self):
         super(MosaicCoverArtPlugin, self).__init__()
-        self.config.add({'maxwidth': 300})
-        self.maxwidth = self.config['maxwidth'].get(int)
+
+        self.config.add({'geometry': '',
+                         'tile': '',  # or e.g. x3 3x
+                         'label': '',
+                         'background': 'ffffff',
+                         'mosaic': 'mosaic.png',
+                         'show_mosaic': False,
+                         'watermark': '',
+                         'watermark_alpha': 0.4})
 
     def commands(self):
         cmd = ui.Subcommand('mosaic', help=u"create mosaic from coverart")
 
+        cmd.parser.add_option(
+            u'-m', u'--mosaic', dest='mosaic',
+            action='store_false', default=None,
+            help=u'add filename for final mosaic picture - default: mosaic.png'
+        )
+
+        cmd.parser.add_option(
+            u'-w', u'--watermark', dest='watermark',
+            action='store_false', default=None,
+            help=u'add filename for a picture to blend over mosaic'
+        )
+
+        cmd.parser.add_option(
+            u'-a', u'--alpha', dest='watermark_alpha',
+            action='store_false', default=None,
+            help=u'alpha value for blending - default: 0.4'
+        )
+        cmd.parser.add_option(
+            u'-c', u'--color', dest='background',
+            action='store_false', default=None,
+            help=u'background color - default: ffffff'
+        )
+
         def func(lib, opts, args):
+            self.config.set_args(opts)
+
+            if self.config['mosaic']:
+                mosaic = opts.mosaic or self.config['mosaic'].get(str)
+            else:
+                mosaic = opts.mosaic
+
+            self._log.info(u'Mosaic: {}', mosaic)
+
+            if self.config['watermark']:
+                watermark = opts.watermark or self.config['watermark'].get(str)
+            else:
+                watermark = opts.watermark
+
+            watermark_alpha = opts.watermark_alpha or self.config[
+                'watermark_alpha'].get(float)
+
+            if self.config['background']:
+                background = opts.background or self.config[
+                    'background'].get(str)
+            else:
+                background = opts.background
+
+            albums = lib.albums(ui.decargs(args))
+
             self._generate_montage(lib,
-                                   lib.albums(ui.decargs(args)), u'mos.png')
+                                   albums,
+                                   mosaic,
+                                   watermark,
+                                   background,
+                                   watermark_alpha)
 
         cmd.func = func
         return [cmd]
 
-    def _generate_montage(self, lib, albums, output_fn):
+    def _generate_montage(self, lib, albums,
+                          fn_mosaic, fn_watermark,
+                          background, watermark_alpha):
 
         covers = []
+
+        self._log.info(u'Scan available cover art ...')
 
         for album in albums:
 
             if album.artpath and os.path.exists(album.artpath):
-                self._log.info(u'#{}#', album.artpath)
+                self._log.debug(u'{}', album.artpath)
                 covers.append(album.artpath)
             else:
-                self._log.info(u'#{} has no album?#', album)
+                self._log.debug(u'#{} has no album?#', album)
 
         sqrtnum = int(math.sqrt(len(covers)))
 
@@ -66,28 +130,33 @@ class MosaicCoverArtPlugin(BeetsPlugin):
 
         self._log.info(u'{}x{}', cols, rows)
 
-        montage = Image.new(mode='RGB',
-                            size=(cols * (100 + self.margin),
-                                  rows * (100 + self.margin)),
-                            color=(0, 0, 0, 0))
+        mosaic_size_width = self.margin + (cols * (100 + self.margin))
+        mosaic_size_height = self.margin + (rows * (100 + self.margin))
+
+        self._log.info(u'Mosaic size: {}x{}',
+                       mosaic_size_width, mosaic_size_height)
+
+        montage = Image.new('RGB', (mosaic_size_width, mosaic_size_height),
+                            tuple(int(background[i:i + 2], 16)
+                                  for i in (0, 2, 4)))
 
         size = 100, 100
-        offset_x = 0
-        offset_y = 0
+        offset_x = self.margin
+        offset_y = self.margin
         colcounter = 0
         for cover in covers:
 
             try:
                 im = Image.open(cover)
                 im.thumbnail(size, Image.ANTIALIAS)
-                self._log.info(u'Paste into mosaic: {} - {}x{}',
-                               cover, offset_x, offset_y)
+                self._log.debug(u'Paste into mosaic: {} - {}x{}',
+                                cover, offset_x, offset_y)
                 montage.paste(im, (offset_x, offset_y))
 
                 colcounter += 1
                 if colcounter >= cols:
                     offset_y += 100 + self.margin
-                    offset_x = 0
+                    offset_x = self.margin
                     colcounter = 0
                 else:
                     offset_x += 100 + self.margin
@@ -95,44 +164,45 @@ class MosaicCoverArtPlugin(BeetsPlugin):
                     im.close()
             except IOError:
                 self._log.error(u'Problem with {}', cover)
-        self._log.info(u'Save montage to: {}', output_fn)
-        foreground = Image.open("c:/temp/tool.png")
-        m_width, m_height = montage.size
-        f_width, f_height = foreground.size
 
-        if f_width > f_height:
-            d = f_width / 2 - (f_height / 2)
-            e = f_width / 2 + (f_height / 2)
-            box = (d, 0, e, f_height)
-            nf = foreground.crop(box)
-        elif f_width < f_height:
-            d = f_height / 2 - (f_width / 2)
-            e = f_height / 2 + (f_width / 2)
-            box = (0, d, f_width, e)
-            nf = foreground.crop(box)
-        else:
-            nf = foreground
+        if fn_watermark:
+            foreground = Image.open(fn_watermark)
+            m_width, m_height = montage.size
+            f_width, f_height = foreground.size
 
-        longer_side = max(montage.size)
-        horizontal_padding = (longer_side - montage.size[0]) / 2
-        vertical_padding = (longer_side - montage.size[1]) / 2
-        img5 = montage.crop(
-            (
-                -horizontal_padding,
-                -vertical_padding,
-                montage.size[0] + horizontal_padding,
-                montage.size[1] + vertical_padding
+            if f_width > f_height:
+                d = f_width / 2 - (f_height / 2)
+                e = f_width / 2 + (f_height / 2)
+                box = (d, 0, e, f_height)
+                nf = foreground.crop(box)
+            elif f_width < f_height:
+                d = f_height / 2 - (f_width / 2)
+                e = f_height / 2 + (f_width / 2)
+                box = (0, d, f_width, e)
+                nf = foreground.crop(box)
+            else:
+                nf = foreground
+
+            longer_side = max(montage.size)
+            horizontal_padding = (longer_side - montage.size[0]) / 2
+            vertical_padding = (longer_side - montage.size[1]) / 2
+            img5 = montage.crop(
+                (
+                    -horizontal_padding,
+                    -vertical_padding,
+                    montage.size[0] + horizontal_padding,
+                    montage.size[1] + vertical_padding
+                )
             )
-        )
 
-        m_width, m_height = img5.size
+            m_width, m_height = img5.size
 
-        nf2 = nf.resize(img5.size, Image.ANTIALIAS)
-        f_width, f_height = nf2.size
+            nf2 = nf.resize(img5.size, Image.ANTIALIAS)
+            f_width, f_height = nf2.size
 
-        self._log.info(u'Save montage to: {}:{}-{} {}:{}-{}',
-                       img5.mode, m_width, m_height, nf.mode,
-                       f_width, f_height)
-        Image.blend(img5, nf2, 0.4).save(output_fn)
-
-        # montage.save(output_fn)
+            self._log.debug(u'Save montage to: {}:{}-{} {}:{}-{}',
+                            img5.mode, m_width, m_height, nf.mode,
+                            f_width, f_height)
+            Image.blend(img5, nf2, watermark_alpha).save(fn_mosaic)
+        else:
+            montage.save(fn_mosaic)
