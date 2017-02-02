71984806cc6c0849fc84f38c867567ee5ddc6000
tigranl <t.kostandyan1@gmail.com>
2017-02-03 00:58:32 +0300

    Recover deleted commit

--- beetsplug/web_tagger.py
+++ beetsplug/web_tagger.py
@@ -12,57 +12,57 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-""" Runs local http server for MusicBrainz web tagger and handles users' requests """
+"""Runs a local HTTP server for MusicBrainz web tagger and handles
+users' requests.
+"""
 
 from __future__ import division, absolute_import, print_function
 
-import threading
 import socket
 import webbrowser
-try:
-    import urllib.parse as urlparse
-    from urllib.parse import urlencode
-except ImportError: # python 2 support
-    import urlparse
-    from urlparse import urlparse
+from io import BytesIO
+from six.moves.urllib_parse import urlparse, urlencode, parse_qs
+from six.moves.BaseHTTPServer import BaseHTTPRequestHandler
+from beets import autotag
 from beets.plugins import BeetsPlugin
 from beets.ui.commands import PromptChoice
+from beets import config
 from beets import ui
-from beets import autotag
 
-PORT = 8000
+PORT = config['mbweb']['port'].as_number()
 
 
+# Simple HTTP parser
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = BytesIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.parse_request()
+
+
+# Get 'id' parameter from URI
 def parse(data):
-    data = data.splitlines()
-    s = data[0].decode()
-    url = ''
-    for char in s[5:]:
-        if char != ' ':
-            url += char
-        else:
-            break
-    parsed = urlparse.urlparse(url)
-    return str(urlparse.parse_qs(parsed.query)['id'][0])
+    r = HTTPRequest(data)
+    parsed = urlparse(r.path)
+    return parse_qs(parsed.query)['id'][0]
 
 
-class Server(threading.Thread):
+class Server(object):
     def __init__(self):
-        threading.Thread.__init__(self)
-        self.daemon = True
         self.host = '127.0.0.1'
         self.port = PORT
         try:  # Start TCP socket, catch soket.error
-            self.run_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.run_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.run_server.bind((self.host, self.port))
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.setsockopt(socket.SOL_SOCKET,
+                                       socket.SO_REUSEADDR, 1)
+            self.sock.bind((self.host, self.port))
         except socket.error as error_msg:
             print("Error occurred: {0}".format(error_msg))
 
     def listen(self, size=1024):
-        self.run_server.listen(5)
+        self.sock.listen(1)
         while True:
-            con, addr = self.run_server.accept()
+            con, addr = self.sock.accept()
             while True:
                 data = con.recv(size)
                 con.send(data)
@@ -77,31 +77,31 @@ class MBWeb(BeetsPlugin):
         self.port = PORT
         self.register_listener('before_choose_candidate', self.prompt)
         self.server = Server()
-        self.server.start()
-        self.server.join()
 
     def prompt(self, session, task):
         return [PromptChoice('l', 'Lookup', self.choice)]
 
     def choice(self, session, task):
-        artist = ui.input_('Artist:')
-        realise = ui.input_('Album:')
-        track = ui.input_('Track:')
-        if not (artist, realise, track):
-            ui.print_('Please, fill the search query')
-            return self.prompt
-        else:
-            query = {'tport': self.port,
-                     'artist': artist,
-                     'track': track,
-                     'realise': realise,
-                     }
-            url = 'http://musicbrainz.org/taglookup?{0}'.format(urlencode(query))
-            ui.print_("Choose your tracks and click 'tagger' button to add:")
-            webbrowser.open(url)
+        url = 'http://musicbrainz.org/taglookup?{0}'
+        query = {'tport': self.port,
+                 'artist': '',
+                 'track': '',
+                 'release': '',
+                 }
+        prompt = u"Choose your {0} and click 'tagger' button to add." \
+        .format(u'album' if task.is_album else u'track')
+        ui.print_(prompt)
+        if task.is_album:
+            query['artist'] = task.cur_artist
+            query['release'] = task.cur_album
+            webbrowser.open(url.format(urlencode(query)))
             id_choice = self.server.listen()
-            if task.is_album:
-                _, _, proposal, _ = autotag.tag_album(task.items, search_ids=id_choice)
-                return proposal
-            else:
-                return autotag.tag_item(task.item, search_ids=id_choice)
+            _, _, proposal = autotag.tag_album(task.items,
+                                               search_ids=id_choice)
+            return proposal
+        else:
+            query['artist'] = task.item.artist
+            query['track'] = task.item.title
+            webbrowser.open(url.format(urlencode(query)))
+            id_choice = self.server.listen()
+            return autotag.tag_item(task.item, search_ids=id_choice)
