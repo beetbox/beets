f315a17bb21e4c8eb57f8de5fc5f47557d3ad99b
Pauligrinder <pauligrinder@gmail.com>
2017-03-01 10:39:40 +0200

    Added the copyright header
    Also added config['kodi']['pwd'].redact = True as suggested.

--- beetsplug/kodiupdate.py
+++ beetsplug/kodiupdate.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+# This file is part of beets.
+# Copyright 2017, Pauli Kettunen.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
 
 """Updates a Kodi library whenever the beets library is changed. This is based on the Plex Update plugin.
 
@@ -39,6 +52,7 @@ class KodiUpdate(BeetsPlugin):
             u'user': u'kodi',
             u'pwd': u'kodi'})
 
+        config['kodi']['pwd'].redact = True
         self.register_listener('database_change', self.listen_for_db_change)
 
     def listen_for_db_change(self, lib, model):
