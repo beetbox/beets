cd292fa11e96f017228b144ea98f7299117f74c2
dosoe <soergeldorian@gmail.com>
2017-06-03 13:14:52 +0200

    debug message

--- beetsplug/parentwork.py
+++ beetsplug/parentwork.py
@@ -139,7 +139,8 @@ class ParentWorkPlugin(BeetsPlugin):
                     i=i+1
                 except musicbrainzngs.musicbrainz.ResponseError: 
                     i=i+1
-                
+                if i=5
+                    print('Wokr unreachable')
             print(parent_composer_sort)
             item['parent_work']          = u', '.join(parent_work)
             item['parent_work_disambig'] = u', '.join(parent_work_disambig)
