ebc577443e8c3ade07af6da97c96b40ccb108ee9
robot3498712 <robot3498712@void.null>
2017-06-15 13:27:28 +0200

    fixed failing test - line too long

--- beetsplug/web/__init__.py
+++ beetsplug/web/__init__.py
@@ -217,7 +217,8 @@ def all_items():
 @app.route('/item/<int:item_id>/file')
 def item_file(item_id):
     item = g.lib.get_item(item_id)
-    item_path = util.syspath(item.path) if os.name == 'nt' else util.py3_path(item.path)
+    item_path = util.syspath(item.path) if (os.name == 'nt') else (
+        util.py3_path(item.path))
     response = flask.send_file(
         item_path,
         as_attachment=True,
