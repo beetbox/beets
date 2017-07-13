4d212d604f683741edcdd878141ba9f1f960a3d9
dosoe <soergeldorian@gmail.com>
2017-07-14 00:16:06 +0200

    better logging

--- beetsplug/parentwork.py
+++ beetsplug/parentwork.py
@@ -204,7 +204,7 @@ class ParentWorkPlugin(BeetsPlugin):
         if found:
             self._log.info("Work found for: " + item.artist + ' - ' +
                            item.title)
-            self._log.info("Work fetched: " + u', '.join(parent_work) +
+            self._log.info("Work fetched: " + u', '.join(parent_work) + ' - '
                            u', '.join(parent_composer))
             item['parent_work']          = u', '.join(parent_work)
             item['parent_work_disambig'] = u', '.join(parent_work_disambig)
