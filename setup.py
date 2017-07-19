df851db58b6d378492e9c4e68aa9288d10f8e977
mmmmmaster <1343451123@qq.com>
2017-07-19 17:58:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@
 # Copyright 2016, Adrian Sampson.
 #
 #
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
