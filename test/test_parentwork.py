c8d362f49ef13e70ccc3aeb28e560cb65837ef8d
dosoe <soergeldorian@gmail.com>
2017-08-03 19:01:31 +0200

    Copyright

--- test/test_parentwork.py
+++ test/test_parentwork.py
@@ -13,7 +13,7 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-"""Tests for the 'ftintitle' plugin."""
+"""Tests for the 'parentwork' plugin."""
 
 from __future__ import division, absolute_import, print_function
 
