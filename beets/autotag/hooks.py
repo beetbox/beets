4914ee1c678bc1e48901b69261206503995a8f81
dosoe <soergeldorian@gmail.com>
2017-08-07 16:23:31 +0200

    typo

--- beets/autotag/hooks.py
+++ beets/autotag/hooks.py
@@ -174,8 +174,8 @@ class TrackInfo(dict):
         return self.get(attr)
         # work around for pickle bug in Python 3.4
         # see http://bugs.python.org/issue16251
-        if name == "__getnewargs_ex__":
-            raise AttributeError("%r has no attribute %r" % (type(self), name))
+        if attr == "__getnewargs_ex__":
+            raise AttributeError("%r has no attribute %r" % (type(self), attr))
 
     def __setattr__(self, key, value):
         self.__setitem__(key, value)
