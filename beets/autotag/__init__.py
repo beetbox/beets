be7fe866fe514857cdcee974bdc0c4ab36b9b361
gabrielhdt <gabrielhondet@gmail.com>
2017-08-13 19:34:00 +0200

    Apply_metadata tidied up
    On the way to become generic. Used dictionnaries to map item attributes
    to ItemInfo keys

--- beets/autotag/__init__.py
+++ beets/autotag/__init__.py
@@ -65,29 +65,66 @@ def apply_metadata(album_info, mapping):
     :param album_info: object to match
     :type album_info: :py:class:`hooks.AlbumInfo`
     """
+    # Maps item attribute to AlbumInfo key
+    itemattr_to_albuminfokey = {
+        'albumartist_sort': 'artist_sort',
+        'albumartist_credit': 'artist_credit',
+        'disctotal': 'mediums',
+        'mb_albumid': 'album_id',
+        'mb_albumartistid': 'artist_id',
+        'mb_releasegroupid': 'releasegroup_id',
+        'comp': 'va',  # Compilation flag
+        'albumtype': 'albumtype',
+        'label': 'label',
+        'asin': 'asin',
+        'catalognum': 'catalognum',
+        'script': 'script',
+        'language': 'language',
+        'country': 'country',
+        'albumstatus': 'albumstatus',
+        'albumdisambig': 'albumdisambig',
+        'data_source': 'data_source',
+        'album': 'album',
+        'albumartist': 'artist',
+    }
+    # Maps item attributes to the corresponding TrackInfo key
+    itemattr_to_trackinfokey = {
+        'title': 'title',
+        'disc': 'medium',
+        'mb_trackid': 'track_id',
+        'track_alt': 'track_alt',
+        'lyricist': 'lyricist',
+        'composer': 'composer',
+        'composer_sort': 'composer_sort',
+        'arranger': 'arranger',
+        'media': 'media',
+        'disctitle': 'disctitle',
+    }
+    # Maps the item attribute to the corresponding key to be tried
+    # first on TrackInfo and then on AlbumInfo
+    itemattr_to_torainfokey = {
+        'artist': 'artist',
+        'artist_sort': 'artist_sort',
+        'artist_credit': 'artist_credit',
+        'mb_artistid': 'artist_id',
+    }
     for item, track_info in mapping.items():
-        # Album, artist, track count.
         def try_track_info(attr):
             """Tries to set attr to the one of ti, else uses ai"""
             return (track_info[attr] if attr in track_info.keys()
                     else album_info[attr] if attr in album_info.keys()
                     else None)
 
-        setattr(item, 'artist', try_track_info('artist'))
+        # Info taken from AlbumInfo
+        for attr, key in itemattr_to_albuminfokey.items():
+            setattr(item, attr, album_info[key])
 
-        setattr(item, 'albumartist', (album_info['artist']
-                                      if 'artist' in album_info.keys()
-                                      else None))
+        # Info taken from TrackInfo
+        for attr, key in itemattr_to_trackinfokey.items():
+            setattr(item, attr, track_info[key])
 
-        setattr(item, 'album', (album_info['album']
-                                if 'album' in album_info.keys()
-                                else None))
-
-        # Artist sort and credit names.
-        setattr(item, 'artist_sort', try_track_info('artist_sort'))
-        setattr(item, 'artist_credit', try_track_info('artist_credit'))
-        setattr(item, 'albumartist_sort', album_info['artist_sort'])
-        setattr(item, 'albumartist_credit', album_info['artist_credit'])
+        for attr, key in itemattr_to_torainfokey.items():
+            setattr(item, attr, try_track_info(key))
 
         # Release date.
         for prefix in '', 'original_':
@@ -113,9 +150,6 @@ def apply_metadata(album_info, mapping):
                 if config['original_date']:
                     item[suffix] = value
 
-        # Title. Required field, no need of fallback None
-        setattr(item, 'title', track_info['title'])
-
         if config['per_disc_numbering']:
             # We want to let the track number be zero, but if the medium index
             # is not provided we need to fall back to the overall index.
@@ -131,43 +165,3 @@ def apply_metadata(album_info, mapping):
         else:
             setattr(item, 'track', track_info['index'])
             setattr(item, 'tracktotal', len(album_info['tracks']))
-
-        # Disc and disc count.
-        setattr(item, 'disc', track_info['medium'])
-        setattr(item, 'disctotal', album_info['mediums'])
-
-        # MusicBrainz IDs.
-        setattr(item, 'mb_trackid', (track_info['track_id']
-                                     if 'track_id' in track_info.keys()
-                                     else None))
-        setattr(item, 'mb_albumid', (album_info['album_id']
-                                     if 'album_id' in album_info.keys()
-                                     else None))
-        setattr(item, 'mb_artistid', try_track_info('artist_id'))
-        setattr(item, 'mb_albumartistid', (album_info['artist_id']
-                                           if 'artist_id' in album_info.keys()
-                                           else None))
-        setattr(item, 'mb_releasegroupid', (album_info['releasegroup_id']
-                                            if 'releasegroup_id'
-                                            in album_info.keys()
-                                            else None))
-
-        # Compilation flag.
-        setattr(item, 'comp', (album_info['va'] if 'va' in album_info.keys()
-                               else None))
-
-        # Miscellaneous metadata.
-        for field in ('albumtype', 'label', 'asin', 'catalognum', 'script',
-                      'language', 'country', 'albumstatus', 'albumdisambig',
-                      'data_source',):
-            if field in album_info.keys():
-                setattr(item, field, album_info[field])
-
-        for field in ('lyricist', 'composer', 'composer_sort', 'arranger',
-                      'media', 'disctitle'):
-            if field in track_info.keys():
-                setattr(item, field, track_info[field])
-
-        setattr(item, 'track_alt', (track_info['track_alt']
-                                    if 'track_alt' in track_info.keys()
-                                    else None))
