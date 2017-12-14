d63102e8af451834ad04bfba9091d567b160d9c2
Dorian SOERGEL <soergeldorian@gmail.com>
2017-12-14 15:25:10 +0100

    simplifying the test

--- test/test_parentwork.py
+++ test/test_parentwork.py
@@ -45,86 +45,14 @@ class ParentWorkPluginFunctional(unittest.TestCase, TestHelper):
 
     def _pw_set_config(self, force):
         self.config['parentwork']['force'] = force
-
-    def test_normal_case(self):
-        item = self._pw_add_item(artist=u'Johann Sebastian Bach',
-                                 title=u'Matthäus-Passion Part I Ouverture',
-                                 work_id=u'2e4a3668-458d-\
-                                 3b2a-8be2-0b08e0d8243a')
-        self.run_command('parentwork', '-f')
-        item.load()
-        self.assertEqual(item['parent_work'], u'Matthäus-Passion, BWV 244')
-        self.assertEqual(item['parent_work_disambig'], u'')
-        self.assertEqual(item['parent_composer'], u'Johann Sebastian Bach')
-        self.assertEqual(item['parent_composer_sort'],
-                         u'Bach, Johann Sebastian')
-        self.assertEqual(item['parent_work_id'],
-                         u'45afb3b2-18ac-4187-bc72-beb1b1c194ba')
-
-    def test_several_composers_disambig(self):
-        item = self._pw_add_item(artist=u'Mozart',
-                                 title=u'Requiem I. Introitus',
-                                 work_id=u'e27bda6e-531e-\
-                                 36d3-9cd7-b8ebc18e8c53')
-        self.run_command('parentwork', '-f')
-        item.load()
-        self.assertEqual(item['parent_work'], u'Requiem in D minor, K. 626')
-        self.assertEqual(item['parent_work_disambig'], u'Süßmayr Edition')
-        self.assertEqual(item['parent_composer'],
-                         u'Wolfgang Amadeus Mozart, Franz Xaver Süßmayr')
-        self.assertEqual(item['parent_composer_sort'],
-                         u'Mozart, Wolfgang Amadeus, Süßmayr, Franz Xaver')
-        self.assertEqual(item['parent_work_id'],
-                         u'32c8943f-1b27-3a23-8660-4567f4847c94')
-
-    def test_force_in_config(self):
-        self._pw_set_config('yes')
-        item = self._pw_add_item(artist=u'Mozart',
-                                 title=u'Requiem I. Introitus',
-                                 work_id=u'e27bda6e-531e-36d3-\
-                                 9cd7-b8ebc18e8c53')
-        self.run_command('parentwork', '-f')
-        item.load()
-        self.assertEqual(item['parent_work'], u'Requiem in D minor, K. 626')
-        self.assertEqual(item['parent_work_disambig'], u'Süßmayr Edition')
-        self.assertEqual(item['parent_composer'],
-                         u'Wolfgang Amadeus Mozart, Franz Xaver Süßmayr')
-        self.assertEqual(item['parent_composer_sort'],
-                         u'Mozart, Wolfgang Amadeus, Süßmayr, Franz Xaver')
-        self.assertEqual(item['parent_work_id'],
-                         u'32c8943f-1b27-3a23-8660-4567f4847c94')
-
-        self._pw_set_config('no')
-        item = self._pw_add_item(artist=u'Mozart',
-                                 title=u'Requiem II. Kyrie',
-                                 work_id=u'6eaede01-c31a-3402\
-                                 -bedb-598e6bcbad03',
-                                 parent_work=u'Requiem in D minor, K. 626',
-                                 parent_work_disambig=u'Süßmayr Edition',
-                                 parent_composer=u'Wolfgang Amadeus Mozart, \
-                                 Franz Xaver Süßmayr',
-                                 parent_work_id=u'32c8943f-1b27-3a23-\
-                                 8660-4567f4847c94')
-        self.run_command('parentwork', '-f')
-        item.load()
-        # nothing should change
-        self.assertEqual(item['parent_work'], u'Requiem in D minor, K. 626')
-        self.assertEqual(item['parent_work_disambig'], u'Süßmayr Edition')
-        self.assertEqual(item['parent_composer'],
-                         u'Wolfgang Amadeus Mozart, Franz Xaver Süßmayr')
-        self.assertEqual(item['parent_composer_sort'],
-                         u'Mozart, Wolfgang Amadeus, Süßmayr, Franz Xaver')
-        self.assertEqual(item['parent_work_id'],
-                         u'32c8943f-1b27-3a23-8660-4567f4847c94')
-
-    # test individual functions, still with Matthew Passion Ouverture
-
+        
     def test_father_work(self):
         work_id = u'2e4a3668-458d-3b2a-8be2-0b08e0d8243a'
         self.assertEqual(u'f04b42df-7251-4d86-a5ee-67cfa49580d1',
                          parentwork.work_father(work_id))
         self.assertEqual(u'45afb3b2-18ac-4187-bc72-beb1b1c194ba',
                          parentwork.work_parent(work_id))
+
 
 def suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
