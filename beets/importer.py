5276492ed60208e055d6b94c31e699201f38e6f8
Michael Yoo <michael@yoo.id.au>
2018-01-13 01:46:28 +1030

    Fix incremental_skip_later bug from #2782

--- beets/importer.py
+++ beets/importer.py
@@ -577,8 +577,10 @@ class ImportTask(BaseImportTask):
         # Update progress.
         if session.want_resume:
             self.save_progress()
-        if (session.config['incremental'] and
-                not session.config['incremental_skip_later']):
+        if session.config['incremental'] and not (
+            # Should we skip recording to incremental list?
+            self.skip and session.config['incremental_skip_later']
+        ):
             self.save_history()
 
         self.cleanup(copy=session.config['copy'],
