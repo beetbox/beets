c303ac33bf86d32ec01861c70f17011517eb4363
Dorian Soergel <soergeldorian@gmail.com>
2018-08-26 22:25:46 +0200

    test for failing test

--- test/test_parentwork.py
+++ test/test_parentwork.py
@@ -48,41 +48,7 @@ class ParentWorkTest(unittest.TestCase, TestHelper):
         self.assertEqual(item['parent_work_id'],
                          u'32c8943f-1b27-3a23-8660-4567f4847c94')
 
-    def test_force(self, command_output):
-        self.config['parentwork']['force'] = True
-        item = Item(path='/file',
-                    work_id=u'e27bda6e-531e-36d3-9cd7-b8ebc18e8c53',
-                    parent_work_id=u'XXX')
-        item.add(self.lib)
-
-        command_output.return_value = u'32c8943f-1b27-3a23-8660-4567f4847c94'
-        self.run_command('parentwork')
-
-        item.load()
-        self.assertEqual(item['parent_work_id'],
-                         u'32c8943f-1b27-3a23-8660-4567f4847c94')
-
-    def test_no_force(self, command_output):
-        self.config['parentwork']['force'] = True
-        item = Item(path='/file', work_id=u'e27bda6e-531e-36d3-9cd7-\
-                    b8ebc18e8c53', parent_work_id=u'XXX')
-        item.add(self.lib)
-
-        command_output.return_value = u'32c8943f-1b27-3a23-8660-4567f4847c94'
-        self.run_command('parentwork')
-
-        item.load()
-        self.assertEqual(item['parent_work_id'], u'XXX')
-
-    # test different cases, still with Matthew Passion Ouverture or Mozart
-    # requiem
-
-    def test_father_work(self, command_output):
-        work_id = u'2e4a3668-458d-3b2a-8be2-0b08e0d8243a'
-        self.assertEqual(u'f04b42df-7251-4d86-a5ee-67cfa49580d1',
-                         parentwork.work_father(work_id)[0])
-        self.assertEqual(u'45afb3b2-18ac-4187-bc72-beb1b1c194ba',
-                         parentwork.work_parent(work_id)[0])
+    
 
 
 def suite():
