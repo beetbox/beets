338541c066fd385915e19466ff9f349f92e51230
Frederik “Freso” S. Olesen <freso.dk@gmail.com>
2018-10-30 09:33:35 +0100

    Add Python 3.7 to classifiers in setup.py
    beets runs perfectly well under 3.7 on Arch Linux and we're also doing
    automated testing/continuous integration for Python 3.7 in Travis.

--- setup.py
+++ setup.py
@@ -142,6 +142,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
