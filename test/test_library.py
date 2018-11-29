ebf754cd2fab3f50c183a8227f47eee1260ad6a4
Guilherme Danno <guilherme.danno@gmail.com>
2018-11-29 10:52:20 -0200

    Add tests to titlecase (#3033)

--- test/test_library.py
+++ test/test_library.py
@@ -589,6 +589,10 @@ class DestinationFunctionTest(_common.TestCase, PathFormattingMixin):
         self._setf(u'%title{$title}')
         self._assert_dest(b'/base/The Title')
 
+    def test_title_case_variable(self):
+        self._setf(u'%title{I can\'t}')
+        self._assert_dest(b'/base/I Can\'t')
+
     def test_asciify_variable(self):
         self._setf(u'%asciify{ab\xa2\xbdd}')
         self._assert_dest(b'/base/abC_ 1_2 d')
