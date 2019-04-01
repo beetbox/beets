36c85a8aebe31e721cd4155a3445040ef9f52d50
Carl Suster <carl@contraflo.ws>
2019-04-02 10:10:59 +1100

    Fix beets.util.inspect for Python 3
    Under the original compatibility shim we weren't correctly inclusing
    `self` in the argument list for bound methods.

--- beets/util/inspect.py
+++ beets/util/inspect.py
@@ -16,35 +16,11 @@
 from __future__ import division, absolute_import, print_function
 
 import inspect
-from collections import namedtuple
 
 from six import PY2
-
-
-ArgSpec = namedtuple('ArgSpec', 'args varargs keywords defaults')
 
 
 def getargspec(func):
     if PY2:
         return inspect.getargspec(func)
-
-    sig = inspect.signature(func)
-    args = [
-        p.name for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
-    ]
-    varargs = [
-        p.name for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.VAR_POSITIONAL
-    ]
-    varargs = varargs[0] if varargs else None
-    varkw = [
-        p.name for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.VAR_KEYWORD
-    ]
-    varkw = varkw[0] if varkw else None
-    defaults = tuple(p.default for p in sig.parameters.values()
-                     if p.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
-                     and p.default is not p.empty) or None
-
-    return ArgSpec(args, varargs, varkw, defaults)
+    return inspect.getfullargspec(func)
