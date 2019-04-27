941dd6e48f989ab1da2773f1a71e7bab5b34a70e
Rainer Hihn <hihn.rainer@gmail.com>
2019-04-27 17:58:26 +0200

    Formatting

--- beetsplug/acousticbrainz.py
+++ beetsplug/acousticbrainz.py
@@ -211,8 +211,7 @@ class AcousticPlugin(plugins.BeetsPlugin):
             if data:
                 for attr, val in self._map_data_to_scheme(data, ABSCHEME):
                     if not tags or attr in tags:
-                        self._log.debug(
-                            u'attribute {} of {} set to {}',
+                        self._log.debug(u'attribute {} of {} set to {}',
                             attr,
                             item,
                             val)
