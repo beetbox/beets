f9d27cb77e0641818804cd96ef3eac0254dec953
Adrian Sampson <adrian@radbox.org>
2019-05-16 20:44:38 -0400

    Version bump: 1.4.9
    Given what we have in the pipeline, we may need to change this to 1.5.0
    instead...

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if 'sdist' in sys.argv:
 
 setup(
     name='beets',
-    version='1.4.8',
+    version='1.4.9',
     description='music tagger and library organizer',
     author='Adrian Sampson',
     author_email='adrian@radbox.org',
