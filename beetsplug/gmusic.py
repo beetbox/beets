d7c14d5ef16d3b63ca92587f2e8ce204bdea277c
Adrian Sampson <adrian@radbox.org>
2019-05-18 16:32:56 -0400

    gmusic: Fix compatibility with gmusicapi>=12.0.0
    Fixes #3270.

--- beetsplug/gmusic.py
+++ beetsplug/gmusic.py
@@ -31,12 +31,19 @@ class Gmusic(BeetsPlugin):
     def __init__(self):
         super(Gmusic, self).__init__()
         self.m = Musicmanager()
+
+        # OAUTH_FILEPATH was moved in gmusicapi 12.0.0.
+        if hasattr(Musicmanager, 'OAUTH_FILEPATH'):
+            oauth_file = Musicmanager.OAUTH_FILEPATH
+        else:
+            oauth_file = gmusicapi.clients.OAUTH_FILEPATH
+
         self.config.add({
             u'auto': False,
             u'uploader_id': '',
             u'uploader_name': '',
             u'device_id': '',
-            u'oauth_file': gmusicapi.clients.OAUTH_FILEPATH,
+            u'oauth_file': oauth_file,
         })
         if self.config['auto']:
             self.import_stages = [self.autoupload]
