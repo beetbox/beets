5fe92730daf492c4ad6ae560e83be56f10fe5319
Dorian Soergel <soergeldorian@gmail.com>
2019-05-26 00:40:13 +0200

    completed autotag/__int__

--- beets/autotag/__init__.py
+++ beets/autotag/__init__.py
@@ -54,6 +54,12 @@ def apply_item_metadata(item, track_info):
         item.composer_sort = track_info.composer_sort
     if track_info.arranger is not None:
         item.arranger = track_info.arranger
+    if track_info.work is not None:
+        item.work = track_info.work
+    if track_info.work_id is not None:
+        item.work_id = track_info.work_id
+    if track_info.work_disambig is not None:
+        item.work_disambig = track_info.work_disambig
 
     # At the moment, the other metadata is left intact (including album
     # and track number). Perhaps these should be emptied?
