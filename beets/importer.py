ba4430c0bf6f1e30bfb2698972f863ee290b080d
Chris <>
2019-08-22 19:27:23 +0100

    Simplified conditional test, now w/o function call

--- beets/importer.py
+++ beets/importer.py
@@ -754,7 +754,7 @@ class ImportTask(BaseImportTask):
             self.record_replaced(lib)
             self.remove_replaced(lib)
             self.album = lib.add_album(self.imported_items())
-            if 'data_source' in self.imported_items()[0].keys():
+            if 'data_source' in self.imported_items()[0]:
                 self.album.data_source = self.imported_items()[0].data_source
             self.reimport_metadata(lib)
 
