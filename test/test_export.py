855b988d4c6094d12c5ac49c7136c3b0b1af7d8d
Austin Marino <austinmarino@Austins-MacBook-Pro.local>
2019-10-15 11:45:38 -0700

    Updated Test structure

--- test/test_export.py
+++ test/test_export.py
@@ -20,7 +20,10 @@ from __future__ import division, absolute_import, print_function
 
 import unittest
 from test.helper import TestHelper
-import re
+import re  # used to test csv format
+import json
+from xml.etree.ElementTree import Element
+import xml.etree.ElementTree as ET
 
 
 class ExportPluginTest(unittest.TestCase, TestHelper):
@@ -34,10 +37,11 @@ class ExportPluginTest(unittest.TestCase, TestHelper):
         self.teardown_beets()
 
     def execute_command(self, format_type, artist):
+        query = ','.format(list(self.test_values.keys()))
         actual = self.run_with_output(
             'export',
             '-f', format_type,
-            '-i', 'album,title',
+            '-i', query,
             artist
         )
         return re.sub("\\s+", '', actual)
@@ -53,18 +57,64 @@ class ExportPluginTest(unittest.TestCase, TestHelper):
 
     def test_json_output(self):
         item1 = self.create_item()
-        actual = self.execute_command(
-            format_type='json',
-            artist=item1.artist
+        out = self.run_with_output(
+            'export',
+            '-f', 'json',
+            '-i', 'album,title',
+            item1.artist
         )
+        json_data = json.loads(out)[0]
         for key, val in self.test_values.items():
-            self.check_assertin(
-                actual=actual,
-                str_format='"{0}":"{1}"',
-                key=key,
-                val=val
-            )
+            self.assertTrue(key in json_data)
+            self.assertEqual(val, json_data[key])
 
+    def test_csv_output(self):
+        item1 = self.create_item()
+        out = self.run_with_output(
+            'export',
+            '-f', 'csv',
+            '-i', 'album,title',
+            item1.artist
+        )
+        csv_list = re.split('\r', re.sub('\n', '', out))
+        head = re.split(',', csv_list[0])
+        vals = re.split(',|\r', csv_list[1])
+        for index, column in enumerate(head):
+            self.assertTrue(self.test_values.get(column, None) is not None)
+            self.assertEqual(vals[index], self.test_values[column])
+
+    def test_xml_output(self):
+        item1 = self.create_item()
+        out = self.run_with_output(
+            'export',
+            '-f', 'xml',
+            '-i', 'album,title',
+            item1.artist
+        )
+        library = ET.fromstring(out)
+        self.assertIsInstance(library, Element)
+        for track in library[0]:
+            for details in track:
+                tag = details.tag
+                txt = details.text
+                self.assertTrue(tag in self.test_values, msg=tag)
+                self.assertEqual(self.test_values[tag], txt, msg=txt)
+
+    def check_assertin(self, actual, str_format, key, val):
+        expected = str_format.format(key, val)
+        self.assertIn(
+            expected,
+            actual
+        )
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
+"""
     def test_csv_output(self):
         item1 = self.create_item()
         actual = self.execute_command(
@@ -93,16 +143,17 @@ class ExportPluginTest(unittest.TestCase, TestHelper):
                 val=val
             )
 
-    def check_assertin(self, actual, str_format, key, val):
-        expected = str_format.format(key, val)
-        self.assertIn(
-            expected,
-            actual
+    def test_json_output(self):
+        item1 = self.create_item()
+        actual = self.execute_command(
+            format_type='json',
+            artist=item1.artist
         )
-
-
-def suite():
-    return unittest.TestLoader().loadTestsFromName(__name__)
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='suite')
+        for key, val in self.test_values.items():
+            self.check_assertin(
+                actual=actual,
+                str_format='"{0}":"{1}"',
+                key=key,
+                val=val
+            )
+"""
