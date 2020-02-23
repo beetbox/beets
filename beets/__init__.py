d6a291a3cf225a1f973dddae5c7e7e65af3c711f
Adam Jakab <jakabadambalazs@gmail.com>
2020-02-24 00:07:24 +0100

    fixed flake8 long line warning

--- beets/__init__.py
+++ beets/__init__.py
@@ -36,7 +36,8 @@ class IncludeLazyConfig(confuse.LazyConfig):
                 if os.path.isfile(filename):
                     self.set_file(filename)
                 else:
-                    raise FileNotFoundError("Warning! Configuration file({0}) does not exist!".format(filename))
+                    raise FileNotFoundError(
+                        "Warning! Configuration file({0}) does not exist!".format(filename))
         except confuse.NotFoundError:
             pass
 
