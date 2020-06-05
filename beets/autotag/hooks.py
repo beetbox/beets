14dc285b4f6348729e6cb3c7173fa3f786ef75b8
Jef LeCompte <jeffreylec@gmail.com>
2020-06-04 21:55:47 -0400

    chore(flake8): update comment and version

--- beets/autotag/hooks.py
+++ beets/autotag/hooks.py
@@ -332,7 +332,7 @@ class Distance(object):
         self._penalties = {}
 
     @LazyClassProperty
-    def _weights(cls):  # noqa
+    def _weights(cls):  # noqa: N805
         """A dictionary from keys to floating-point weights.
         """
         weights_view = config['match']['distance_weights']
