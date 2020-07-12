beetbox/beets

commit 117d16f2adf256ef872dbb78e97b17511da840cc

lyrics: add tests to track which websites can be scraped by our algo and be used as sources fot the google custom search engine.

beetsplug/lyrics.py
@@ -22,6 +22,7 @@ import urllib
 import json
 import unicodedata
 import difflib
+import string
 
 from beets.plugins import BeetsPlugin
 from beets import ui
@@ -239,8 +240,8 @@ def slugify(text):
     # http://stackoverflow.com/questions/295135/turn-a-string-into-a-valid-
     # filename-in-python
 
-    # Remove content within parentheses
-    pat = "([^,\(]*)\((.*?)\)"
+    text = re.sub("['_-]", ' ', text)
+    pat = "([^,\(]*)\((.*?)\)"  # Remove content within parentheses
     text = re.sub(pat, '\g<1>', text).strip()
     try:
         text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
@@ -305,34 +306,49 @@ def sanitize_lyrics(text):
 
     return text
 
+def remove_credits(text):
+    """Remove first/last line of text if it contains the word 'lyrics'
+    eg 'Lyrics by songsdatabase.com'
+    """
+    textlines = text.split('\n')
+    credits = None
+    for i in (0,-1):
+        if textlines and 'lyrics' in textlines[i].lower():
+            credits = textlines.pop(i)
+    if credits:
+        text = '\n'.join(textlines)
+    return text
 
-def is_lyrics(text, artist):
+
+def is_lyrics(text, artist=None):
     """Determine whether the text seems to be valid lyrics.
     """
-    badTriggers = []
+    if not text:
+        return
+
+    badTriggersOcc = []
     nbLines = text.count('\n')
     if nbLines <= 1:
         log.debug("Ignoring too short lyrics '%s'" % text)
         return 0
     elif nbLines < 5:
-        badTriggers.append('too_short')
+        badTriggersOcc.append('too_short')
     else:
-        # Don't penalize long text because of lyrics keyword in credits
-        textlines = text.split('\n')
-        popped = False
-        for i in [len(textlines) - 1, 0]:
-            if 'lyrics' in textlines[i].lower():
-                popped = textlines.pop(i)
-        if popped:
-            text = '\n'.join(textlines)
+        # Lyrics look legit, remove credits to avoid being penalized further
+        # down
+        text = remove_credits(text)
 
-    for item in artist, 'lyrics', 'copyright', 'property':
-        badTriggers += [item] * len(re.findall(r'\W%s\W' % item, text, re.I))
+    badTriggers = ['lyrics', 'copyright', 'property']
+    if artist:
+        badTriggersOcc += [artist]
 
-    if badTriggers:
-        log.debug('Bad triggers detected: %s' % badTriggers)
+    for item in badTriggers:
+        badTriggersOcc += [item] * len(re.findall(r'\W%s\W' % item, text, re.I))
 
-    return len(badTriggers) < 2
+    if badTriggersOcc:
+        log.debug('Bad triggers detected: %s' % badTriggersOcc)
+
+    return len(badTriggersOcc) < 2
 
 
 def scrape_lyrics_from_url(url):
@@ -403,7 +419,7 @@ def fetch_google(artist, title):
     if 'error' in data:
         reason = data['error']['errors'][0]['reason']
         log.debug(u'google lyrics backend error: %s' % reason)
-        return None
+        return
 
     if 'items' in data.keys():
         for item in data['items']:

test/_lyricstext.py
@@ -1,0 +1,164 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""Lyrics samples for test_lyrics unit tests"""
+
+texts = dict()  # reference lyrics against which are compared fetched lyrics
+mockcontents = dict()  # content of fetched urls
+
+texts['Lady Madonna'] = \
+    """Lady Madonna, children at your feet
+    Wonder how you manage to make ends meet
+    Who finds the money? When you pay the rent?
+    Did you think that money was Heaven sent?
+    Friday night arrives without a suitcase
+    Sunday morning creep in like a nun
+    Monday's child has learned to tie his bootlace
+    See how they run
+
+    Lady Madonna, baby at your breast
+    Wonder how you manage to feed the rest
+
+    See how they run
+
+    Lady Madonna, lying on the bed
+    Listen to the music playing in your head
+
+    Tuesday afternoon is never ending
+    Wednesday morning papers didn't come
+    Thursday night you stockings needed mending
+    See how they run
+
+    Lady Madonna, children at your feet
+    Wonder how you manage to make ends meet"""
+
+texts["Jazz'n'blues"] = \
+    """It's always gone within two days.
+    Follow my father, his extravagant ways.
+    So if I got it I will spend it all,
+    Camden and Parkway 'til I hit a wall.
+
+    I cross my fingers at the cash machine,
+    As I check my balance I kiss the screen.
+    I love it when it says I got the means,
+    To go to Miss Sixty and pick up my new jeans.
+
+    Chorus: (x2)
+    Never lasts me long,
+    Handle finance wrong,
+    Blow it all on bags and shoes,
+    Jazz 'n blues.
+
+    Standin' too deep at the bar today,
+    Wait with impatience to throw my cash away.
+    Four white Russians a JD and Coke,
+    Buy their drinks all night and now I am broke.
+    But that's cool 'cause I can borrow more from you,
+    And I didn't forget about that fifty pound, too.
+    Tell you what, my advance is comin' through,
+    I'll take you out shoppin',
+    Can you wait 'til next June? Yeah.
+
+    Chorus:
+    Never lasts me long,
+    Handle finance wrong (handle it wrong),
+    Blow it all on bags and shoes,
+    Jazz 'n blues (jazz and blues).
+    Never lasts me long (long),
+    Handle finance wrong (wrong),
+    Blow it all on bags and shoes (blow it all),
+    Jazz 'n blues."""
+
+texts["I could beat myself"] = \
+    """oooh ahh, I'm hurting, bad
+    oooh ahh I'm hurting bad!
+
+    I did not see what I was supposed to see
+    taking it easy when a friend told me I was in
+    danger, so much danger.
+    Underestimating my woman, not taking her
+    out, working too hard and now she's gone off
+    with a stranger, someone I don't even know.
+
+    I should have taken her out, every once and a
+    while, taken her to dinner on the finer side, shown
+    her a life that was all worth while, now I guese I gotta
+    walk an extra mile! I could beat myself! Ahh yeah.
+
+    Now I'm gonna feel funny out there in the crowd
+    when my friends all ask me, where is your woman?
+    long time I don't see. Now I've got to think fast gotta
+    use my head, give a good story and make sure they
+    buy my version of the situation.
+
+    All the while I wouldn't lie I'm gonna do this once
+    see my reputation sinking in the distance, if they
+    knew the truth that really existed...then my little sanity
+    would be wasted! I could beat myself ahh yes..I could
+    beat myself!!! Ohh lord lord.
+
+    oooh ooh I'm hurting, hurting inside
+    oooh ooh I'm hurting
+
+    Now I really want to hear a little news now and
+    then, this is not what I expect to hear from my
+    friend I'm dissapointed. He should realize that its
+    gonna destroy my position (remember I'm a name
+    brand) now I've gotta see....I should have held her tight
+    every once and a while, gotten it together on the finer style
+    shown her a life that was all worth while, now it seems I gotta
+    walk an extra mile....I could beat myself..ahh yes, I could beat myself
+    ooh lord
+
+    ooh I'm hurting
+    ooooh I'm hurting,hurting inside
+
+    I don't know what I wanna tell you
+    but I wanna tell you something real, real good yes
+    someting to make you
+    wanna shiver"""
+
+texts["Hey it's ok"] = \
+    """Mama, Papa, please forget the times
+    I wet my bed
+    I swear not to do it again
+    Please forgive the way I looked
+    when I was fourteen
+    I didn't know who I wanted to be
+
+    Hey It's OK, It's OK
+    Cause I've found what i wanted
+    Hey It's OK, I'ts Ok
+    Cause I've found what i wanted
+
+    Friends and lovers please forgive
+    the mean things I've said
+    I swear not to do again
+    Please forgive the way I act when
+    I've had too much to drink
+    I'm fighting against myself
+
+    Hey It's OK, It's OK
+    Cause I've found what i wanted
+    Hey It's OK, I'ts Ok
+
+    Cause I've found what i wanted
+
+    And I swear not to do anything funny anymore
+    yes I swear not to do anything funny anymore
+
+    Hey It's OK, It's OK
+    Cause I've found what i wanted
+    Hey It's OK, I'ts Ok
+    Cause I've found what i wanted
+    Hey It's OK, It's OK
+    Cause I've found what i wanted
+    Hey It's OK, I'ts Ok
+    Cause I've found what i wanted"""
+
+missing_texts = [
+    """Lyricsmania staff is working hard for you to add $TITLE lyrics as soon
+    as they'll be released by $ARTIST, check back soon!
+    In case you have the lyrics to $TITLE and want to send them to us, fill out
+    the following form."""
+]

test/lyrics_sources.py
@@ -1,0 +1,176 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""Tests for the 'lyrics' plugin"""
+
+import os
+import logging
+from _common import unittest
+import _lyricstext
+from beetsplug import lyrics
+from beets import config
+from beets.util import confit
+from bs4 import BeautifulSoup
+from nose.plugins.attrib import attr
+
+log = logging.getLogger('beets')
+
+try:
+    googlekey = config['lyrics']['google_API_key'].get(unicode)
+except confit.NotFoundError:
+    googlekey = None
+
+# default query for tests
+definfo = dict(artist=u'The Beatles', title=u'Lady Madonna')
+
+
+class MockFetchUrl(object):
+    def __init__(self, pathval='fetched_path'):
+        self.pathval = pathval
+        self.fetched = None
+
+    def __call__(self, url, filename=None):
+        self.fetched = url
+        url = url.replace('http://', '').replace('www.', '')
+        fn = "".join(x for x in url if (x.isalnum() or x == '/'))
+        fn = fn.split('/')
+        fn = os.path.join('rsrc', 'lyrics', fn[0], fn[-1]) + '.txt'
+        with open(fn, 'r') as f:
+            content = f.read()
+        return content
+
+
+def is_lyrics_content_ok(title, text):
+    """Compare lyrics text to expected lyrics for given title"""
+
+    setexpected = set(_lyricstext.texts[title].split())
+    settext = set(text.split())
+    setinter = setexpected.intersection(settext)
+    # consider lyrics ok if they share 50% or more with the reference
+    if len(setinter):
+        ratio = 1.0 * max(len(setexpected), len(settext)) / len(setinter)
+        return (ratio > .5 and ratio < 2)
+    return False
+
+
+class LyricsPluginTest(unittest.TestCase):
+    def setUp(self):
+        """Set up configuration"""
+        lyrics.LyricsPlugin()
+
+    def test_default_ok(self):
+        """Test each lyrics engine with the default query"""
+
+        lyrics.fetch_url = MockFetchUrl()
+
+        for f in (lyrics.fetch_lyricswiki, lyrics.fetch_lyricscom):
+            res = f(definfo['artist'], definfo['title'])
+            self.assertTrue(lyrics.is_lyrics(res))
+            self.assertTrue(is_lyrics_content_ok(definfo['title'], res))
+
+    def test_missing_lyrics(self):
+        for msg in _lyricstext.missing_texts:
+            self.assertFalse(lyrics.is_lyrics(msg), msg)
+
+
+@attr('slow')
+class LyricsScrapingPluginTest(unittest.TestCase):
+
+    # Every source entered in default beets google custom search engine
+    # must be listed below.
+    # Use default query when possible, or override artist and title field
+    # if website don't have lyrics for default query.
+    sourcesOk = [
+        dict(definfo, url=u'http://www.smartlyrics.com',
+             path=u'/Song18148-The-Beatles-Lady-Madonna-lyrics.aspx'),
+        dict(definfo, url=u'http://www.elyricsworld.com',
+             path=u'/lady_madonna_lyrics_beatles.html'),
+        dict(artist=u'Beres Hammond', title=u'I could beat myself',
+            url=u'http://www.reggaelyrics.info',
+            path=u'/beres-hammond/i-could-beat-myself'),
+        dict(definfo, artist=u'Lilly Wood & the prick', title=u"Hey it's ok",
+             url=u'http://www.lyricsmania.com',
+             path=u'/hey_its_ok_lyrics_lilly_wood_and_the_prick.html'),
+        dict(definfo, artist=u'Lilly Wood & the prick', title=u"Hey it's ok",
+             url=u'http://www.paroles.net/',
+             path=u'lilly-wood-the-prick/paroles-hey-it-s-ok'),
+        dict(definfo, artist=u'Amy Winehouse', title=u"Jazz'n'blues",
+             url=u'http://www.lyricsontop.com',
+             path=u'/amy-winehouse-songs/jazz-n-blues-lyrics.html'),
+        dict(definfo, url=u'http://www.sweetslyrics.com',
+             path=u'/761696.The%20Beatles%20-%20Lady%20Madonna.html'),
+        dict(definfo, url=u'http://www.lyrics007.com',
+             path=u'/The%20Beatles%20Lyrics/Lady%20Madonna%20Lyrics.html'),
+        dict(definfo, url=u'http://www.absolutelyrics.com',
+             path=u'/lyrics/view/the_beatles/lady_madonna'),
+        dict(definfo, url=u'http://www.azlyrics.com/',
+             path=u'/lyrics/beatles/ladymadonna.html'),
+        dict(definfo, url=u'http://www.chartlyrics.com',
+             path=u'/_LsLsZ7P4EK-F-LD4dJgDQ/Lady+Madonna.aspx'),
+        dict(definfo, url='http://www.releaselyrics.com',
+             path=u'/e35f/the-beatles-lady-madonna'),
+    ]
+
+    # Websites that can't be scraped yet and whose results must be
+    # flagged as invalid lyrics.
+    sourcesFail = [
+        dict(definfo, url='http://www.songlyrics.com',
+             path=u'/the-beatles/lady-madonna-lyrics'),
+        dict(definfo, url='http://www.metrolyrics.com/',
+             path='best-for-last-lyrics-adele.html')
+    ]
+
+    # Websites that return truncated lyrics because of scraping issues, and
+    # thus should not be included as sources to Google CSE.
+    # They are good candidates for later inclusion after improvement
+    # iterations of the scraping algorithm.
+    sourcesIncomplete = [
+        dict(definfo, artist=u'Lilly Wood & the prick', title=u"Hey it's ok",
+             url=u'http://www.lacoccinelle.net',
+             path=u'/paroles-officielles/550512.html'),
+    ]
+
+    def test_sources_ok(self):
+        for s in self.sourcesOk:
+            url = s['url'] + s['path']
+            res = lyrics.scrape_lyrics_from_url(url)
+            self.assertTrue(lyrics.is_lyrics(res), url)
+            self.assertTrue(is_lyrics_content_ok(s['title'], res), url)
+
+    def test_sources_fail(self):
+        for s in self.sourcesFail:
+            url = s['url'] + s['path']
+            res = lyrics.scrape_lyrics_from_url(url)
+            # very unlikely these sources pass if the scraping algo is not
+            # tweaked on purpose for these cases
+            self.assertFalse(lyrics.is_lyrics(res), "%s => %s" % (url, res))
+
+    def test_sources_incomplete(self):
+        for s in self.sourcesIncomplete:
+            url = s['url'] + s['path']
+            res = lyrics.scrape_lyrics_from_url(url)
+
+            self.assertTrue(lyrics.is_lyrics(res))
+            # these sources may pass if the html source evolve or after
+            # a random improvement in the scraping algo: we want to
+            # be noticed if it's the case.
+            if is_lyrics_content_ok(s['title'], res):
+                log.debug('Source %s actually return valid lyrics!' % s['url'])
+
+    def test_is_page_candidate(self):
+        for s in self.sourcesOk:
+            url = unicode(s['url'] + s['path'])
+            html = lyrics.fetch_url(url)
+            soup = BeautifulSoup(html)
+            if not soup.title:
+                continue
+            self.assertEqual(lyrics.is_page_candidate(url, soup.title.string,
+                                                      s['title'], s['artist']),
+                             True, url)
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')

test/test_lyrics.py
@@ -71,6 +71,21 @@ class LyricsPluginTest(unittest.TestCase):
             'Song'
         )
 
+    def test_remove_credits(self):
+        self.assertEqual(
+            lyrics.remove_credits("""It's close to midnight
+                                     Lyrics brought by example.com"""),
+            "It's close to midnight"
+        )
+        self.assertEqual(
+            lyrics.remove_credits("""Lyrics brought by example.com"""),
+            ""
+        )
+        text = """Look at all the shit that i done bought her
+                  See lyrics ain't nothin 
+                  if the beat aint crackin"""
+        self.assertEqual(lyrics.remove_credits(text), text)
+
 
 def suite():
     return unittest.TestLoader().loadTestsFromName(__name__)

commit 44e4d558c97271244091e8541336454129b0622c

lyrics: fix bad_triggers in the google plugin
regression from 117d16f

beetsplug/lyrics.py
@@ -563,7 +563,7 @@ class Google(Backend):
 
         bad_triggers = ['lyrics', 'copyright', 'property', 'links']
         if artist:
-            bad_triggers_occ += [artist]
+            bad_triggers += [artist]
 
         for item in bad_triggers:
             bad_triggers_occ += [item] * len(re.findall(r'\W%s\W' % item,