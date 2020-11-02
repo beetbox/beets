2d024d2f38fcc8ac7a134ec290cb6a6b9f3b5ca4
FichteFoll <fichtefoll2@googlemail.com>
2020-11-02 01:06:05 +0100

    Avoid overeager inclusion of album attributes
    Co-Authored-By: Curtis Rueden <ctrueden@wisc.edu>

--- beets/library.py
+++ beets/library.py
@@ -375,9 +375,10 @@ class FormattedItemMapping(dbcore.db.FormattedMapping):
     """
 
     def __init__(self, item, for_path=False):
-        super(FormattedItemMapping, self).__init__(item, for_path)
         # We treat album and item keys specially here,
         # so exclude transitive album keys from the model's keys.
+        super(FormattedItemMapping, self).__init__(item, for_path,
+                                                   compute_keys=False)
         self.model_keys = item.keys(computed=True, with_album=False)
         self.item = item
 
