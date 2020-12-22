ec1419f49d633625f4450301e2447f66ee6c1de2
soergeld <soergeldorian@gmail.com>
2020-12-22 15:59:45 +0100

    mock confined to test

--- test/test_parentwork.py
+++ test/test_parentwork.py
@@ -147,9 +147,10 @@ class ParentWorkTest(unittest.TestCase, TestHelper):
         """Set up configuration"""
         self.setup_beets()
         self.load_plugins('parentwork')
-        #musicbrainzngs.get_work_by_id = Mock(side_effect=mock_workid_response)
+#        musicbrainzngs.get_work_by_id = Mock(side_effect=mock_workid_response)
         self.patcher = patch('musicbrainzngs.get_work_by_id',
                              side_effect=mock_workid_response)
+        self.patcher.start()
 
     def tearDown(self):
         self.unload_plugins()
