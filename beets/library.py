f1cbbfa02d16b682b86b900118b4611fea47bf11
Andrew Sultana <andrewdsultana@gmail.com>
2021-06-16 16:30:42 +0100

    template: fix ifdef bug #3852 involving non-string fields with missing values

--- beets/library.py
+++ beets/library.py
@@ -1753,7 +1753,7 @@ class DefaultTemplateFunctions(object):
         :param falseval: The string if the condition is false
         :return: The string, based on condition
         """
-        if self.item.formatted().get(field):
+        if field in self.item:
             return trueval if trueval else self.item.formatted().get(field)
         else:
             return falseval
