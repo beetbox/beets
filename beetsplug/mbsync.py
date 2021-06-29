0422306b7999a1b414e6e0024e64a3975849125c
Dorian Soergel <soergeldorian@gmail.com>
2021-06-29 17:11:49 +0200

    accidentally deleted line

--- beetsplug/mbsync.py
+++ beetsplug/mbsync.py
@@ -12,6 +12,7 @@
 #
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
+
 """Update library's tags using MusicBrainz.
 """
 from __future__ import division, absolute_import, print_function
