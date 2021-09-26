895dcbb828662b3a40f72527ad9160ec4fa9f9d5
Andrew Rogl <arogl1@gmail.com>
2021-09-26 17:52:22 +1000

    git workflow is strange

--- beets/autotag/mb.py
+++ beets/autotag/mb.py
@@ -18,8 +18,9 @@
 import musicbrainzngs
 import re
 import traceback
+
 from collections import Counter
-from six.moves.urllib.parse import urljoin
+from urllib.parse import urljoin
 
 from beets import logging
 from beets import plugins
