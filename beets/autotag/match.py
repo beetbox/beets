4580f80813c8969a727e7ec8652ab7c81e83060a
Adrian Sampson <adrian@radbox.org>
2022-02-27 14:01:48 -0500

    Slight rewording

--- beets/autotag/match.py
+++ beets/autotag/match.py
@@ -357,7 +357,7 @@ def _add_candidate(items, results, info):
         log.debug('No tracks.')
         return
 
-    # Prevent duplicates
+    # Prevent duplicates.
     if info.album_id and info.album_id in results:
         log.debug('Duplicate.')
         return
