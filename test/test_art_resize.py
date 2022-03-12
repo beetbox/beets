c4722947b3d0d77be635284c304879eefa796253
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2022-03-12 11:23:54 +0100

    artresizer: backend classes part 1: stub classes, version checks

--- test/test_art_resize.py
+++ test/test_art_resize.py
@@ -22,14 +22,32 @@ from test import _common
 from test.helper import TestHelper
 from beets.util import command_output, syspath
 from beets.util.artresizer import (
+    IMBackend,
+    PILBackend,
     pil_resize,
     im_resize,
-    get_im_version,
-    get_pil_version,
     pil_deinterlace,
     im_deinterlace,
-    ArtResizer,
 )
+
+
+class DummyIMBackend(IMBackend):
+    """An `IMBackend` which pretends that ImageMagick is available, and has
+    a sufficiently recent version to support image comparison.
+    """
+    def __init__(self):
+        self.version = (7, 0, 0)
+        self.legacy = False
+        self.convert_cmd = ['magick']
+        self.identify_cmd = ['magick', 'identify']
+        self.compare_cmd = ['magick', 'compare']
+
+
+class DummyPILBackend(PILBackend):
+    """An `PILBackend` which pretends that PIL is available.
+    """
+    def __init__(self):
+        pass
 
 
 class ArtResizerFileSizeTest(_common.TestCase, TestHelper):
@@ -46,11 +64,11 @@ class ArtResizerFileSizeTest(_common.TestCase, TestHelper):
         """Called after each test, unloading all plugins."""
         self.teardown_beets()
 
-    def _test_img_resize(self, resize_func):
+    def _test_img_resize(self, backend, resize_func):
         """Test resizing based on file size, given a resize_func."""
         # Check quality setting unaffected by new parameter
         im_95_qual = resize_func(
-            ArtResizer.shared,
+            backend,
             225,
             self.IMG_225x225,
             quality=95,
@@ -61,7 +79,7 @@ class ArtResizerFileSizeTest(_common.TestCase, TestHelper):
 
         # Attempt a lower filesize with same quality
         im_a = resize_func(
-            ArtResizer.shared,
+            backend,
             225,
             self.IMG_225x225,
             quality=95,
@@ -74,7 +92,7 @@ class ArtResizerFileSizeTest(_common.TestCase, TestHelper):
 
         # Attempt with lower initial quality
         im_75_qual = resize_func(
-            ArtResizer.shared,
+            backend,
             225,
             self.IMG_225x225,
             quality=75,
@@ -83,7 +101,7 @@ class ArtResizerFileSizeTest(_common.TestCase, TestHelper):
         self.assertExists(im_75_qual)
 
         im_b = resize_func(
-            ArtResizer.shared,
+            backend,
             225,
             self.IMG_225x225,
             quality=95,
@@ -94,37 +112,38 @@ class ArtResizerFileSizeTest(_common.TestCase, TestHelper):
         self.assertLess(os.stat(syspath(im_b)).st_size,
                         os.stat(syspath(im_75_qual)).st_size)
 
-    @unittest.skipUnless(get_pil_version(), "PIL not available")
+    @unittest.skipUnless(PILBackend.available(), "PIL not available")
     def test_pil_file_resize(self):
         """Test PIL resize function is lowering file size."""
-        self._test_img_resize(pil_resize)
+        self._test_img_resize(PILBackend(), pil_resize)
 
-    @unittest.skipUnless(get_im_version(), "ImageMagick not available")
+    @unittest.skipUnless(IMBackend.available(), "ImageMagick not available")
     def test_im_file_resize(self):
         """Test IM resize function is lowering file size."""
-        self._test_img_resize(im_resize)
+        self._test_img_resize(IMBackend(), im_resize)
 
-    @unittest.skipUnless(get_pil_version(), "PIL not available")
+    @unittest.skipUnless(PILBackend.available(), "PIL not available")
     def test_pil_file_deinterlace(self):
         """Test PIL deinterlace function.
 
         Check if pil_deinterlace function returns images
         that are non-progressive
         """
-        path = pil_deinterlace(ArtResizer.shared, self.IMG_225x225)
+        path = pil_deinterlace(PILBackend(), self.IMG_225x225)
         from PIL import Image
         with Image.open(path) as img:
             self.assertFalse('progression' in img.info)
 
-    @unittest.skipUnless(get_im_version(), "ImageMagick not available")
+    @unittest.skipUnless(IMBackend.available(), "ImageMagick not available")
     def test_im_file_deinterlace(self):
         """Test ImageMagick deinterlace function.
 
         Check if im_deinterlace function returns images
         that are non-progressive.
         """
-        path = im_deinterlace(ArtResizer.shared, self.IMG_225x225)
-        cmd = ArtResizer.shared.im_identify_cmd + [
+        im = IMBackend()
+        path = im_deinterlace(im, self.IMG_225x225)
+        cmd = im.identify_cmd + [
             '-format', '%[interlace]', syspath(path, prefix=False),
         ]
         out = command_output(cmd).stdout
