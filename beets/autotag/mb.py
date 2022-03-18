2f4511fccaf7c3160befef0906ae2648cfa5bd2f
Šarūnas Nejus <snejus@protonmail.com>
2022-03-18 05:05:58 +0000

    Move the logic to hooks.py

--- beets/autotag/mb.py
+++ beets/autotag/mb.py
@@ -482,9 +482,6 @@ def match_album(artist, album, tracks=None, extra_tags=None):
     The query consists of an artist name, an album name, and,
     optionally, a number of tracks on the album and any other extra tags.
     """
-    if not config["musicbrainz"]["enabled"].get(bool):
-        return None
-
     # Build search criteria.
     criteria = {'release': album.lower().strip()}
     if artist is not None:
@@ -561,9 +558,6 @@ def album_for_id(releaseid):
     object or None if the album is not found. May raise a
     MusicBrainzAPIError.
     """
-    if not config["musicbrainz"]["enabled"].get(bool):
-        return None
-
     log.debug('Requesting MusicBrainz release {}', releaseid)
     albumid = _parse_id(releaseid)
     if not albumid:
@@ -585,9 +579,6 @@ def track_for_id(releaseid):
     """Fetches a track by its MusicBrainz ID. Returns a TrackInfo object
     or None if no track is found. May raise a MusicBrainzAPIError.
     """
-    if not config["musicbrainz"]["enabled"].get(bool):
-        return None
-
     trackid = _parse_id(releaseid)
     if not trackid:
         log.debug('Invalid MBID ({0}).', releaseid)
