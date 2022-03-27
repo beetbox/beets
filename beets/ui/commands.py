b07b0e2f7e377d466ef4e36901396a4bb7dcfdbf
alicezou <alicezou@umich.edu>
2022-03-27 10:33:54 -0400

    add prompt

--- beets/ui/commands.py
+++ beets/ui/commands.py
@@ -1398,6 +1398,12 @@ version_cmd = ui.Subcommand(
 version_cmd.func = show_version
 default_commands.append(version_cmd)
 
+# database_location: return true if user wants to create the parent directories.
+def database_dir_creation(path):
+    # Ask the user for a choice.
+    return ui.input_yn("{} does not exist, create it (Y/n)?"
+                    .format(displayable_path(path)))
+
 
 # modify: Declaratively change metadata.
 
