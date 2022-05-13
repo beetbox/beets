1b2463f62e46b1765ed576611dfa8fd7b19fef18
Alok Saboo <arsaboo@gmx.com>
2022-05-12 20:42:13 -0400

    Update spotify.py

--- beetsplug/spotify.py
+++ beetsplug/spotify.py
@@ -1,5 +1,6 @@
 # This file is part of beets.
 # Copyright 2019, Rahul Ahuja.
+# Copyright 2022, Alok Saboo.
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
