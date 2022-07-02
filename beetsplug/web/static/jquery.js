8c788f3eeda9781aa35493ee84f0301d325a4f9b
Tim Gates <tim.gates@iress.com>
2022-07-02 23:32:58 +1000

    docs: fix simple typo, seperated -> separated
    There is a small typo in beetsplug/web/static/jquery.js.
    
    Should read `separated` rather than `seperated`.

--- beetsplug/web/static/jquery.js
+++ beetsplug/web/static/jquery.js
@@ -2278,7 +2278,7 @@ jQuery.fn.extend({
 					classNames = value.split( rspace );
 
 				while ( (className = classNames[ i++ ]) ) {
-					// check each className given, space seperated list
+					// check each className given, space separated list
 					state = isBool ? state : !self.hasClass( className );
 					self[ state ? "addClass" : "removeClass" ]( className );
 				}
