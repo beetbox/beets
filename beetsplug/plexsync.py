3bea97f733829ed47098361c0fd9589b9c8b894b
Alok Saboo <arsaboo@gmx.com>
2022-07-16 17:35:53 -0400

    Update plexsync.py

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -114,4 +114,7 @@ class PlexSync(BeetsPlugin):
         self._log.info('year: {}, album: {}, title: {}', item.year, item.album, item.title)
         tracks = self.music.search(year=item.year, filters={'album.title': item.album, 'track.title': item.title}, libtype='track')
         self._log.info('tracks: {}', len(tracks))
-        return tracks[0]
+        if len(tracks) = 0:
+            continue
+        else:
+            return tracks[0]
