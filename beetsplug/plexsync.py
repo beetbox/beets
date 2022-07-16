b76b2e1ada3d9ad131d7f07712855e7588301d7c
Alok Saboo <arsaboo@gmx.com>
2022-07-16 15:37:59 -0400

    Add plexsync initial commit

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -10,12 +10,12 @@ Put something like the following in your config.yaml to configure:
 
 import beets.ui
 from beets import config
-from beets.plugins import BeetsPlugin
+from beets.plugins import BeetsPlugin, MetadataSourcePlugin
 from plexapi import exceptions
 from plexapi.server import PlexServer
 
 
-class PlexSync(BeetsPlugin):
+class PlexSync(MetadataSourcePlugin, BeetsPlugin):
     data_source = 'Plex'
 
     def __init__(self):
@@ -57,9 +57,57 @@ class PlexSync(BeetsPlugin):
             self._plexupdate()
 
         plexupdate_cmd.func = func
+
+        # plexsync command
+        sync_cmd = ui.Subcommand('plexsync',
+                                 help="fetch track attributes from Plex")
+        sync_cmd.parser.add_option(
+            '-f', '--force', dest='force_refetch',
+            action='store_true', default=False,
+            help='re-download data when already present'
+        )
+
+        def func_sync(lib, opts, args):
+            items = lib.items(ui.decargs(args))
+            self._fetch_plex_info(items, ui.should_write(), opts.force_refetch)
+
+        sync_cmd.func = func_sync
+
         return [plexupdate_cmd]
 
     def _plexupdate(self):
         """Update Plex music library."""
 
-        self.music.update()
+        try:
+            self.music.update()
+            self._log.info('Update started.')
+        except exceptions.PlexApiException:
+            self._log.warning("{} Update failed",
+                              self.config['plex']['library_name'])
+
+    def _fetch_plex_info(self, items, write, force):
+        """Obtain track information from Plex."""
+
+        self._log.debug('Total {} tracks', len(items))
+
+        for index, item in enumerate(items, start=1):
+            self._log.info('Processing {}/{} tracks - {} ',
+                           index, len(items), item)
+            # If we're not forcing re-downloading for all tracks, check
+            # whether the popularity data is already present
+            if not force:
+                if 'plex_userrating' in item:
+                    self._log.debug('Plex rating already present for: {}',
+                                    item)
+                    continue
+            item.plex_key = self.plex_track_key(item)
+            item.store()
+            if write:
+                item.try_write()
+
+    def plex_track_key(self, item):
+        """Fetch the Plex track key."""
+        tracks = self.music.search(year=item.year, filters={'album.title': item.album, 'track.title': item.title}, libtype='track')
+        plex_track_key = tracks[0].key
+        self._log.info('track_key: {}', plex_track_key)
+        return plex_track_key
