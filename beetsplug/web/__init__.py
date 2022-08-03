fde2ad3f65c76f040ef5794b1ed3e852577d7059
vicholp <vlinerospardo@gmail.com>
2022-08-03 01:22:35 -0400

    fix get item file of web plugin

--- beetsplug/web/__init__.py
+++ beetsplug/web/__init__.py
@@ -324,7 +324,7 @@ def item_file(item_id):
     response = flask.send_file(
         item_path,
         as_attachment=True,
-        attachment_filename=safe_filename
+        download_name=safe_filename
     )
     response.headers['Content-Length'] = os.path.getsize(item_path)
     return response
