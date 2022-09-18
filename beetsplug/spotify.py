e4063b5880300c7a06b09732e94175e6c8ec8c4d
Alok Saboo <arsaboo@gmx.com>
2022-09-17 20:56:48 -0400

    remove datetype

--- beetsplug/spotify.py
+++ beetsplug/spotify.py
@@ -57,7 +57,6 @@ class SpotifyPlugin(MetadataSourcePlugin, BeetsPlugin):
         'spotify_tempo': types.FLOAT,
         'spotify_time_signature': types.INTEGER,
         'spotify_valence': types.FLOAT,
-        'spotify_updated': DateType(),
     }
 
     # Base URLs for the Spotify API
