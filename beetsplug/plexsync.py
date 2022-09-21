7071b8d14bc94a2dc63570c4a8e78228cd79207a
Alok Saboo <arsaboo@gmx.com>
2022-09-21 08:16:09 -0400

    Additional error handling

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -197,7 +197,7 @@ class PlexSync(BeetsPlugin):
         for item in items:
             try:
                 plex_set.add(self.plex.fetchItem(item.plex_ratingkey))
-            except exceptions.NotFound:
+            except (exceptions.NotFound, AttributeError):
                 self._log.warning('{} not found in Plex library', item)
                 continue
         to_add = plex_set - playlist_set
