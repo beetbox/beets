beetbox/beets

commit 4a25d7c9d6b9bf0bd5fa5b515fddfff41cd5046a

Fix beets#4472 - set 'max_bitrate' default param to none

beetsplug/convert.py
@@ -26,7 +26,7 @@ import logging
 
 from beets import ui, util, plugins, config
 from beets.plugins import BeetsPlugin
-from confuse import ConfigTypeError
+from confuse import ConfigTypeError, Optional
 from beets import art
 from beets.util.artresizer import ArtResizer
 from beets.library import parse_query_string
@@ -101,7 +101,9 @@ def should_transcode(item, fmt):
     if config['convert']['never_convert_lossy_files'] and \
             not (item.format.lower() in LOSSLESS_FORMATS):
         return False
-    maxbr = config['convert']['max_bitrate'].get(int)
+    maxbr = config['convert']['max_bitrate'].get(Optional(int))
+    if maxbr is None:
+        return False
     return fmt.lower() != item.format.lower() or \
         item.bitrate >= 1000 * maxbr
 
@@ -136,7 +138,7 @@ class ConvertPlugin(BeetsPlugin):
                 'wma':
                     'ffmpeg -i $source -y -vn -acodec wmav2 -vn $dest',
             },
-            'max_bitrate': 500,
+            'max_bitrate': None,
             'auto': False,
             'auto_keep': False,
             'tmpdir': None,

test/test_convert.py
@@ -171,6 +171,8 @@ class ConvertCliTest(unittest.TestCase, TestHelper, ConvertCommand):
         )
         self.config['convert'] = {
             'dest': self.convert_dest,
+            # Enforce running convert
+            'max_bitrate': 1,
             'paths': {'default': 'converted'},
             'format': 'mp3',
             'formats': {
@@ -249,6 +251,13 @@ class ConvertCliTest(unittest.TestCase, TestHelper, ConvertCommand):
             self.run_convert('An impossible query')
         self.assertEqual(logs[0], 'convert: Empty query result.')
 
+    def test_no_transcode_when_max_bitrate_set_to_none(self):
+        self.config['convert']['max_bitrate'] = None
+        with control_stdin('y'):
+            self.run_convert()
+        converted = os.path.join(self.convert_dest, b'converted.ogg')
+        self.assertNoFileTag(converted, 'mp3')
+
 
 @_common.slow_test()
 class NeverConvertLossyFilesTest(unittest.TestCase, TestHelper,
@@ -263,6 +272,8 @@ class NeverConvertLossyFilesTest(unittest.TestCase, TestHelper,
         self.convert_dest = os.path.join(self.temp_dir, b'convert_dest')
         self.config['convert'] = {
             'dest': self.convert_dest,
+            # Enforce running convert
+            'max_bitrate': 1,
             'paths': {'default': 'converted'},
             'never_convert_lossy_files': True,
             'format': 'mp3',

commit ecf4edd04e3d841f72b8e63f7ffa060b0454b834

Fix recent bug that would not transcode when format was wrong
Bug introduced in 4a25d7c9d6b9bf0bd5fa5b515fddfff41cd5046a, should_transcode would return False even when the format was different.

Also fix up tests and documentation.

beetsplug/convert.py
@@ -102,10 +102,9 @@ def should_transcode(item, fmt):
             not (item.format.lower() in LOSSLESS_FORMATS):
         return False
     maxbr = config['convert']['max_bitrate'].get(Optional(int))
-    if maxbr is None:
-        return False
-    return fmt.lower() != item.format.lower() or \
-        item.bitrate >= 1000 * maxbr
+    if maxbr is not None and item.bitrate >= 1000 * maxbr:
+        return True
+    return fmt.lower() != item.format.lower()
 
 
 class ConvertPlugin(BeetsPlugin):

test/test_convert.py
@@ -171,12 +171,11 @@ class ConvertCliTest(unittest.TestCase, TestHelper, ConvertCommand):
         )
         self.config['convert'] = {
             'dest': self.convert_dest,
-            # Enforce running convert
-            'max_bitrate': 1,
             'paths': {'default': 'converted'},
             'format': 'mp3',
             'formats': {
                 'mp3': self.tagged_copy_cmd('mp3'),
+                'ogg': self.tagged_copy_cmd('ogg'),
                 'opus': {
                     'command': self.tagged_copy_cmd('opus'),
                     'extension': 'ops',
@@ -251,12 +250,48 @@ class ConvertCliTest(unittest.TestCase, TestHelper, ConvertCommand):
             self.run_convert('An impossible query')
         self.assertEqual(logs[0], 'convert: Empty query result.')
 
-    def test_no_transcode_when_max_bitrate_set_to_none(self):
-        self.config['convert']['max_bitrate'] = None
+    def test_no_transcode_when_maxbr_set_high_and_different_formats(self):
+        self.config['convert']['max_bitrate'] = 5000
+        with control_stdin('y'):
+            self.run_convert()
+        converted = os.path.join(self.convert_dest, b'converted.mp3')
+        self.assertFileTag(converted, 'mp3')
+
+    def test_transcode_when_maxbr_set_low_and_different_formats(self):
+        self.config['convert']['max_bitrate'] = 5
+        with control_stdin('y'):
+            self.run_convert()
+        converted = os.path.join(self.convert_dest, b'converted.mp3')
+        self.assertFileTag(converted, 'mp3')
+
+    def test_transcode_when_maxbr_set_to_none_and_different_formats(self):
+        with control_stdin('y'):
+            self.run_convert()
+        converted = os.path.join(self.convert_dest, b'converted.mp3')
+        self.assertFileTag(converted, 'mp3')
+
+    def test_no_transcode_when_maxbr_set_high_and_same_formats(self):
+        self.config['convert']['max_bitrate'] = 5000
+        self.config['convert']['format'] = 'ogg'
         with control_stdin('y'):
             self.run_convert()
         converted = os.path.join(self.convert_dest, b'converted.ogg')
-        self.assertNoFileTag(converted, 'mp3')
+        self.assertNoFileTag(converted, 'ogg')
+
+    def test_transcode_when_maxbr_set_low_and_same_formats(self):
+        self.config['convert']['max_bitrate'] = 5
+        self.config['convert']['format'] = 'ogg'
+        with control_stdin('y'):
+            self.run_convert()
+        converted = os.path.join(self.convert_dest, b'converted.ogg')
+        self.assertFileTag(converted, 'ogg')
+
+    def test_transcode_when_maxbr_set_to_none_and_same_formats(self):
+        self.config['convert']['format'] = 'ogg'
+        with control_stdin('y'):
+            self.run_convert()
+        converted = os.path.join(self.convert_dest, b'converted.ogg')
+        self.assertNoFileTag(converted, 'ogg')
 
 
 @_common.slow_test()
@@ -272,8 +307,6 @@ class NeverConvertLossyFilesTest(unittest.TestCase, TestHelper,
         self.convert_dest = os.path.join(self.temp_dir, b'convert_dest')
         self.config['convert'] = {
             'dest': self.convert_dest,
-            # Enforce running convert
-            'max_bitrate': 1,
             'paths': {'default': 'converted'},
             'never_convert_lossy_files': True,
             'format': 'mp3',