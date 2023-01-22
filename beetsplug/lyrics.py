00ed448c4dd7e4ea30dfb184e31b8ba51cb1c156
Jordyn <onlinecloud1@gmail.com>
2023-01-22 17:38:39 -0600

    lyrics: Move optional dependency on tidalapi to the Tidal backend

--- beetsplug/lyrics.py
+++ beetsplug/lyrics.py
@@ -28,7 +28,6 @@ import unicodedata
 from unidecode import unidecode
 import warnings
 import urllib
-import tidalapi
 import datetime
 import confuse
 
@@ -448,6 +447,8 @@ class Tidal(Backend):
     REQUIRES_BS = False
     
     def __init__(self, config, log):
+        import tidalapi
+
         super().__init__(config, log)
         self._log = log
         
