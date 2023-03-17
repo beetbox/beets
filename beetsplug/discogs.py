e8dd30e7edb2f6e458afa9b98d4d9872febb19ba
J0J0 Todos <jojo@peek-a-boo.at>
2023-03-17 20:10:07 +0100

    Strip word "vinyl" when searching Discogs
    In DiscogsPlugin.get_albums() we already strip away the words "CD" and "disk".
    It makes sense to also remove "vinyl"

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -277,7 +277,7 @@ class DiscogsPlugin(BeetsPlugin):
         query = re.sub(r'(?u)\W+', ' ', query)
         # Strip medium information from query, Things like "CD1" and "disk 1"
         # can also negate an otherwise positive result.
-        query = re.sub(r'(?i)\b(CD|disc)\s*\d+', '', query)
+        query = re.sub(r'(?i)\b(CD|disc|vinyl)\s*\d+', '', query)
 
         try:
             releases = self.discogs_client.search(query,
