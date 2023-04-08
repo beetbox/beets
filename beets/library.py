a6e014ecc33628f208a76c2296b843e4f9665a44
Šarūnas Nejus <snejus@protonmail.com>
2023-04-08 13:54:23 +0100

    Add support for filtering relations

--- beets/library.py
+++ beets/library.py
@@ -32,6 +32,7 @@ from beets.dbcore import Results, types
 from beets.util import (
     MoveOperation,
     bytestring_path,
+    cached_classproperty,
     normpath,
     samefile,
     syspath,
@@ -640,6 +641,19 @@ class Item(LibModel):
     # Cached album object. Read-only.
     __album = None
 
+    @cached_classproperty
+    def _relation(cls) -> type[Album]:
+        return Album
+
+    @cached_classproperty
+    def relation_join(cls) -> str:
+        """Return the FROM clause which includes related albums.
+
+        We need to use a LEFT JOIN here, otherwise items that are not part of
+        an album (e.g. singletons) would be left out.
+        """
+        return f"LEFT JOIN {cls._relation._table} ON {cls._table}.album_id = {cls._relation._table}.id"
+
     @property
     def _cached_album(self):
         """The Album object that this item belongs to, if any, or
@@ -1239,6 +1253,19 @@ class Album(LibModel):
     ]
 
     _format_config_key = "format_album"
+
+    @cached_classproperty
+    def _relation(cls) -> type[Item]:
+        return Item
+
+    @cached_classproperty
+    def relation_join(cls) -> str:
+        """Return FROM clause which joins on related album items.
+
+        Here we can use INNER JOIN (which is more performant than LEFT JOIN),
+        since we only want to see albums that have at least one Item in them.
+        """
+        return f"INNER JOIN {cls._relation._table} ON {cls._table}.id = {cls._relation._table}.album_id"
 
     @classmethod
     def _getters(cls):
