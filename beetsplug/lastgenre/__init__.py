6bcec25a3f6acc361105815603588d71ec74487e
Alok Saboo <arsaboo@gmx.com>
2023-04-10 06:53:16 -0400

    Update __init__.py

--- beetsplug/lastgenre/__init__.py
+++ beetsplug/lastgenre/__init__.py
@@ -398,6 +398,7 @@ class LastGenrePlugin(plugins.BeetsPlugin):
             if opts.album:
                 # Fetch genres for whole albums
                 for album in lib.albums(ui.decargs(args)):
+                    print(album.genre)
                     album.genre, src = self._get_genre(album)
                     self._log.info('genre for album {0} ({1}): {0.genre}',
                                    album, src)
