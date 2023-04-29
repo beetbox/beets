9944599639dd7c54560cde3b10fadcb5c6752908
Alok Saboo <arsaboo@gmx.com>
2023-04-29 16:44:20 -0400

    Update fetchart.py

--- beetsplug/fetchart.py
+++ beetsplug/fetchart.py
@@ -839,7 +839,7 @@ class CoverArtUrl(RemoteArtSource):
         image_url = None
         try:
             image_url = album.items().get().cover_art_url
-        except ValueError:
+        except AttributeError:
             self._log.debug('Cover art URL not found for {0}', album)
             return
         if image_url:
