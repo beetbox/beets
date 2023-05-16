ade5f8f9f9515968ba99b600ffb5fe95dc8fc18d
Alok Saboo <arsaboo@gmx.com>
2023-05-16 12:18:25 -0400

    Update __init__.py

--- beetsplug/lastgenre/__init__.py
+++ beetsplug/lastgenre/__init__.py
@@ -192,11 +192,16 @@ class LastGenrePlugin(plugins.BeetsPlugin):
         if not tags:
             return None
         
-        # split self.orig_genre into list using self.config['separator'].as_str() as separator and convert them to lower case
+        # split self.orig_genre into list using self.config['separator'].as_str() as separator
         if self.orig_genre is None:
             self.orig_genre = ''
         else:
-            self.orig_genre = [genre.lower() for genre in self.orig_genre.split(self.config['separator'].as_str())]
+            self.orig_genre = [
+                genre.lower() for genre in self.orig_genre.split(
+                    self.config['separator'].as_str()
+                )
+            ]
+        print(f"self.orig_genre: {self.orig_genre}")
 
         if not self.orig_genre is None:
             tags = self.orig_genre + tags
