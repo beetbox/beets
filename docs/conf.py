6a3d17bdf62fac9a84cc65cf911d8295f51f2b24
J0J0 Todos <jojo@peek-a-boo.at>
2023-10-23 09:53:56 +0200

    Revert "Set and configure Sphinx theme book"
    This reverts commit 71873b66de4ccfc151bf87c0940c1c82409ec421.
    
    Revert back to using "pydata" theme. Keep this in the history, just in
    case we reconsider to using "book".

--- docs/conf.py
+++ docs/conf.py
@@ -59,8 +59,8 @@ man_pages = [
     ),
 ]
 
-# Options for book theme
-html_theme = 'sphinx_book_theme'
+# Options for pydata theme
+html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
     'collapse_navigation': True,
     "logo": {
@@ -69,7 +69,3 @@ html_theme_options = {
 }
 html_title = "beets"
 html_logo = "assets/beets_logo.png"
-html_static_path = ['_static']
-html_css_files = [
-    'css/custom.css',
-]
