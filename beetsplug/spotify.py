8d3e370515a41b9e4ec465b6639f1a25410ebee2
Alok Saboo <arsaboo@gmx.com>
2023-11-11 21:35:14 -0500

    Also obtain track ISRC

--- beetsplug/spotify.py
+++ beetsplug/spotify.py
@@ -338,6 +338,7 @@ class SpotifyPlugin(MetadataSourcePlugin, BeetsPlugin):
             medium_index=track_data["track_number"],
             data_source=self.data_source,
             data_url=track_data["external_urls"]["spotify"],
+            isrc=track_data["external_ids"].get("isrc"),
         )
 
     def track_for_id(self, track_id=None, track_data=None):
