1adec914a99ab3b749fae5a33ae8d5a64e109836
Edgars Supe <edgars.supe@gmail.com>
2024-01-25 19:19:37 +0200

    lyrics: Fallback to plain lyrics if synced not available

--- test/plugins/test_lyrics.py
+++ test/plugins/test_lyrics.py
@@ -696,12 +696,26 @@ class LRCLibLyricsTest(unittest.TestCase):
         mock_get.return_value.json.return_value = mock_response
         mock_get.return_value.status_code = 200
 
+        self.plugin.config["synced"] = False
         lyrics = lrclib.fetch("la", "la", "la", 999)
         self.assertEqual(lyrics, mock_response["plainLyrics"])
 
         self.plugin.config["synced"] = True
         lyrics = lrclib.fetch("la", "la", "la", 999)
         self.assertEqual(lyrics, mock_response["syncedLyrics"])
+
+    @patch("beetsplug.lyrics.requests.get")
+    def test_fetch_synced_lyrics_fallback(self, mock_get):
+        mock_response = {
+            "syncedLyrics": "",
+            "plainLyrics": "la la la",
+        }
+        mock_get.return_value.json.return_value = mock_response
+        mock_get.return_value.status_code = 200
+
+        self.plugin.config["synced"] = True
+        lyrics = lrclib.fetch("la", "la", "la", 999)
+        self.assertEqual(lyrics, mock_response["plainLyrics"])
 
     @patch("beetsplug.lyrics.requests.get")
     def test_fetch_plain_lyrics(self, mock_get):
@@ -712,6 +726,7 @@ class LRCLibLyricsTest(unittest.TestCase):
         mock_get.return_value.json.return_value = mock_response
         mock_get.return_value.status_code = 200
 
+        self.plugin.config["synced"] = False
         lyrics = lrclib.fetch("la", "la", "la", 999)
 
         self.assertEqual(lyrics, mock_response["plainLyrics"])
