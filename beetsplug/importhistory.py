224f330877a94555621f7e7d0c291174c31f92f4
Doron Behar <doron.behar@gmail.com>
2024-03-14 20:10:53 +0200

    Better sanity checks before deleting source_dir

--- beetsplug/importhistory.py
+++ beetsplug/importhistory.py
@@ -11,7 +11,7 @@ from beets import library
 from beets.plugins import BeetsPlugin
 from beets.ui import colorize as colorize_text
 from beets.ui import input_options, input_yn
-from beets.util import displayable_path
+from beets.util import displayable_path, syspath
 
 
 class ImportHistPlugin(BeetsPlugin):
@@ -61,99 +61,100 @@ class ImportHistPlugin(BeetsPlugin):
             or item.mb_albumid in self.stop_suggestions_for_albums
         ):
             return
-        # TODO: Consider whether it is even possible for this to happen
-        if os.path.isdir(item.source_path):
-            # We ask the user whether they'd like to delete the item's source
-            # directory
-            delete = input_yn(
-                "The item:\n{path}\nis originated in the directory:\n{source}\n"
-                "Would you like to delete the source directory of this item?".format(
-                    path=colorize_text(
-                        "text_warning", displayable_path(item.path)
-                    ),
-                    source=colorize_text(
-                        "text_warning", displayable_path(item.source_path)
-                    ),
-                ),
-                require=True,
+        if not os.path.isfile(syspath(item.source_path)):
+            self._log.warn(
+                "Item with a source_path that doesn't exist was found:\n%s",
+                item.source_path.decode("utf-8"),
             )
-            if delete:
+            return
+        source_dir = os.path.dirname(syspath(item.source_path))
+        if not (
+            os.access(syspath(item.source_path), os.W_OK) and
+            os.access(source_dir, os.W_OK | os.X_OK)
+        ):
+            self._log.warn(
+                "Item with a source_path isn't deletable:\n%s",
+                item.source_path.decode("utf-8"),
+            )
+            return
+        try:
+            open(syspath(item.source_path), 'w').close()
+        except OSError:
+            self._log.warn(
+                "Item with a source_path is open in another process, and hence "
+                "cannot be deleted:\n%s",
+                item.source_path.decode("utf-8"),
+            )
+            return
+        # We ask the user whether they'd like to delete the item's source
+        # directory
+        print(
+            "The item:\n{path}\nis originated from:\n{source}\n"
+            "What would you like to do?".format(
+                path=colorize_text(
+                    "text_warning", item.path.decode("utf-8")
+                ),
+                source=colorize_text(
+                    "text_warning", item.source_path.decode("utf-8")
+                ),
+            )
+        )
+        resp = input_options(
+            [
+                "Delete the item's source",
+                "Recursively delete the source's directory",
+                "do Nothing",
+                "do nothing and Stop suggesting to delete items from this album",
+            ],
+            require=True,
+        )
+        if resp == "d":
+            self._log.info(
+                "Deleting the item's source file: {}", item.source_path
+            )
+            os.remove(syspath(item.source_path))
+        elif resp == "r":
+            self._log.info(
+                "Searching for other items with a source_path attr containing: {}",
+                source_dir,
+            )
+            source_dir_query = library.PathQuery(
+                "source_path",
+                source_dir,
+                # The "source_path" attribute may not be present in all
+                # items of the library, so we avoid errors with this:
+                fast=False
+            )
+            print(
+                "Doing so will delete the following items' sources as well:"
+            )
+            for searched_item in item._db.items(source_dir_query):
+                print(
+                    colorize_text(
+                        "text_warning",
+                        searched_item["path"].decode("utf-8"),
+                    )
+                )
+            print("Would you like to continue?")
+            continue_resp = input_options(
+                ["Yes", "delete None", "delete just the File"],
+                require=False,  # Yes is the a default
+            )
+            if continue_resp == "y":
                 self._log.info(
-                    "Deleting the item's source which is a directory: %s",
+                    "Deleting the item's source directory: %s", source_dir
+                )
+                rmtree(source_dir)
+            elif continue_resp == "n":
+                self._log.info("doing nothing - aborting hook function")
+                return
+            elif continue_resp == "f":
+                self._log.info(
+                    "removing just the item's original source: %s",
                     item.source_path,
                 )
-                rmtree(item.source_path)
-        elif os.path.isfile(item.source_path):
-            # We ask the user whether they'd like to delete the item's source
-            # directory
-            print(
-                "The item:\n{path}\nis originated from:\n{source}\n"
-                "What would you like to do?".format(
-                    path=colorize_text(
-                        "text_warning", item.path.decode("utf-8")
-                    ),
-                    source=colorize_text(
-                        "text_warning", item.source_path.decode("utf-8")
-                    ),
-                )
-            )
-            resp = input_options(
-                [
-                    "Delete the item's source",
-                    "Recursively delete the source's directory",
-                    "do Nothing",
-                    "do nothing and Stop suggesting to delete items from this album",
-                ],
-                require=True,
-            )
-            if resp == "d":
-                self._log.info(
-                    "Deleting the item's source file: {}", item.source_path
-                )
                 os.remove(item.source_path)
-            elif resp == "r":
-                source_dir = os.path.dirname(item.source_path)
-                self._log.info(
-                    "Searching for other items with a source_path attr containing: {}",
-                    source_dir,
-                )
-                source_dir_query = library.PathQuery(
-                    "source_path",
-                    source_dir,
-                    # The "source_path" attribute may not be present in all
-                    # items of the library, so we avoid errors with this:
-                    fast=False
-                )
-                print(
-                    "Doing so will delete the following items' sources as well:"
-                )
-                for searched_item in item._db.items(source_dir_query):
-                    print(
-                        colorize_text(
-                            "text_warning",
-                            searched_item["path"].decode("utf-8"),
-                        )
-                    )
-                print("Would you like to continue?")
-                continue_resp = input_options(
-                    ["Yes", "delete None", "delete just the File"],
-                    require=False,  # Yes is the a default
-                )
-                if continue_resp == "y":
-                    self._log.info(
-                        "Deleting the item's source directory: %s", source_dir
-                    )
-                    rmtree(source_dir)
-                elif continue_resp == "n":
-                    self._log.info("doing nothing - aborting hook function")
-                    return
-                elif continue_resp == "f":
-                    self._log.info(
-                        "removing just the item's original source: %s",
-                        item.source_path,
-                    )
-                    os.remove(item.source_path)
-            elif resp == "s":
-                self.stop_suggestions_for_albums.append(item.mb_albumid)
-            else:
-                self._log.info("Doing nothing")
+        elif resp == "s":
+            self.stop_suggestions_for_albums.append(item.mb_albumid)
+        else:
+            self._log.info("Doing nothing")
