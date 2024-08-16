7b45f8d4b019cfe28c80f991e791b4fd7e72886c
Šarūnas Nejus <snejus@protonmail.com>
2024-08-16 08:34:24 +0100

    Add Item.filepath property to simplify path handling
    Additionally, fix DefaultTemplateFunctions._func_names definition.

--- beets/library.py
+++ beets/library.py
@@ -12,8 +12,8 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-"""The core data store and collection logic for beets.
-"""
+"""The core data store and collection logic for beets."""
+
 from __future__ import annotations
 
 import os
@@ -24,6 +24,7 @@ import sys
 import time
 import unicodedata
 from functools import cached_property
+from pathlib import Path
 
 from mediafile import MediaFile, UnreadableFileError
 
@@ -657,6 +658,11 @@ class Item(LibModel):
             f"LEFT JOIN {cls._relation._table} "
             f"ON {cls._table}.album_id = {cls._relation._table}.id"
         )
+
+    @property
+    def filepath(self) -> Path | None:
+        """The path to the item's file as pathlib.Path."""
+        return Path(os.fsdecode(self.path)) if self.path else self.path
 
     @property
     def _cached_album(self):
@@ -1741,6 +1747,11 @@ class DefaultTemplateFunctions:
 
     _prefix = "tmpl_"
 
+    @cached_classproperty
+    def _func_names(cls) -> list[str]:
+        """Names of tmpl_* functions in this class."""
+        return [s for s in dir(cls) if s.startswith(cls._prefix)]
+
     def __init__(self, item=None, lib=None):
         """Parametrize the functions.
 
@@ -2038,11 +2049,3 @@ class DefaultTemplateFunctions:
             return trueval if trueval else self.item.formatted().get(field)
         else:
             return falseval
-
-
-# Get the name of tmpl_* functions in the above class.
-DefaultTemplateFunctions._func_names = [
-    s
-    for s in dir(DefaultTemplateFunctions)
-    if s.startswith(DefaultTemplateFunctions._prefix)
-]
