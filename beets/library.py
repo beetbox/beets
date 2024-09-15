96e011e9eaf723ab0a38a7d6760142a4d16ea65a
Šarūnas Nejus <snejus@protonmail.com>
2024-09-15 03:15:14 +0100

    Remove unused platform parameter in item.destination

--- beets/library.py
+++ beets/library.py
@@ -1025,7 +1025,6 @@ class Item(LibModel):
         self,
         relative_to_libdir=False,
         basedir=None,
-        platform=None,
         path_formats=None,
     ) -> bytes:
         """Return the path in the library directory designated for the item
@@ -1035,7 +1034,6 @@ class Item(LibModel):
         library's base directory for the destination.
         """
         db = self._check_db()
-        platform = platform or sys.platform
         basedir = basedir or db.directory
         path_formats = path_formats or db.path_formats
 
@@ -1065,7 +1063,7 @@ class Item(LibModel):
         subpath = self.evaluate_template(subpath_tmpl, True)
 
         # Prepare path for output: normalize Unicode characters.
-        if platform == "darwin":
+        if sys.platform == "darwin":
             subpath = unicodedata.normalize("NFD", subpath)
         else:
             subpath = unicodedata.normalize("NFC", subpath)
