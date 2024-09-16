abc9192b2ce80f0523d268d87629fa6442721275
Šarūnas Nejus <snejus@protonmail.com>
2024-09-16 16:17:42 +0100

    Rip away io stuff from TestHelper to IOMixin

--- test/test_ui_init.py
+++ test/test_ui_init.py
@@ -21,14 +21,15 @@ from random import random
 
 from beets import config, ui
 from beets.test import _common
-from beets.test.helper import BeetsTestCase, ItemInDBTestCase, control_stdin
+from beets.test.helper import (
+    BeetsTestCase,
+    IOMixin,
+    ItemInDBTestCase,
+    control_stdin,
+)
 
 
-class InputMethodsTest(BeetsTestCase):
-    def setUp(self):
-        super().setUp()
-        self.io.install()
-
+class InputMethodsTest(IOMixin, BeetsTestCase):
     def _print_helper(self, s):
         print(s)
 
