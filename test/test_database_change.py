54c01f964320d19d4f980512dc5658ce7429e1d4
valrus <ian@mccowan.space>
2024-12-25 14:02:03 -0800

    Add failing test for dupe db changes

---
+++ test/test_database_change.py
@@ -0,0 +1,24 @@
+import os
+
+import beets
+from beets.test import _common
+from beets.test.helper import BeetsTestCase, capture_log
+
+
+class DatabaseChangeTestBase(BeetsTestCase):
+    def test_item_added_one_database_change(self):
+        self.item = _common.item()
+        self.item.path = beets.util.normpath(
+            os.path.join(
+                self.temp_dir,
+                b"a",
+                b"b.mp3",
+            )
+        )
+        self.item.album = "a"
+        self.item.title = "b"
+
+        with capture_log() as logs:
+            self.lib.add(self.item)
+
+        assert logs.count("Sending event: database_change") == 1
