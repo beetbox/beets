7dc81061da69179750d222b3a3023730d4bde144
J <25012611+Uncorrupt3318@users.noreply.github.com>
2025-02-08 15:07:59 +0000

    basic working prototype

--- beetsplug/replace.py
+++ beetsplug/replace.py
@@ -1,12 +1,26 @@
 from beets.plugins import BeetsPlugin
-from beets.ui import Subcommand
-
-def replace(lib, opts, args):
-    print("Hello!")
+from beets import ui
+import os
 
 class ReplacePlugin(BeetsPlugin):
     def commands(self):
-        return [replace_command]
+        cmd = ui.Subcommand('replace', help='This command replaces the target audio file, while keeping tags intact')
+        cmd.func = self.run
+        return [cmd]
+    def run(self, lib, opts, args):
+        newFilePath = args[-1]
+        title = args[:-1]
 
-replace_command = Subcommand('replace', help='This command replaces the target audio file, while keeping tags intact')
-replace_command.func = replace
+        song = lib.items(title)[0]
+        song.write(newFilePath)
+        originalFilePath = song.path.decode()
+
+        originalFileBase, originalFileExt = os.path.splitext(originalFilePath)
+        newFileBase, newFileExt = os.path.splitext(newFilePath)
+        dest = originalFileBase + newFileExt
+        destEncoded = dest.encode()
+        os.rename(newFilePath, dest)
+        if newFileExt != originalFileExt:
+            os.remove(originalFilePath)
+        song.path = destEncoded
+        song.store()
