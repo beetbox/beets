7b335090eddb56150cb5748de306188f3e75aad4
J <25012611+Uncorrupt3318@users.noreply.github.com>
2025-02-23 16:09:18 +0000

    add ruff to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/030ba1976b7c0e1a67d9716b17308ccdab5b381e.tar.gz") {};
 in pkgs.mkShell {
   packages = with pkgs; [
+    ruff
     pre-commit
     poetry
     poethepoet
