0f5f4ae223cdbc62dd67db3d48771845b1a5632c
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-04-18 14:26:42 +0200

    plugins: remove no-op load_plugins call
    without an argument (i.e. the default `names=()`), this was a no-op
    actual plugin loading is triggered by tests, or by the ui code

--- beets/plugins.py
+++ beets/plugins.py
@@ -301,7 +301,6 @@ def find_plugins():
         # See https://github.com/beetbox/beets/pull/3810
         return list(_instances.values())
 
-    load_plugins()
     plugins = []
     for cls in _classes:
         # Only instantiate each plugin class once.
