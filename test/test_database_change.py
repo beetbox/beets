3f080ba82e72b30acd7e8c2ef7dd63772bfda282
J0J0 Todos <jojo@peek-a-boo.at>
2025-04-26 19:36:24 +0200

    Add license and docstring to database_change test

--- test/test_database_change.py
+++ test/test_database_change.py
@@ -1,3 +1,19 @@
+# This file is part of beets.
+# Copyright 2025, valrus.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+"""Test beets library add emits only one database_change event."""
+
 import os
 
 import beets
