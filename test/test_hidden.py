47d98cc410586765c50f157a4be8a8ee3d58fbcc
valrus <ian@mccowan.space>
2025-04-27 09:26:42 -0700

    Fix imports actually with ruff this time

--- test/test_hidden.py
+++ test/test_hidden.py
@@ -22,8 +22,7 @@ import tempfile
 import unittest
 
 from beets import util
-from beets.util import bytestring_path
-from beets.util import hidden
+from beets.util import bytestring_path, hidden
 
 
 class HiddenFileTest(unittest.TestCase):
