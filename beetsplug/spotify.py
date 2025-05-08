7836fac6a76de1d3d770f2f2349f8b8346a8997e
Sebastian Mohr <sebastian@mohrenclan.de>
2025-05-08 15:02:45 +0200

    Formatting issue

--- beetsplug/spotify.py
+++ beetsplug/spotify.py
@@ -367,9 +367,9 @@ class SpotifyPlugin(SearchApiMetadataSourcePluginNext):
                     "Retry-After", DEFAULT_WAITING_TIME
                 )
                 self._log.debug(
-                    f"Too many API requests. Retrying after "
-                    f"{seconds} seconds."
+                    f"Too many API requests. Retrying after {seconds} seconds."
                 )
+
                 time.sleep(int(seconds) + 1)
                 return self._handle_response(
                     method,
