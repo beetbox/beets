b7a8ca0b9780fb3c6da80882f808c4d46d4849cb
Šarūnas Nejus <snejus@protonmail.com>
2025-05-25 14:01:29 +0100

    Test sanitize_pairs

--- test/util/test_config.py
+++ test/util/test_config.py
@@ -1,15 +1,38 @@
-import unittest
+import pytest
 
-from beets.util.config import sanitize_choices
+from beets.util.config import sanitize_choices, sanitize_pairs
 
 
-class HelpersTest(unittest.TestCase):
-    def test_sanitize_choices(self):
-        assert sanitize_choices(["A", "Z"], ("A", "B")) == ["A"]
-        assert sanitize_choices(["A", "A"], ("A")) == ["A"]
-        assert sanitize_choices(["D", "*", "A"], ("A", "B", "C", "D")) == [
-            "D",
-            "B",
-            "C",
-            "A",
-        ]
+@pytest.mark.parametrize(
+    "input_choices, valid_choices, expected",
+    [
+        (["A", "Z"], ("A", "B"), ["A"]),
+        (["A", "A"], ("A"), ["A"]),
+        (["D", "*", "A"], ("A", "B", "C", "D"), ["D", "B", "C", "A"]),
+    ],
+)
+def test_sanitize_choices(input_choices, valid_choices, expected):
+    assert sanitize_choices(input_choices, valid_choices) == expected
+
+
+def test_sanitize_pairs():
+    assert sanitize_pairs(
+        [
+            ("foo", "baz bar"),
+            ("foo", "baz bar"),
+            ("key", "*"),
+            ("*", "*"),
+            ("discard", "bye"),
+        ],
+        [
+            ("foo", "bar"),
+            ("foo", "baz"),
+            ("foo", "foobar"),
+            ("key", "value"),
+        ],
+    ) == [
+        ("foo", "baz"),
+        ("foo", "bar"),
+        ("key", "value"),
+        ("foo", "foobar"),
+    ]
