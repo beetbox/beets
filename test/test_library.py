632ae2aa336e5a50e25ba909b8e54e7d165af742
Ian McCowan <ian@mccowan.space>
2025-05-30 06:41:29 -0700

    Fix duplicate database change event send on Library.add (#5561)
    ## Description
    
    Fixes #5560. Also a couple other incidental changes / improvements:
    * Add `EventType` that holds the actual string literals used for event
    sending. With type checking, this can prevent subtle bugs resulting from
    misspelled event names.
    * Fix `HiddenFileTest` by using `bytestring_path()`
    
    ## To Do
    
    - [x] ~Documentation.~
    - [x] Changelog.
    - [x] Tests.
    
    ---------
    
    Co-authored-by: J0J0 Todos <jojo@peek-a-boo.at>
    Co-authored-by: J0J0 Todos <2733783+JOJ0@users.noreply.github.com>

--- test/test_library.py
+++ test/test_library.py
@@ -29,11 +29,12 @@ from mediafile import MediaFile, UnreadableFileError
 
 import beets.dbcore.query
 import beets.library
+import beets.logging as blog
 from beets import config, plugins, util
 from beets.library import Album
 from beets.test import _common
 from beets.test._common import item
-from beets.test.helper import BeetsTestCase, ItemInDBTestCase
+from beets.test.helper import BeetsTestCase, ItemInDBTestCase, capture_log
 from beets.util import as_string, bytestring_path, normpath, syspath
 
 # Shortcut to path normalization.
@@ -125,6 +126,25 @@ class AddTest(BeetsTestCase):
             .fetchone()["grouping"]
         )
         assert new_grouping == self.i.grouping
+
+    def test_library_add_one_database_change_event(self):
+        """Test library.add emits only one database_change event."""
+        self.item = _common.item()
+        self.item.path = beets.util.normpath(
+            os.path.join(
+                self.temp_dir,
+                b"a",
+                b"b.mp3",
+            )
+        )
+        self.item.album = "a"
+        self.item.title = "b"
+
+        blog.getLogger("beets").set_global_level(blog.DEBUG)
+        with capture_log() as logs:
+            self.lib.add(self.item)
+
+        assert logs.count("Sending event: database_change") == 1
 
 
 class RemoveTest(ItemInDBTestCase):
