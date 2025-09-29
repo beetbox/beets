f8766c1fb8623fe9c9c541b5e4fea5f703d12c16
J0J0 Todos <jojo@peek-a-boo.at>
2025-09-29 11:46:48 +0200

    smartplaylist: Display available lists cp-ready
    When supplying an invalid playlist name, the list of all playlists is
    shell quoted (single quotes) and becomes usable for copy/paste-ing to
    the user's shell.

--- beetsplug/smartplaylist.py
+++ beetsplug/smartplaylist.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 import os
+from shlex import quote as shell_quote
 from typing import TYPE_CHECKING, Any, TypeAlias
 from urllib.parse import quote
 from urllib.request import pathname2url
@@ -160,8 +161,10 @@ class SmartPlaylistPlugin(BeetsPlugin):
             }
             if not playlists:
                 unmatched = [name for name, _, _ in self._unmatched_playlists]
+                unmatched.sort()
+                quoted_names = " ".join(shell_quote(name) for name in unmatched)
                 raise ui.UserError(
-                    f"No playlist matching any of {unmatched} found"
+                    f"No playlist matching any of {quoted_names} found"
                 )
 
             self._matched_playlists = playlists
