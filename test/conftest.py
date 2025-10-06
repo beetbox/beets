e96b3bd032e245d9358c1dd613d2a496ad57d0aa
Šarūnas Nejus <snejus@protonmail.com>
2025-10-06 09:00:46 +0100

    Reset cached_classproperty cache for every test

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 from beets.dbcore.query import Query
+from beets.util import cached_classproperty
 
 
 def skip_marked_items(items: list[pytest.Item], marker_name: str, reason: str):
@@ -41,3 +42,8 @@ def pytest_make_parametrize_id(config, val, argname):
         return inspect.getsource(val).split("lambda")[-1][:30]
 
     return repr(val)
+
+
+@pytest.fixture(autouse=True)
+def clear_cached_classproperty():
+    cached_classproperty.cache.clear()
