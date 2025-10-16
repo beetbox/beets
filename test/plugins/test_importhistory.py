d842882d0637a517f02fb59fe107ff5c184a2583
Šarūnas Nejus <snejus@protonmail.com>
2025-10-16 09:36:21 +0100

    Use explicit path query

--- test/plugins/test_importhistory.py
+++ test/plugins/test_importhistory.py
@@ -35,64 +35,45 @@ def preserve_plugin_listeners():
 
 class ImportHistTest(PluginMixin, AutotagImportTestCase):
     plugin = "importhistory"
+    preload_plugin = False
 
     def setUp(self):
         preserve_plugin_listeners()
         super().setUp()
+        self.config[self.plugin]["suggest_removal"] = True
+        self.load_plugins()
         self.prepare_album_for_import(2)
         self.importer = self.setup_importer()
         self.importer.add_choice(importer.Action.APPLY)
-        # TODO: Set this
-        self.source_dir = os.path.join(self.import_dir, b"album")
-
-    def test_imported_album_has_source_path(self):
         self.importer.run()
-        album = self.lib.albums().get()
-        for item in album.items():
-            assert self.source_dir in item.source_path
+        self.all_items = self.lib.albums().get().items()
+        self.item_to_remove = self.all_items[0]
+
+    def interact(self, stdin_input: str):
+        with control_stdin(stdin_input):
+            self.run_command(
+                "remove",
+                f"path:{syspath(self.item_to_remove.path)}",
+            )
 
     def test_do_nothing(self):
-        with self.configure_plugin({"suggest_removal": True}):
-            self.importer.run()
-            item_to_remove = self.lib.albums().get().items()[0]
-            with control_stdin("N"):
-                self.run_command(
-                    "remove",
-                    syspath(item_to_remove.path),
-                )
-        assert os.path.exists(item_to_remove.source_path)
+        self.interact("N")
+
+        assert os.path.exists(self.item_to_remove.source_path)
 
     def test_remove_single(self):
-        with self.configure_plugin({"suggest_removal": True}):
-            self.importer.run()
-            items_to_remove = self.lib.albums().get().items()
-            with control_stdin("y\nD"):
-                self.run_command(
-                    "remove",
-                    syspath(items_to_remove[0].path),
-                )
-        assert not os.path.exists(items_to_remove[0].source_path)
+        self.interact("y\nD")
+
+        assert not os.path.exists(self.item_to_remove.source_path)
 
     def test_remove_all_from_single(self):
-        with self.configure_plugin({"suggest_removal": True}):
-            self.importer.run()
-            items_to_remove = self.lib.albums().get().items()
-            with control_stdin("y\nR\ny"):
-                self.run_command(
-                    "remove",
-                    syspath(items_to_remove[0].path),
-                )
-        for item in items_to_remove:
+        self.interact("y\nR\ny")
+
+        for item in self.all_items:
             assert not os.path.exists(item.source_path)
 
     def test_stop_suggesting(self):
-        with self.configure_plugin({"suggest_removal": True}):
-            self.importer.run()
-            items_to_remove = self.lib.albums().get().items()
-            with control_stdin("y\nS"):
-                self.run_command(
-                    "remove",
-                    os.path.dirname(syspath(items_to_remove[0].path)),
-                )
-        for item in items_to_remove:
+        self.interact("y\nS")
+
+        for item in self.all_items:
             assert os.path.exists(item.source_path)
