2cc81a0db5188055445c18433d25726f90be6293
Šarūnas Nejus <snejus@protonmail.com>
2025-11-05 11:43:51 +0000

    pyupgrade Python 3.10

--- beets/util/hidden.py
+++ beets/util/hidden.py
@@ -20,10 +20,9 @@ import os
 import stat
 import sys
 from pathlib import Path
-from typing import Union
 
 
-def is_hidden(path: Union[bytes, Path]) -> bool:
+def is_hidden(path: bytes | Path) -> bool:
     """
     Determine whether the given path is treated as a 'hidden file' by the OS.
     """
