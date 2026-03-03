049deac235b798e9af764e07b9ec8d59528bcf6e
Peter Dolan <peter@peterdolan.us>
2026-03-02 19:36:26 -0500

    Fix tests

--- test/test_library.py
+++ test/test_library.py
@@ -1322,7 +1322,8 @@ class ItemsWithProgressTest(BeetsTestCase):
         items_progress_list = list(
             self.lib.items_with_progress("Test", unit="items")
         )
-        assert items_progress_list == items_list
+        assert len(items_progress_list) == len(items_list)
+        assert [i.id for i in items_progress_list] == [i.id for i in items_list]
 
     def test_items_with_progress_respects_query(self):
         i1 = self.add_item_fixture()
@@ -1349,7 +1350,8 @@ class AlbumsWithProgressTest(BeetsTestCase):
         albums_progress_list = list(
             self.lib.albums_with_progress("Test", unit="albums")
         )
-        assert albums_progress_list == albums_list
+        assert len(albums_progress_list) == len(albums_list)
+        assert [a.id for a in albums_progress_list] == [a.id for a in albums_list]
 
     def test_albums_with_progress_respects_query(self):
         self.add_album_fixture()
