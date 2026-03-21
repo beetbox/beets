5fad834ad2248b1223d4f816bf0dde89785acaa1
Šarūnas Nejus <snejus@protonmail.com>
2026-03-21 18:18:49 +0000

    Ensure we use Distance to initialise Match objects

--- beetsplug/mbsync.py
+++ beetsplug/mbsync.py
@@ -17,6 +17,7 @@
 from collections import defaultdict
 
 from beets import library, metadata_plugins, ui, util
+from beets.autotag.distance import Distance
 from beets.autotag.hooks import AlbumMatch, TrackMatch
 from beets.plugins import BeetsPlugin, apply_item_changes
 
@@ -91,7 +92,7 @@ class MBSyncPlugin(BeetsPlugin):
 
             # Apply.
             with lib.transaction():
-                TrackMatch(0, track_info, item).apply_metadata()
+                TrackMatch(Distance(), track_info, item).apply_metadata()
                 apply_item_changes(lib, item, move, pretend, write)
 
     def albums(self, lib, query, move, pretend, write):
@@ -158,7 +159,7 @@ class MBSyncPlugin(BeetsPlugin):
             self._log.debug("applying changes to {}", album)
             with lib.transaction():
                 AlbumMatch(
-                    0, album_info, dict(item_info_pairs)
+                    Distance(), album_info, dict(item_info_pairs)
                 ).apply_metadata()
                 changed = False
                 # Find any changed item to apply changes to album.
