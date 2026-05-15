8cdedf87944bca0557389dcd5b1e434019962617
Pieter Lenaerts <plenae@disroot.org>
2026-05-15 09:30:25 +0200

    Fix formatting in test/plugins/test_web_xss.py

--- test/plugins/test_web_xss.py
+++ test/plugins/test_web_xss.py
@@ -11,6 +11,11 @@ The test checks the index.html template file served by Flask to ensure
 all user data interpolations in the Underscore.js templates use the escaping
 syntax.
 
+This was reported in
+https://github.com/beetbox/beets/security/advisories/GHSA-3gxm-wfjx-m847
+and remediated in
+https://github.com/plenaerts/beets/commit/75f0d8f4899e61afb939adf02dcfb078aed23a6a
+
 Generated using mistral vibe, verified by Pieter Lenaerts <plenae@disroot.org>
 """
 
@@ -53,18 +58,34 @@ class WebXSSTest(ItemInDBTestCase):
 
         # Check that no <%= %> (non-escaping) tags exist for user data
         # We look for <%= followed by a variable name (word characters)
-        non_escaping_pattern = r'<%=\s*(\w+)\s*%>'
-        non_escaping_matches = re.findall(non_escaping_pattern, all_template_content)
+        non_escaping_pattern = r"<%=\s*(\w+)\s*%>"
+        non_escaping_matches = re.findall(
+            non_escaping_pattern, all_template_content
+        )
 
         # List of fields that should be escaped (user-controlled data)
         user_data_fields = [
-            'title', 'artist', 'album', 'year', 'track', 'tracktotal',
-            'disc', 'disctotal', 'length', 'format', 'bitrate',
-            'mb_trackid', 'id', 'lyrics', 'comments'
+            "title",
+            "artist",
+            "album",
+            "year",
+            "track",
+            "tracktotal",
+            "disc",
+            "disctotal",
+            "length",
+            "format",
+            "bitrate",
+            "mb_trackid",
+            "id",
+            "lyrics",
+            "comments",
         ]
 
         # Check if any user data fields are using non-escaping <%= %>
-        vulnerable_fields = [field for field in non_escaping_matches if field in user_data_fields]
+        vulnerable_fields = [
+            field for field in non_escaping_matches if field in user_data_fields
+        ]
 
         # If we found any user data fields using <%= %>, the templates are vulnerable
         assert len(vulnerable_fields) == 0, (
@@ -73,11 +94,13 @@ class WebXSSTest(ItemInDBTestCase):
         )
 
         # Also verify that escaping tags (<%- %>) are present for user data
-        escaping_pattern = r'<%-\s*(\w+)\s*%>'
+        escaping_pattern = r"<%-\s*(\w+)\s*%>"
         escaping_matches = re.findall(escaping_pattern, all_template_content)
 
         # At least some user data fields should use escaping
-        safe_fields = [field for field in escaping_matches if field in user_data_fields]
+        safe_fields = [
+            field for field in escaping_matches if field in user_data_fields
+        ]
         assert len(safe_fields) > 0, (
             "No escaping <%- %> tags found for user data fields. "
             "Templates should use <%- %> for HTML escaping."
