12639cde609f4c5d58862e0db8f3482191882fcf
Konstantin <78656278+amogus07@users.noreply.github.com>
2026-05-19 11:44:30 +0200

    avoid casting from object

--- beets/util/__init__.py
+++ beets/util/__init__.py
@@ -29,7 +29,7 @@ import sys
 import tempfile
 import traceback
 from collections import Counter
-from collections.abc import Sequence
+from collections.abc import Hashable, Sequence
 from contextlib import suppress
 from enum import Enum
 from functools import cache
@@ -37,16 +37,7 @@ from importlib import import_module
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
 from re import Pattern
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    AnyStr,
-    ClassVar,
-    Generic,
-    NamedTuple,
-    TypeVar,
-    cast,
-)
+from typing import TYPE_CHECKING, Any, AnyStr, Generic, NamedTuple, TypeVar
 
 from unidecode import unidecode
 
@@ -1063,7 +1054,15 @@ def par_map(transform: Callable[[T], Any], items: Sequence[T]) -> None:
         pool.map(_worker, items)
 
 
-class cached_classproperty(Generic[T, R_co]):
+H = TypeVar("H", bound=type[Hashable])
+
+
+@cache
+def _cached_classproperty_get(getter: Callable[[H], R_co], owner: H, /) -> R_co:
+    return getter(owner)
+
+
+class cached_classproperty(Generic[H, R_co]):
     """Descriptor implementing cached class properties.
 
     Must be used in combination with @classmethod.
@@ -1073,30 +1072,27 @@ class cached_classproperty(Generic[T, R_co]):
     instance properties, this operates on the class rather than instances.
     """
 
-    _cache: ClassVar[dict[tuple[type[object], str], object]] = {}
-
-    def __init__(self, getter: Callable[[type[T]], R_co], /) -> None:
+    def __init__(self, getter: Callable[[H], R_co], /) -> None:
         """Initialize the descriptor with the property getter function."""
-        self.getter: Callable[[type[T]], R_co] = getter
+        self.getter: Callable[[H], R_co] = getter.__func__  # type: ignore[attr-defined]
         self.name: str
 
-    def __set_name__(self, owner: type[T], name: str, /) -> None:
+    def __set_name__(self, owner: H, name: str, /) -> None:
         """Capture the attribute name this descriptor is assigned to."""
         self.name = name
 
-    def __get__(self, instance: T | None, owner: type[T], /) -> R_co:
+    @staticmethod
+    @cache
+    def _get_cached(getter: Callable[[H], R_co], owner: H, /) -> R_co:
+        return getter(owner)
+
+    def __get__(self, instance: object | None, owner: H, /) -> R_co:
         """Compute and cache if needed, and return the property value."""
-        key: tuple[type[object], str] = owner, self.name
-        try:
-            return cast(R_co, type(self)._cache[key])
-        except KeyError:
-            obj: R_co = self.getter.__func__(owner)  # type: ignore[attr-defined]
-            type(self)._cache[key] = obj
-            return obj
+        return _cached_classproperty_get(self.getter, owner)
 
     @classmethod
     def clear_cache(cls) -> None:
-        cls._cache.clear()
+        _cached_classproperty_get.cache_clear()
 
 
 class LazySharedInstance(Generic[T]):
