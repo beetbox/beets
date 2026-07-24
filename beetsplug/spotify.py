cc2f665884aa89a0c1206197f7b18b962e7b5548
Alok Saboo <arsaboo@gmx.com>
2026-07-24 09:46:38 -0400

    improve 503 error handling

--- beetsplug/spotify.py
+++ beetsplug/spotify.py
@@ -290,8 +290,20 @@ class SpotifyPlugin(
                     method, url, params=params, retry_count=retry_count + 1
                 )
             if e.response.status_code == 503:
-                self._log.error("Service Unavailable.")
-                raise APIError("Service Unavailable.")
+                seconds = e.response.headers.get(
+                    "Retry-After", DEFAULT_WAITING_TIME
+                )
+                self._log.debug(
+                    "Service Unavailable. Retrying after {} seconds.",
+                    seconds,
+                )
+                time.sleep(int(seconds) + 1)
+                return self._handle_response(
+                    method,
+                    url,
+                    params=params,
+                    retry_count=retry_count + 1,
+                )
             if e.response.status_code == 502:
                 self._log.error("Bad Gateway.")
                 raise APIError("Bad Gateway.")
