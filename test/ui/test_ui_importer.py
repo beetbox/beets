00aaad8920179eeff7085cd1778a39b5000fd575
Shurong Cao <170531907+CAOShurong@users.noreply.github.com>
2026-09-10 00:07:04 +0800

    test: add regression tests for manual id and singleton manual search

--- test/ui/test_ui_importer.py
+++ test/ui/test_ui_importer.py
@@ -4,6 +4,8 @@ test_importer module. But here the test importer inherits from
 ``TerminalImportSession``. So we test this class, too.
 """
 
+from unittest import mock
+
 from beets import importer
 from beets.test.helper import TerminalImportMixin
 from test import test_importer
@@ -18,11 +20,17 @@ class TestNonAutotaggedImport(
 class TestImport(TerminalImportMixin, test_importer.TestImport):
     pass
 
-
 class ImportSingletonTest(
     TerminalImportMixin, test_importer.ImportSingletonTest
 ):
-    pass
+    def test_singleton_manual_search_updates_candidates(self):
+        self.importer.io.addinput("e")
+        self.importer.io.addinput("ManualArtist")
+        self.importer.io.addinput("ManualTrack")
+        self.importer.io.addinput("a")
+        self.importer.run()
+        assert not self.lib.albums()
+        assert self.lib.items().get().title == "ManualTrack"
 
 
 class ImportTracksTest(TerminalImportMixin, test_importer.ImportTracksTest):
@@ -51,6 +59,19 @@ class ChooseCandidateTest(
         self.importer.run()
         album = self.lib.albums().get()
         assert "ManualAlbum" in album.album
+
+    def test_manual_id_updates_candidates(self):
+        album_info = self.matcher._make_album_match("IDArtist", "IDAlbum", 1)
+        with mock.patch(
+            "beets.metadata_plugins.albums_for_ids", return_value=[album_info]
+        ):
+            self.importer.io.addinput("i")
+            self.importer.io.addinput("custom_release_id")
+            self.importer.io.addinput("1")
+            self.importer.io.addinput("a")
+            self.importer.run()
+            album = self.lib.albums().get()
+            assert "IDAlbum" in album.album
 
 
 class GroupAlbumsImportTest(
